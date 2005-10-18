2b1976468fc579c47775c3a8b59ce4187ed49dcd
Konstantin V. Arkhipov <voxus@onphp.org>
2005-10-18 17:43:17 +0000

    + storableMappedDAO
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@924 db88c99c-74ec-0310-8720-8416fdbde762

---
+++ main/DAOs/MappedStorableDAO.class.php
@@ -0,0 +1,83 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2005 by Konstantin V. Arkhipov                          *
+ *   voxus@shadanakar.org                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id$ */
+
+	abstract class MappedStorableDAO extends SmartDAO implements MappedDAO
+	{
+		public function getIdName()
+		{
+			return 'id';
+		}
+		
+		public function getFields()
+		{
+			static $fields = null;
+			
+			if ($fields === null)
+				foreach ($this->mapping as $prop => $field)
+					$fields[] = ($field === null ? $prop : $field);
+			
+			return $fields;
+		}
+
+		public function take(Identifiable $object)
+		{
+			return
+				$object->getId()
+					? $this->save($object)
+					: $this->add($object);
+		}
+		
+		public function add(Identifiable $object)
+		{
+			$object =
+				$this->inject(
+					OSQL::insert(),
+					$object->setId(
+						DBFactory::getDefaultInstance()->obtainSequence(
+							$this->getSequence()
+						)
+					)
+				);
+			
+			$this->dropLists();
+			
+			return $object;
+		}
+		
+		public function save(Identifiable $object)
+		{
+			return
+				$this->inject(
+					OSQL::update()->where(
+						Expression::eqId($this->getIdName(), $object)
+					),
+					$object
+				);
+		}
+
+		protected function inject(
+			InsertOrUpdateQuery $query, Identifiable $object
+		)
+		{
+			DBFactory::getDefaultInstance()->queryNull(
+				$this->setQueryFields(
+					$query->setTable($this->getTable()), $object
+				)
+			);
+			
+			$this->uncacheById($object->getId());
+			
+			return $object;
+		}
+	}
+?>
