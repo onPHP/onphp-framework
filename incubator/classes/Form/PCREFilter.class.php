dc0e21438838721f4b62f74dc7301b323fd840a6
Sveta A. Smirnova <sveta@microbecal.com>
2005-10-25 23:08:10 +0000

    new Filter
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@985 db88c99c-74ec-0310-8720-8416fdbde762

---
+++ incubator/classes/Form/PCREFilter.class.php
@@ -0,0 +1,32 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2005 by Sveta Smirnova                                  *
+ *   sveta@microbecal.com                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id$ */
+
+	class PCREFilter extends BaseFilter
+	{
+		private $search 	= null;
+		private $replace	= null;
+		
+		public function setExpression($search, $replace)
+		{
+			$this->search = $search;
+			$this->replace = $replace;
+			
+			return $this;
+		}
+		
+		public function apply($value)
+		{
+			return preg_replace($this->search, $this->replace, $value);
+		}
+	}
+?>
