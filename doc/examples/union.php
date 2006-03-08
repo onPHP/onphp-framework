74dd6fb022dbc8e7e213d27f24e3630d2ae7b92e
Konstantin V. Arkhipov <voxus@onphp.org>
2006-03-08 22:01:30 +0000

    * cosmetics
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@1312 db88c99c-74ec-0310-8720-8416fdbde762

--- doc/examples/union.php
+++ doc/examples/union.php
@@ -1,6 +1,6 @@
-<?
+<?php
 /***************************************************************************
- *   Copyright (C) 2006 by Sergey S. Sergeev                               *
+ *   Copyright (C) 2005-2006 by Sergey S. Sergeev                          *
  *   webs.support@gmail.com                                                *
  *   	                                                                   *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -11,73 +11,71 @@
  ***************************************************************************/
 /* $Id: main.inc.php 1188 2006-01-21 03:02:20Z voxus $ */
 
-require dirname(__FILE__).'/../../global.inc.php.tpl';
-
-define('DB_BASE', 'test');
-define('DB_USER', 'postgres');
-define('DB_PASS', '');
-define('DB_HOST', 'localhost');
-define('DB_CLASS', 'PgSQL');
-setlocale(LC_CTYPE, "ru_RU.UTF-8");
-setlocale(LC_TIME, "ru_RU.UTF-8");
-define('DEFAULT_ENCODING', 'UTF-8');
-$db = DBFactory::getDefaultInstance();
-
-//example queries
-$query1 = OSQL::select()->from('message1')->get(new DBField('id', 'message1'));
-$query2 = OSQL::select()->from('message2')->get(new DBField('id', 'message2'));
-$query3 = OSQL::select()->from('message3')->get(new DBField('id', 'message3'));
-
-//example Factory
-$queryUnion1 = CombineQuery::union($query1, $query2);
-$queryUnion1 = CombineQuery::union($queryUnion1, $query3);
-echo "<pre>";
-print_r($queryUnion1->toString($db->getDialect()));
-echo "</pre>";
-
-//example Chain
-$queryUnion2 = CombineQuery::chain();
-$queryUnion2->union($query1);
-$queryUnion2->intersect($query2);
-$queryUnion2->except($query3);
-echo "<pre>";
-print_r($queryUnion2->toString($db->getDialect()));
-echo "</pre>";
-
-//example Block
-$queryUnion3 = CombineQuery::exceptBlock($query1, $query2, $query3);
-echo "<pre>";
-print_r($queryUnion3->toString($db->getDialect()));
-echo "</pre>";
-
-//example composite
-$queryUnion4 = CombineQuery::union($query1, $queryUnion3);
-echo "<pre>";
-print_r($queryUnion4->toString($db->getDialect()));
-echo "</pre>";
-
-$query5 = OSQL::select()->
-	get(new DBField('id', 'foo'))->
-	from($queryUnion3, "foo");
-echo "<pre>";
-print_r($query5->toString($db->getDialect()));
-echo "</pre>";
-
-$query6 = OSQL::select()->
-	get(new DBField('id', 'messages'))->
-	from('messages')->where(
-		Expression::in('id', $queryUnion3)
-	);
-
-echo "<pre>";
-print_r($query6->toString($db->getDialect()));
-echo "</pre>";
-
-$query7 = OSQL::select()->
-	get(new DBField('id', 'messages'))->
-	from(CombineQuery::union($query1, $query2), 'foo');
-
-echo "<pre>";
-print_r($query7->toString($db->getDialect()));
-echo "</pre>";
+	require dirname(__FILE__).'/../../global.inc.php.tpl';
+	
+	$db = DBFactory::getDefaultInstance();
+	
+	// example queries
+	$query1 = OSQL::select()->from('message1')->get(new DBField('id', 'message1'));
+	$query2 = OSQL::select()->from('message2')->get(new DBField('id', 'message2'));
+	$query3 = OSQL::select()->from('message3')->get(new DBField('id', 'message3'));
+	
+	// example Factory
+	$queryUnion1 = CombineQuery::union($query1, $query2);
+	$queryUnion1 = CombineQuery::union($queryUnion1, $query3);
+	
+	echo "<pre>";
+	print_r($queryUnion1->toString($db->getDialect()));
+	echo "</pre>";
+	
+	// example Chain
+	$queryUnion2 = CombineQuery::chain();
+	$queryUnion2->union($query1);
+	$queryUnion2->intersect($query2);
+	$queryUnion2->except($query3);
+	
+	echo "<pre>";
+	print_r($queryUnion2->toString($db->getDialect()));
+	echo "</pre>";
+	
+	// example Block
+	$queryUnion3 = CombineQuery::exceptBlock($query1, $query2, $query3);
+	echo "<pre>";
+	print_r($queryUnion3->toString($db->getDialect()));
+	echo "</pre>";
+	
+	// example composite
+	$queryUnion4 = CombineQuery::union($query1, $queryUnion3);
+	echo "<pre>";
+	print_r($queryUnion4->toString($db->getDialect()));
+	echo "</pre>";
+	
+	$query5 =
+		OSQL::select()->
+		get(new DBField('id', 'foo'))->
+		from($queryUnion3, "foo");
+	
+	echo "<pre>";
+	print_r($query5->toString($db->getDialect()));
+	echo "</pre>";
+	
+	$query6 =
+		OSQL::select()->
+		get(new DBField('id', 'messages'))->
+		from('messages')->where(
+			Expression::in('id', $queryUnion3)
+		);
+	
+	echo "<pre>";
+	print_r($query6->toString($db->getDialect()));
+	echo "</pre>";
+	
+	$query7 =
+		OSQL::select()->
+		get(new DBField('id', 'messages'))->
+		from(CombineQuery::union($query1, $query2), 'foo');
+	
+	echo "<pre>";
+	print_r($query7->toString($db->getDialect()));
+	echo "</pre>";
 ?>
