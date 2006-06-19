9058aec20f716db42ce98d8fbe9bd90cac8ced87
Konstantin V. Arkhipov <voxus@onphp.org>
2006-06-19 16:28:53 +0000

    * typo
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@1737 db88c99c-74ec-0310-8720-8416fdbde762

--- incubator/ext/src/onphp.h
+++ incubator/ext/src/onphp.h
@@ -9,7 +9,7 @@
 	PHP_ME(onphp_ ## class_name, function_name, arg_info, flags)
 
 #define ONPHP_ABSTRACT_ME(class_name, function_name, arg_info, flags) \
-	ZEND_FENTRY(onphp_ ## class_name, NULL, arg_info, flags)
+	ZEND_FENTRY(function_name, NULL, arg_info, flags|ZEND_ACC_ABSTRACT)
 
 #define REGISTER_ONPHP_INTERFACE(class_name) \
 	spl_register_interface(&onphp_ce_ ## class_name, # class_name, onphp_funcs_ ## class_name TSRMLS_CC);
