e35b3f11f223d9bb57504fbe599419e93443cec2
Konstantin V. Arkhipov <voxus@onphp.org>
2006-06-24 20:50:29 +0000

    * 0.2.15 and 0.4.7
    git-svn-id: https://svn.shadanakar.org/onPHP/tags/0.4.7@1772 db88c99c-74ec-0310-8720-8416fdbde762

---
+++ core/OSQL/DBValue.class.php
@@ -0,0 +1,56 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2005-2006 by Anton E. Lebedevich                        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id$ */
+
+	/**
+	 * Container for passing values into OSQL queries.
+	 * 
+	 * @ingroup OSQL
+	 * @ingroup Module
+	**/
+	class DBValue extends Castable implements DialectString
+	{
+		private $value = null;
+		
+		private $unquotable = false; // indeed
+		
+		public static function create($value)
+		{
+			return new DBValue($value);
+		}
+
+		public function __construct($value)
+		{
+			if ($value === (int) $value)
+				$this->unquotable = true;
+			
+			$this->value = $value;
+		}
+
+		public function getValue()
+		{
+			return $this->value;
+		}
+
+		public function toDialectString(Dialect $dialect)
+		{
+			$out =
+				$this->unquotable
+					? $this->value
+					: $dialect->quoteValue($this->value);
+			
+			return
+				$this->cast
+					? $dialect->toCasted($out, $this->cast)
+					: $out;
+		}
+	}
+?>
