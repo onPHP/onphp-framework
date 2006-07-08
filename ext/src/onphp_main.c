68c3bef71bf0d2df547515646c8e097549473f24
Konstantin V. Arkhipov <voxus@onphp.org>
2006-07-08 19:16:33 +0000

    + forgotten onphp_main stuff
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@1855 db88c99c-74ec-0310-8720-8416fdbde762

---
+++ ext/src/onphp_main.c
@@ -0,0 +1,24 @@
+/* $Id$ */
+
+#include "onphp_main.h"
+#include "onphp_util.h"
+
+#include "main/DAOs/Handlers/SegmentHandler.h"
+#include "main/DAOs/Handlers/SharedMemorySegmentHandler.h"
+
+PHP_MINIT_FUNCTION(onphp_main)
+{
+	REGISTER_ONPHP_INTERFACE(SegmentHandler);
+	
+	REGISTER_ONPHP_STD_CLASS_EX(
+		SharedMemorySegmentHandler,
+		onphp_empty_object_new,
+		onphp_funcs_SharedMemorySegmentHandler
+	);
+	REGISTER_ONPHP_PROPERTY(SharedMemorySegmentHandler, "id", ZEND_ACC_PRIVATE);
+	REGISTER_ONPHP_IMPLEMENTS(SharedMemorySegmentHandler, SegmentHandler);
+	REGISTER_ONPHP_CLASS_CONST_LONG(SharedMemorySegmentHandler, "SEGMENT_SIZE", SMSH_SEGMENT_SIZE);
+	onphp_ce_SharedMemorySegmentHandler->ce_flags |= ZEND_ACC_FINAL_CLASS;
+	
+	return PHP_MINIT(SharedMemorySegmentHandler)(INIT_FUNC_ARGS_PASSTHRU);
+}
