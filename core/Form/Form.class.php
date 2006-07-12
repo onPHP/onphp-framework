bfd419846a49e2dd95e8634b0f978638822f8709
Konstantin V. Arkhipov <voxus@onphp.org>
2006-07-12 12:12:29 +0000

    * getTexturalErrors' deprecation
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.4@1876 db88c99c-74ec-0310-8720-8416fdbde762

--- core/Form/Form.class.php
+++ core/Form/Form.class.php
@@ -78,6 +78,16 @@
 		}
 
 		/**
+		 * Bogus spelling of method's name below.
+		 *
+		 * @deprecated and removed in 0.5
+		**/
+		public function getTexturalErrors()
+		{
+			return $this->getTextualErrors();
+		}
+		
+		/**
 		 * Returns plain list of error's labels
 		**/
 		public function getTexturalErrors()
