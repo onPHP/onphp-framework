6ad1b55e5e2dfeff74ac33d4e296c98b84fe822b
Konstantin V. Arkhipov <voxus@onphp.org>
2006-08-08 13:25:38 +0000

    * r1950 propagated further
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.2@1951 db88c99c-74ec-0310-8720-8416fdbde762

--- core/Base/Timestamp.class.php
+++ core/Base/Timestamp.class.php
@@ -37,7 +37,7 @@
 		const WEEKDAY_THURSDAY	= 4;
 		const WEEKDAY_FRIDAY	= 5;
 		const WEEKDAY_SATURDAY	= 6;
-		const WEEKDAY_SUNDAY	= 7;
+		const WEEKDAY_SUNDAY	= 0; // because strftime('%w') is 0 on Sunday
 		
 		public function __construct($timestamp)
 		{
