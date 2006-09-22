f0c396ef55afad1f016ac3961f682bad0933e7b7
Konstantin V. Arkhipov <voxus@onphp.org>
2006-09-22 11:37:38 +0000

    * one more
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@2006 db88c99c-74ec-0310-8720-8416fdbde762

--- core/Cache/WatermarkedPeer.class.php
+++ core/Cache/WatermarkedPeer.class.php
@@ -60,26 +60,11 @@
 			return $this->peer->isAlive();
 		}
 
-		public function set($key, &$value, $expires = Cache::EXPIRES_MEDIUM)
-		{
-			return $this->peer->set($this->watermark.$key, $value, $expires);
-		} 
-		
-		public function add($key, &$value, $expires = Cache::EXPIRES_MEDIUM)
-		{
-			return $this->peer->add($this->watermark.$key, $value, $expires);
-		} 
-
-		public function replace($key, &$value, $expires = Cache::EXPIRES_MEDIUM)
-		{
-			return $this->peer->replace($this->watermark.$key, $value, $expires);
-		}
-		
 		protected function store(
 			$action, $key, &$value, $expires = Cache::EXPIRES_MEDIUM
 		)
 		{
-			throw new UnsupportedMethodException();
+			return $this->peer->$action($this->watermark.$key, $value, $expires);
 		}
 	}
 ?>
