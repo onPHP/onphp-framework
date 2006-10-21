8349bfd5afc23bdebb235f53ad69e92eed06f58f
Konstantin V. Arkhipov <voxus@onphp.org>
2006-10-21 11:30:02 +0000

    * 0.8.0, 0.6.3, 0.4.10, 0.2.17
    git-svn-id: https://svn.shadanakar.org/onPHP/tags/0.2.17@2084 db88c99c-74ec-0310-8720-8416fdbde762

---
+++ core/Form/StripTagsFilter.class.php
@@ -0,0 +1,42 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2005 by Anton E. Lebedevich                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id$ */
+
+	/**
+	 * @see RegulatedPrimitive::addImportFilter()
+	 * 
+	 * @ingroup Filters
+	**/
+	class StripTagsFilter implements Filtrator
+	{
+		private $exclude = null;
+		
+		public static function create()
+		{
+			return new self;
+		}
+		
+		public function setAllowableTags($exclude)
+		{
+			if (null !== $exclude)
+				Assert::isString($exclude);
+			
+			$this->exclude = $exclude;
+			
+			return $this;
+		}
+		
+		public function apply($value)
+		{
+			return strip_tags($value, $this->exclude);
+		}
+	}
+?>
