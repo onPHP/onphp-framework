b9f5bc0873322e7d5f117d34a67c28f3a5f7ecf8
Konstantin V. Arkhipov <voxus@onphp.org>
2006-10-21 11:30:02 +0000

    * 0.8.0, 0.6.3, 0.4.10, 0.2.17
    git-svn-id: https://svn.shadanakar.org/onPHP/tags/0.4.10@2084 db88c99c-74ec-0310-8720-8416fdbde762

---
+++ meta/types/ObjectType.class.php
@@ -0,0 +1,104 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2006 by Konstantin V. Arkhipov                          *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id$ */
+
+	/**
+	 * @ingroup Types
+	**/
+	class ObjectType extends BasePropertyType
+	{
+		private $class = null;
+		
+		public function __construct($class)
+		{
+			$this->class = $class;
+		}
+		
+		public function getClass()
+		{
+			return $this->class;
+		}
+		
+		public function getDeclaration()
+		{
+			return 'null';
+		}
+		
+		public function isGeneric()
+		{
+			return false;
+		}
+		
+		public function isMeasurable()
+		{
+			return false;
+		}
+		
+		public function toMethods(MetaClassProperty $property)
+		{
+			return
+				parent::toMethods($property)
+				.$this->toDropper($property);
+		}
+		
+		public function toSetter(MetaClassProperty $property)
+		{
+			$name = $property->getName();
+			$methodName = 'set'.ucfirst($name);
+			
+			$method = <<<EOT
+
+public function {$methodName}({$this->class} \${$name})
+{
+	\$this->{$name} = \${$name};
+
+	return \$this;
+}
+
+EOT;
+
+			return $method;
+		}
+		
+		public function toDropper(MetaClassProperty $property)
+		{
+			$name = $property->getName();
+			$methodName = 'drop'.ucfirst($name);
+			
+			$method = <<<EOT
+
+public function {$methodName}()
+{
+	\$this->{$name} = null;
+
+	return \$this;
+}
+
+EOT;
+
+			return $method;
+		}
+		
+		public function toPrimitive()
+		{
+			throw new UnsupportedMethodException();
+		}
+		
+		public function toColumnType()
+		{
+			return
+				MetaConfiguration::me()->
+					getClassByName($this->class)->
+						getIdentifier()->
+							getType()->toColumnType();
+		}
+	}
+?>
