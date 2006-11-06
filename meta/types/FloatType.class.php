1cf22ebd2dc41eca598d085445859fbee42697d9
Konstantin V. Arkhipov <voxus@onphp.org>
2006-11-06 15:15:31 +0000

    * non-required ranges fixed
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@2151 db88c99c-74ec-0310-8720-8416fdbde762

--- meta/types/FloatType.class.php
+++ meta/types/FloatType.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2006 by Nickolay Korolyov                               *
+ *   Copyright (C) 2006 by Nickolay G. Korolyov                            *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
