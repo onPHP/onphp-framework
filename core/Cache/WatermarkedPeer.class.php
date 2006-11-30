040ef10125e1a20444dcca935f1e7cbbdec84963
Konstantin V. Arkhipov <voxus@onphp.org>
2006-11-30 17:32:47 +0000

    * r2218 reverted
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.2@2219 db88c99c-74ec-0310-8720-8416fdbde762

--- core/Cache/WatermarkedPeer.class.php
+++ core/Cache/WatermarkedPeer.class.php
@@ -76,7 +76,7 @@
 		}
 		
 		protected function store(
-			$action, $key, $value, $expires = Cache::EXPIRES_MEDIUM
+			$action, $key, &$value, $expires = Cache::EXPIRES_MEDIUM
 		)
 		{
 			throw new UnsupportedMethodException();
