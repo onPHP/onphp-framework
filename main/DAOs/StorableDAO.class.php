fd6acc73ca95b5e171ef9181df14b455cd0a3c6a
Konstantin V. Arkhipov <voxus@onphp.org>
2006-12-24 18:00:45 +0000

    * path's processing logic extracted
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@2318 db88c99c-74ec-0310-8720-8416fdbde762

--- main/DAOs/StorableDAO.class.php
+++ main/DAOs/StorableDAO.class.php
@@ -66,111 +66,9 @@
 			if ($atom instanceof Property)
 				return $this->mapProperty($atom, $query);
 			elseif (is_string($atom)) {
-				if (strpos($atom, '.') !== false) {
-					$path = explode('.', $atom);
-					
-					$property = $path[0];
-					unset($path[0]);
-					
-					// prevents useless joins
-					if (
-						isset($path[1])
-						&& ($path[1] == 'id')
-						&& (count($path) == 1)
-					) {
-						$onlyId = true;
-					} else {
-						$onlyId = false;
-					}
-					
-					$className = $this->getClassFor($property);
-					
-					if (is_array($className)) { // container
-						$containerName = $className[0];
-						$objectName = $className[1];
-						
-						$table =
-							call_user_func(
-								array($containerName, 'getHelperTable')
-							);
-						
-						$dao = call_user_func(array($objectName, 'dao'));
-						
-						$query->
-							join(
-								$table,
-								
-								Expression::eq(
-									DBField::create(
-										$this->getIdName(),
-										$this->getTable()
-									),
-									
-									DBField::create(
-										call_user_func(
-											array($containerName, 'getParentIdField')
-										),
-										$table
-									)
-								)
-							);
-						
-						if ($onlyId)
-							return
-								DBField::create(
-									call_user_func(
-										array($containerName, 'getChildIdField')
-									),
-									$table
-								);
-						else
-							$query->join(
-								$dao->getTable(),
-								
-								Expression::eq(
-									DBField::create(
-										$dao->getIdName(),
-										$dao->getTable()
-									),
-									
-									DBField::create(
-										call_user_func(
-											array($containerName, 'getChildIdField')
-										),
-										$table
-									)
-								)
-							);
-					} else {
-						if ($onlyId)
-							return
-								new DBField(
-									$this->getFieldFor($property),
-									$this->getTable()
-								);
-
-						$dao = call_user_func(array($className, 'dao'));
-						
-						$query->
-							join(
-								$dao->getTable(),
-								
-								Expression::eq(
-									DBField::create(
-										$this->getFieldFor($property),
-										$this->getTable()
-									),
-									
-									DBField::create(
-										$dao->getIdName(),
-										$dao->getTable()
-									)
-								)
-							);
-					}
-					
-					return $dao->guessAtom(implode('.', $path), $query);
-				} elseif (array_key_exists($atom, $this->mapping))
+				if (strpos($atom, '.') !== false)
+					return $this->processPath($atom, $query);
+				elseif (array_key_exists($atom, $this->mapping))
 					return $this->mapProperty(new Property($atom), $query);
 			} elseif ($atom instanceof LogicalObject)
 				return $atom->toMapped($this, $query);
@@ -178,6 +76,113 @@
 				return $atom;
 			
 			return new DBValue($atom);
+		}
+		
+		protected function processPath($path, JoinCapableQuery $query)
+		{
+			$path = explode('.', $path);
+			
+			$property = $path[0];
+			unset($path[0]);
+			
+			// prevents useless joins
+			if (
+				isset($path[1])
+				&& ($path[1] == 'id')
+				&& (count($path) == 1)
+			) {
+				$onlyId = true;
+			} else {
+				$onlyId = false;
+			}
+			
+			$className = $this->getClassFor($property);
+			
+			if (is_array($className)) { // container
+				$containerName = $className[0];
+				$objectName = $className[1];
+				
+				$table =
+					call_user_func(
+						array($containerName, 'getHelperTable')
+					);
+				
+				$dao = call_user_func(array($objectName, 'dao'));
+				
+				$query->
+					join(
+						$table,
+						
+						Expression::eq(
+							DBField::create(
+								$this->getIdName(),
+								$this->getTable()
+							),
+							
+							DBField::create(
+								call_user_func(
+									array($containerName, 'getParentIdField')
+								),
+								$table
+							)
+						)
+					);
+				
+				if ($onlyId)
+					return
+						DBField::create(
+							call_user_func(
+								array($containerName, 'getChildIdField')
+							),
+							$table
+						);
+				else
+					$query->join(
+						$dao->getTable(),
+						
+						Expression::eq(
+							DBField::create(
+								$dao->getIdName(),
+								$dao->getTable()
+							),
+							
+							DBField::create(
+								call_user_func(
+									array($containerName, 'getChildIdField')
+								),
+								$table
+							)
+						)
+					);
+			} else {
+				if ($onlyId)
+					return
+						new DBField(
+							$this->getFieldFor($property),
+							$this->getTable()
+						);
+
+				$dao = call_user_func(array($className, 'dao'));
+				
+				$query->
+					join(
+						$dao->getTable(),
+						
+						Expression::eq(
+							DBField::create(
+								$this->getFieldFor($property),
+								$this->getTable()
+							),
+							
+							DBField::create(
+								$dao->getIdName(),
+								$dao->getTable()
+							)
+						)
+					);
+			}
+			
+			return $dao->guessAtom(implode('.', $path), $query);
 		}
 		
 		protected function mapProperty(Property $property, JoinCapableQuery $query)
