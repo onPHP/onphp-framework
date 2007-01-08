a0188c906ac225eee4038342c735288d517d1923
Konstantin V. Arkhipov <voxus@onphp.org>
2007-01-08 08:16:44 +0000

    * 0.2.18, 0.4.11, 0.6.4, 0.8.4
    git-svn-id: https://svn.shadanakar.org/onPHP/tags/0.4.11@2399 db88c99c-74ec-0310-8720-8416fdbde762

---
+++ core/Form/ExplodedPrimitive.class.php
@@ -0,0 +1,43 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2005 by Sveta Smirnova                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+***************************************************************************/
+/* $Id$ */
+
+	final class ExplodedPrimitive extends PrimitiveString
+	{
+		protected $separator;
+		
+		public function setSeparator($separator)
+		{
+			$this->separator = $separator;
+			
+			return $this;
+		}
+		
+		public function getSeparator()
+		{
+			return $this->separator;
+		}
+		
+		public function import(&$scope)
+		{
+			if (!$temp = parent::import($scope))
+				return $temp;
+	
+			if ($this->value = explode($this->separator, $this->value)) {
+				return true;
+			} else {
+				return false;
+			}
+			
+			/* NOTREACHED */
+		}
+	}
+?>
---
+++ core/Form/PrimitiveRange.class.php
@@ -0,0 +1,128 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2004-2005 by Konstantin V. Arkhipov                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id$ */
+
+	/**
+	 * @ingroup Primitives
+	**/
+	class PrimitiveRange extends ComplexPrimitive
+	{
+		const MIN	= 'min';
+		const MAX	= 'max';
+
+		/// @deprecated and dropped in 0.6
+		public function setSwapAllowed()
+		{
+			throw new UnsupportedMethodException();
+		}
+
+		/// @deprecated and dropped in 0.6
+		public function setSwapDisallowed()
+		{
+			throw new UnsupportedMethodException();
+		}
+
+		// to be E_STRICT compatible
+		public function setValue(/* Range */ $range)
+		{
+			Assert::isTrue(
+				$range instanceof Range,
+				'only ranges accepted today'
+			);
+
+			$this->value = $range;
+
+			return $this;
+		}
+
+		public function importSingle(&$scope)
+		{
+			if (!BasePrimitive::import($scope) || is_array($scope[$this->name]))
+				return null;
+
+			if (isset($scope[$this->name]) && is_string($scope[$this->name])) {
+				$arr = explode('-', $scope[$this->name], 2);
+
+				$range =
+					Range::lazyCreate(
+						ArrayUtils::getArrayVar($arr, 0),
+						ArrayUtils::getArrayVar($arr, 1)
+					);
+
+				if ($range &&
+					!(
+						($this->min && $range->getMin()) &&
+						$range->getMin() < $this->min
+					) &&
+					!(
+						($this->max && $range->getMax()) &&
+						$range->getMax() > $this->max
+					)
+				) {
+					$this->value = $range;
+
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		public function importMarried(&$scope) // ;-)
+		{
+			$name = &$this->name;
+
+			if (
+				($this->safeGet($scope, $name, self::MIN) === null) &&
+				($this->safeGet($scope, $name, self::MAX) === null)
+			)
+				return null;
+
+			$range =
+				Range::lazyCreate(
+					$this->safeGet($scope, $name, self::MIN),
+					$this->safeGet($scope, $name, self::MAX)
+				);
+
+			if ($range &&
+					!(
+						($this->min && $range->getMin()) &&
+						$range->getMin() < $this->min
+					) &&
+					!(
+						($this->max && $range->getMax()) &&
+						$range->getMax() > $this->max
+					)
+			) {
+				$this->value = $range;
+
+				return true;
+			}
+
+			return false;
+		}
+
+		private function safeGet(&$scope, $firstDimension, $secondDimension)
+		{
+			if (isset($scope[$firstDimension]) && is_array($scope[$firstDimension])) {
+				if (
+					isset($scope[$firstDimension][$secondDimension])
+					&& is_array($scope[$firstDimension])
+					&& !empty($scope[$firstDimension][$secondDimension])
+				) {
+					return $scope[$firstDimension][$secondDimension];
+				}
+			}
+
+			return null;
+		}
+	}
+?>
