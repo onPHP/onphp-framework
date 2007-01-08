3182a4329adcbd7fbb9ec0c6cfcd69f9187c0b6b
Konstantin V. Arkhipov <voxus@onphp.org>
2007-01-08 08:16:44 +0000

    * 0.2.18, 0.4.11, 0.6.4, 0.8.4
    git-svn-id: https://svn.shadanakar.org/onPHP/tags/0.2.18@2399 db88c99c-74ec-0310-8720-8416fdbde762

---
+++ core/Form/DateRangeList.class.php
@@ -0,0 +1,191 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2005 by Konstantin V. Arkhipov, Igor V. Gulyaev         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id$ */
+
+	/**
+	 * @ingroup Primitives
+	**/
+	class DateRangeList extends BasePrimitive
+	{
+		public function import(&$scope)
+		{
+			if (
+				!isset($scope[$this->name])
+				|| !is_array($scope[$this->name])
+				|| empty($scope[$this->name])
+				||
+					(
+						count($scope[$this->name]) == 1
+						&& !current($scope[$this->name])
+					)
+			)
+				return null;
+				
+			$array = $scope[$this->name];
+			$list = array();
+
+			foreach ($array as $string)
+				if (array() !== self::stringToDateRangeList($string))
+					foreach (self::stringToDateRangeList($string) as $range)
+						$list[] = $range;
+			
+			$this->value = $list;
+
+			return ($this->value !== array());
+		}
+		
+		public function toString()
+		{
+			if ($this->value) {
+				$out = array();
+				
+				foreach ($this->value as $range)
+					$out[] = $range->toDateString();
+					
+				return implode(', ', $out);
+			}
+			
+			return null;
+		}
+		
+		public static function stringToDateRangeList($string)
+		{
+			$list = array();
+			
+			if (!empty($string)) {
+				if (strpos($string, ',') !== false)
+					$dates = explode(',', $string);
+				else
+					$dates = array($string);
+				
+				foreach ($dates as $date) {
+					try {
+						$list[] = self::makeRange($date);
+					} catch (WrongArgumentException $e) {
+						// ignore?
+					}
+				}
+			}
+			
+			return $list;
+		}
+		
+		public static function printRange(DateRange $range, $delimiter = '-')
+		{
+			$rangeString = null;
+			
+			if ($range->getStart())
+				$rangeString .= $range->getStart()->toDate($delimiter);
+				
+			$rangeString .= ' - ';
+			
+			if ($range->getEnd())
+				$rangeString .= $range->getEnd()->toDate($delimiter);
+				
+			return $rangeString;
+		}
+		
+		public static function makeRange($string)
+		{
+			if (
+				(substr_count($string, ' - ') === 1)
+				|| (substr_count($string, '-'))
+			) {
+				$delimiter = ' - ';
+				
+				if (substr_count($string, '-') === 1)
+					$delimiter = '-';
+				
+				list($start, $finish) = explode($delimiter, $string, 2);
+				
+				$start = self::toDate(trim($start));
+				$finish = self::toDate(trim($finish));
+				
+				if ($start || $finish) {
+					
+					$range = new DateRange();
+					
+					$range =
+						DateRange::create()->
+						lazySet($start, $finish);
+					
+					return $range;
+					
+				} elseif ($string == ' - ')
+					return DateRange::create();
+				
+			} elseif ($single = self::toDate(trim($string)))
+				return
+					DateRange::create()->
+					setStart($single)->
+					setEnd($single);
+			
+			throw new WrongArgumentException(
+				"unknown string format '{$string}'"
+			);
+		}
+		
+		private static function toDate($date)
+		{
+			if (strpos($date, '.') !== false) {
+				
+				$dots = substr_count($date, '.');
+				
+				$year = null;
+				
+				if ($dots == 2) {
+					list($day, $month, $year) = explode('.', $date, ($dots + 1));
+					
+					if (strlen($day) > 2) {
+						$tmp = $year;
+						$year = $day;
+						$day = $tmp;
+					}
+				} else
+					list($day, $month) = explode('.', $date, ($dots + 1));
+				
+				if (strlen($day) == 1)
+					$day = "0{$day}";
+				
+				if ($month === null)
+					$month = date('m');
+				elseif (strlen($month) == 1)
+					$month = "0{$month}";
+				
+				if ($year === null)
+					$year = date('Y');
+				// we're all die in 2100+ anyway
+				elseif (strlen($year) === 2)
+					$year = "20{$year}";
+				
+				$date = $year.$month.$day;
+			}
+			
+			$lenght = strlen($date);
+			
+			try {
+				if ($lenght > 4)
+					return new Timestamp(strtotime($date));
+				elseif ($lenght === 4) {
+					$date = substr($date, 2).'-'.substr($date, 0, 2);
+					
+					return new Timestamp(strtotime(date('Y-').$date));
+				} elseif (($lenght == 2) || ($lenght == 1)) {
+					return new Timestamp(strtotime(date('Y-m-').$date));
+				}
+			} catch (WrongArgumentException $e) {
+				// seems to be unparseable
+			}
+			
+			return null;
+		}
+	}
+?>
---
+++ core/Form/FilterChain.class.php
@@ -0,0 +1,47 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2005 by Anton E. Lebedevich                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id$ */
+
+	/**
+	 * Chained Filtrator.
+	 * 
+	 * @ingroup Form
+	**/
+	class FilterChain implements Filtrator
+	{
+		protected $chain = array();
+
+		public static function create()
+		{
+			return new self;
+		}
+		
+		public function add(Filtrator $filter)
+		{
+			$this->chain[] = $filter;
+			return $this;
+		}
+
+		public function dropAll()
+		{
+			$this->chain = array();
+			return $this;
+		}
+
+		public function apply($value)
+		{
+			foreach ($this->chain as &$filter)
+				$value = $filter->apply($value);
+
+			return $value;
+		}
+	}
+?>
