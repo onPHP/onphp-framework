5b1bd0452155f35769ff0f306e7b55941023fc22
Konstantin V. Arkhipov <voxus@onphp.org>
2007-01-21 19:12:26 +0000

    * more sanity
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.6@2475 db88c99c-74ec-0310-8720-8416fdbde762

--- core/Cache/DirectoryLocker.class.php
+++ core/Cache/DirectoryLocker.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2006 by Konstantin V. Arkhipov                          *
+ *   Copyright (C) 2006-2007 by Konstantin V. Arkhipov                     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -54,7 +54,7 @@
 		public function free($key)
 		{
 			try {
-				rmdir($this->directory.$key);
+				return rmdir($this->directory.$key);
 			} catch (BaseException $e) {
 				return false;
 			}
