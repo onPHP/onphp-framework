350dde28db0bb45fea4f39821b14b6c8dffa4fe4
Konstantin V. Arkhipov <voxus@onphp.org>
2007-01-25 12:28:04 +0000

    * typo
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@2511 db88c99c-74ec-0310-8720-8416fdbde762

--- main/Flow/DropCommand.class.php
+++ main/Flow/DropCommand.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2006 by Anton E. Lebedevich                             *
+ *   Copyright (C) 2006-2007 by Anton E. Lebedevich                        *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -38,7 +38,7 @@
 					
 					return
 						ModelAndView::create()->
-						setView('success');
+						setView(BaseEditor::COMMAND_SUCCEEDED);
 						
 				} catch (ObjectNotFoundException $e) {
 					$form->markMissing('id');
