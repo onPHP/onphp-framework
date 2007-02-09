ce6142046d30952ce0d7b8afd1ec5bbc78ce765f
Konstantin V. Arkhipov <voxus@onphp.org>
2007-02-09 12:54:00 +0000

    * r2577's forwardport
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@2578 db88c99c-74ec-0310-8720-8416fdbde762

--- meta/builders/AutoDaoBuilder.class.php
+++ meta/builders/AutoDaoBuilder.class.php
@@ -67,8 +67,11 @@ EOT;
 
 				$out .= self::buildPointers($class);
 				
-				if ($class->getParent()) {
-					$typeHint = $class->getFinalParent()->getName();
+				if (
+					$parent = $class->getParent()
+					&& !($parent->getPattern() instanceof AbstractClassPattern)
+				) {
+					$typeHint = $parent->getFinalParent()->getName();
 				} else {
 					$typeHint = '/* '.$class->getName().' */';
 				}
