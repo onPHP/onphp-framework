023e6ac6c0974da45993bac769f52a4d345d14b8
Konstantin V. Arkhipov <voxus@onphp.org>
2007-03-07 14:46:47 +0000

    * cosmetics
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@2762 db88c99c-74ec-0310-8720-8416fdbde762

--- main/Utils/ArrayUtils.class.php
+++ main/Utils/ArrayUtils.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2004-2005 by Konstantin V. Arkhipov                     *
+ *   Copyright (C) 2004-2007 by Konstantin V. Arkhipov                     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -82,10 +82,8 @@
 			$result = array();
 
 			foreach ($array as $value) {
-
 				Assert::isTrue(
-					is_integer($value)
-					|| is_string($value),
+					is_integer($value) || is_string($value),
 					'only integer or string values accepted'
 				);
 
