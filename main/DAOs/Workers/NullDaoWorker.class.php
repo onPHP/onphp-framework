e6fdcff63b53e9451dccbafbd4b1b36350e3fd9b
Konstantin V. Arkhipov <voxus@onphp.org>
2007-03-11 14:50:17 +0000

    * typo
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@2791 db88c99c-74ec-0310-8720-8416fdbde762

--- main/DAOs/Workers/NullDaoWorker.class.php
+++ main/DAOs/Workers/NullDaoWorker.class.php
@@ -173,7 +173,7 @@
 			$count = clone $query;
 			
 			$count =
-				$db->queryRow(
+				DBPool::getByDao($this->dao)->queryRow(
 					$count->dropFields()->dropOrder()->limit(null, null)->
 					get(SQLFunction::create('COUNT', '*')->setAlias('count'))
 				);
