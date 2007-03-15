c992fc9c217b264bf965fe7d116c2e4aae9c94a1
Dmitry E. Pismenny <lifter@oemdesign.ru>
2007-03-15 15:58:08 +0000

    - fixed many additional expression for UnifedContainer which used    criteria
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@2836 db88c99c-74ec-0310-8720-8416fdbde762

--- main/UnifiedContainer/OneToManyLinkedWorker.class.php
+++ main/UnifiedContainer/OneToManyLinkedWorker.class.php
@@ -21,7 +21,7 @@
 		protected function targetize(SelectQuery $query)
 		{
 			return
-				$query->where(
+				$query->andWhere(
 					Expression::eqId(
 						new DBField($this->container->getParentIdField()),
 						$this->container->getParentObject()
