4ce5cf69345ad6cc5c5447521d977f28389c8542
Anton E. Lebedevich <antony@onphp.org>
2007-03-20 10:57:16 +0000

    cosmetics
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@2867 db88c99c-74ec-0310-8720-8416fdbde762

--- core/Form/Primitives/PrimitiveIdentifier.class.php
+++ core/Form/Primitives/PrimitiveIdentifier.class.php
@@ -54,8 +54,11 @@
 		{
 			if ($value instanceof Identifiable) {
 				Assert::isTrue(
-					($this->info && $this->info->isInstance($value)) || 
-					is_subclass_of($value, $this->className)
+					(
+						$this->info 
+						&& $this->info->isInstance($value)
+					) 
+					|| is_subclass_of($value, $this->className)
 				);
 				
 				return
