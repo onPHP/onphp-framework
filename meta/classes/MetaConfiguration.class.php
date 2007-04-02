00640a63b528b1c9edb0742852f03bb50364bf83
Anton E. Lebedevich <antony@onphp.org>
2007-04-02 09:29:22 +0000

    r2972 reverted
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@2974 db88c99c-74ec-0310-8720-8416fdbde762

--- meta/classes/MetaConfiguration.class.php
+++ meta/classes/MetaConfiguration.class.php
@@ -910,7 +910,6 @@
 				)
 				&& (!$class->getPattern() instanceof ValueObjectPattern)
 				&& (!$class->getPattern() instanceof InternalClassPattern)
-				&& (!$class->getPattern() instanceof SpookedClassPattern)
 			) {
 				Assert::isTrue(
 					$class->getIdentifier() !== null,
