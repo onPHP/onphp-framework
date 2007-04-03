7ea0840289c4cb3cce2069484b2b1e80b2321232
Konstantin V. Arkhipov <voxus@onphp.org>
2007-04-03 17:11:37 +0000

    * StringType finalized
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@2995 db88c99c-74ec-0310-8720-8416fdbde762

--- meta/types/StringType.class.php
+++ meta/types/StringType.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2006 by Konstantin V. Arkhipov                          *
+ *   Copyright (C) 2006-2007 by Konstantin V. Arkhipov                     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -13,7 +13,7 @@
 	/**
 	 * @ingroup Types
 	**/
-	class StringType extends BasePropertyType
+	final class StringType extends BasePropertyType
 	{
 		/**
 		 * @throws WrongArgumentException
