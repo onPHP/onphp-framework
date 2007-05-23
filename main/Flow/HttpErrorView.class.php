a964b59d3fcf94df5619590d1700eda1ccc8c47b
Anton E. Lebedevich <antony@onphp.org>
2007-05-23 11:07:40 +0000

    3352:3354 merged from branch 0.10
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@3355 db88c99c-74ec-0310-8720-8416fdbde762

--- main/Flow/HttpErrorView.class.php
+++ main/Flow/HttpErrorView.class.php
@@ -15,53 +15,23 @@
 	**/
 	class HttpErrorView implements View
 	{
+		protected $status	= null;
+		
 		protected $prefix	= null;
-		protected $code		= null;
 		protected $postfix	= null;
 		
-		public function __construct($prefix, $code, $postfix)
+		public function __construct(HttpStatus $status, $prefix, $postfix)
 		{
+			$this->status = $status;
+			
 			$this->prefix = $prefix;
-			$this->code = $code;
 			$this->postfix = $postfix;
 		}
 		
-		public function render($model = null)
+		/* void */ public function render($model = null)
 		{
-			$messages = array(
-				400 => 'Bad Request',
-				401 => 'Unauthorized',
-				402 => 'Payment Required',
-				403 => 'Forbidden',
-				404 => 'Not Found',
-				405 => 'Method Not Allowed',
-				406 => 'Not Acceptable',
-				407 => 'Proxy Authentication Required',
-				408 => 'Request Timeout',
-				409 => 'Conflict',
-				410 => 'Gone',
-				411 => 'Length Required',
-				412 => 'Precondition Failed',
-				413 => 'Request Entity Too Large',
-				414 => 'Request-URI Too Large',
-				415 => 'Unsupported Media Type',
-				416 => 'Requested Range Not Satisfiable',
-				417 => 'Expectation Failed',
-				500 => 'Internal Server Error',
-				501 => 'Not Implemented',
-				502 => 'Bad Gateway',
-				503 => 'Service Unavailable',
-				504 => 'Gateway Timeout',
-				505 => 'HTTP Version not supported'
-			);
-			
-			if (isset($messages[$this->code]))
-				$message = $messages[$this->code];
-			else
-				$message = null;
-
-			header('HTTP/1.0 '.$this->code.' '.$message);
-			include $this->prefix.$this->code.$this->postfix;
+			header($this->status->toString());
+			include $this->prefix.$this->status->getId().$this->postfix;
 		}
 	}
 ?>
