8f243bf4a73c484cb20fa500cbde72a9e54c6189
Anton E. Lebedevich <antony@onphp.org>
2007-05-23 10:25:34 +0000

    code dublication removed
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.10@3353 db88c99c-74ec-0310-8720-8416fdbde762

--- main/Flow/HttpErrorView.class.php
+++ main/Flow/HttpErrorView.class.php
@@ -28,39 +28,8 @@
 		
 		public function render($model = null)
 		{
-			$messages = array(
-				400 => 'Bad Request',
-				401 => 'Unauthorized',
-				402 => 'Payment Required',
-				403 => 'Forbidden',
-				404 => 'Not Found',
-				405 => 'Method Not Allowed',
-				406 => 'Not Acceptable',
-				407 => 'Proxy Authentication Required',
-				408 => 'Request Timeout',
-				409 => 'Conflict',
-				410 => 'Gone',
-				411 => 'Length Required',
-				412 => 'Precondition Failed',
-				413 => 'Request Entity Too Large',
-				414 => 'Request-URI Too Large',
-				415 => 'Unsupported Media Type',
-				416 => 'Requested Range Not Satisfiable',
-				417 => 'Expectation Failed',
-				500 => 'Internal Server Error',
-				501 => 'Not Implemented',
-				502 => 'Bad Gateway',
-				503 => 'Service Unavailable',
-				504 => 'Gateway Timeout',
-				505 => 'HTTP Version not supported'
-			);
-			
-			if (isset($messages[$this->code]))
-				$message = $messages[$this->code];
-			else
-				$message = null;
-
-			header('HTTP/1.0 '.$this->code.' '.$message);
+			$status = new HttpStatus($this->code);
+			header($status->toString());
 			include $this->prefix.$this->code.$this->postfix;
 		}
 	}
