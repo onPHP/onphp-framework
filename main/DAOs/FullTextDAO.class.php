2c3c769d5c6cb14e401354dc01add29466627a41
Konstantin V. Arkhipov <voxus@onphp.org>
2007-06-04 10:55:27 +0000

    - quasi-sane limitation
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.6@3402 db88c99c-74ec-0310-8720-8416fdbde762

--- main/DAOs/FullTextDAO.class.php
+++ main/DAOs/FullTextDAO.class.php
@@ -70,15 +70,10 @@
 		
 		public static function prepareSearchString($string)
 		{
-			$array =
-				explode(
-					' ',
-					$string,
-					substr_count($string, ' ') + 1
-				);
+			$array = explode(' ', $string);
 			
 			$out = array();
-
+			
 			for ($i = 0, $size = count($array); $i < $size; ++$i)
 				if (!empty($array[$i]))
 					if ($element = mb_ereg_replace(
