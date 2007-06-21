5ed57905582a965ac7b1c8f2dbdac0b3fcc59b00
Konstantin V. Arkhipov <voxus@onphp.org>
2007-06-21 12:25:32 +0000

    * typos
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.10@3537 db88c99c-74ec-0310-8720-8416fdbde762

--- main/DAOs/GenericDAO.class.php
+++ main/DAOs/GenericDAO.class.php
@@ -155,8 +155,8 @@
 			$mapped = $remain = array();
 			
 			foreach ($ids as $id) {
-				if (isset($this->identityMap[$ids[$i]])) {
-					$mapped[] = $this->identityMap[$ids[$i]];
+				if (isset($this->identityMap[$ids[$id]])) {
+					$mapped[] = $this->identityMap[$ids[$id]];
 				} else {
 					$remain[] = $id;
 				}
