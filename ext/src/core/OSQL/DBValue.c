bf6ab8111b7325dfb62dda476cddb067dfdb5527
Konstantin V. Arkhipov <voxus@onphp.org>
2007-06-29 12:11:32 +0000

    * cosmetics, simplifications
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@3638 db88c99c-74ec-0310-8720-8416fdbde762

--- ext/src/core/OSQL/DBValue.c
+++ ext/src/core/OSQL/DBValue.c
@@ -80,25 +80,19 @@ ONPHP_METHOD(DBValue, toDialectString)
 	cast = ONPHP_READ_PROPERTY(getThis(), "cast");
 	
 	if (Z_STRLEN_P(cast)) {
-		zval *casted;
-		
 		zend_call_method_with_2_params(
 			&dialect,
 			Z_OBJCE_P(dialect),
 			NULL,
 			"tocasted",
-			&casted,
+			&out,
 			out,
 			cast
 		);
 		
-		ZVAL_FREE(out);
-		
 		if (EG(exception)) {
 			return;
 		}
-		
-		RETURN_ZVAL(casted, 1, 1);
 	} else {
 		// nothing
 	}
