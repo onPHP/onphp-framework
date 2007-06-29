6dae64a059d0a3d8586d2eabb5352c1a25a45930
Anton E. Lebedevich <antony@onphp.org>
2007-06-29 13:48:38 +0000

    normalizeUri added
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.10@3641 db88c99c-74ec-0310-8720-8416fdbde762

--- test/main/TextUtilsTest.class.php
+++ test/main/TextUtilsTest.class.php
@@ -21,5 +21,105 @@
 				TextUtils::friendlyFileSize(2 * pow(1024, 6)), '2048'.$units[5]
 			);
 		}
+		
+		public function testNormalizeUri()
+		{
+			$this->assertEqual(
+				TextUtils::normalizeUri('http://example.com/'),
+				'http://example.com/'
+			);
+
+			$this->assertEqual(
+				TextUtils::normalizeUri('http://example.com'),
+				'http://example.com/'
+			);
+
+			$this->assertEqual(
+				TextUtils::normalizeUri('http://example.com:/'),
+				'http://example.com/'
+			);
+
+			$this->assertEqual(
+				TextUtils::normalizeUri('http://example.com:80/'),
+				'http://example.com/'
+			);
+
+			$this->assertEqual(
+				TextUtils::normalizeUri('http://wWw.exaMPLE.COm/'),
+				'http://www.example.com/'
+			);
+
+			$this->assertEqual(
+				TextUtils::normalizeUri('htTP://example.com/'),
+				'http://example.com/'
+			);
+
+			$this->assertEqual(
+				TextUtils::normalizeUri('http://example.com/foo%2cbar'),
+				'http://example.com/foo%2Cbar'
+			);
+
+			$this->assertEqual(
+				TextUtils::normalizeUri('http://example.com/foo%2Dbar%2dbaz'),
+				'http://example.com/foo-bar-baz'
+			);
+
+			$this->assertEqual(
+				TextUtils::normalizeUri('http://example.com/a/b/c/./../../g'),
+				'http://example.com/a/g'
+			);
+
+			$this->assertEqual(
+				TextUtils::normalizeUri('http://example.com/mid/content=5/../6'),
+				'http://example.com/mid/6'
+			);
+
+			$this->assertEqual(
+				TextUtils::normalizeUri('http://example.com/a/./b'),
+				'http://example.com/a/b'
+			);
+
+			$this->assertEqual(
+				TextUtils::normalizeUri('http://example.com/a/../b'),
+				'http://example.com/b'
+			);
+
+			$this->assertEqual(
+				TextUtils::normalizeUri('http://example.com/../b'),
+				'http://example.com/b'
+			);
+
+			$this->assertEqual(
+				TextUtils::normalizeUri('http://example.com/a/.'),
+				'http://example.com/a/'
+			);
+
+			$this->assertEqual(
+				TextUtils::normalizeUri('http://example.com/a/..'),
+				'http://example.com/'
+			);
+
+			$this->assertEqual(
+				TextUtils::normalizeUri('http://example.com/a/./'),
+				'http://example.com/a/'
+			);
+
+			$this->assertEqual(
+				TextUtils::normalizeUri('http://example.com/a/../'),
+				'http://example.com/'
+			);
+
+			$this->assertEqual(
+				TextUtils::normalizeUri('hTTPS://a/./b/../b/%63/%7bfoo%7d'),
+				'https://a/b/c/%7Bfoo%7D'
+			);
+			
+			try {
+				TextUtils::normalizeUri('http:/foo');
+				$this->fail();
+			} catch (WrongArgumentException $e) {
+				$this->pass();
+			}
+		}
 	}
 ?>
