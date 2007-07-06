0d4ab77ee237919e5ea5d80671ab1a0deb7eb21f
Konstantin V. Arkhipov <voxus@onphp.org>
2007-07-06 08:51:03 +0000

    + forgotten check added
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.6@3721 db88c99c-74ec-0310-8720-8416fdbde762

--- main/DAOs/Workers/TransparentDaoWorker.class.php
+++ main/DAOs/Workers/TransparentDaoWorker.class.php
@@ -135,7 +135,10 @@
 			$toFetch = array();
 			
 			foreach ($ids as $id) {
-				if (!$cached = $this->getCachedById($id)) {
+				if (
+					!($cached = $this->getCachedById($id))
+					|| ($cached === Cache::NOT_FOUND)
+				) {
 					$toFetch[] = $id;
 				} else {
 					$list[] = $cached;
