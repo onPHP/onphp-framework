dfa6a1a51c7924512bbc3faba4f37e3031b3a3b9
Dmitry A. Lomash <groomycian@oemdesign.ru>
2007-07-24 09:11:15 +0000

    up toISOString() from Timestamp to Date
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.10@3866 db88c99c-74ec-0310-8720-8416fdbde762

--- core/Base/Timestamp.class.php
+++ core/Base/Timestamp.class.php
@@ -102,17 +102,6 @@
 				return parent::getDayStartStamp();
 		}
 		
-		/**
-		 * ISO 8601 time string
-		**/
-		public function toISOString($convertToUtc = true)
-		{
-			if ($convertToUtc)
-				return date('Y-m-d\TH:i:s\Z', $this->int - date('Z', $this->int));
-			else
-				return date('Y-m-d\TH:i:sO', $this->int);
-		}
-		
 		protected static function getFormat()
 		{
 			return 'Y-m-d H:i:s';
