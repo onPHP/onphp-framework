16a8eec383fcb142a38fc3e836f2e4390f7e5dc8
Konstantin V. Arkhipov <voxus@onphp.org>
2007-07-27 11:25:10 +0000

    [GPL-2+ -> GPL-3+] branches/0.4
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.4@3921 db88c99c-74ec-0310-8720-8416fdbde762

--- doc/examples/union.php
+++ doc/examples/union.php
@@ -2,10 +2,10 @@
 /***************************************************************************
  *   Copyright (C) 2005-2006 by Sergey S. Sergeev                          *
  *   webs.support@gmail.com                                                *
- *   	                                                                   *
+ *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
+ *   the Free Software Foundation; either version 3 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
