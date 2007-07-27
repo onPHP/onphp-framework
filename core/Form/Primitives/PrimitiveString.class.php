2494331bfef6df2b84d8e9e79ef04adf7605718f
Konstantin V. Arkhipov <voxus@onphp.org>
2007-07-27 11:25:55 +0000

    * name fixed
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.10@3923 db88c99c-74ec-0310-8720-8416fdbde762

--- core/Form/Primitives/PrimitiveString.class.php
+++ core/Form/Primitives/PrimitiveString.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2004-2007 by Konstantin V. Arkhipov, Sveta Smirnova     *
+ *   Copyright (C) 2004-2007 by Konstantin V. Arkhipov, Sveta A. Smirnova  *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Lesser General Public License as        *
