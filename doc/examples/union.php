444f0b2337f0f590418c8693e9d390d7da1777ef
Konstantin V. Arkhipov <voxus@onphp.org>
2007-07-27 11:23:45 +0000

    * cosmetics
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@3919 db88c99c-74ec-0310-8720-8416fdbde762

--- doc/examples/union.php
+++ doc/examples/union.php
@@ -1,7 +1,7 @@
 <?php
 /***************************************************************************
  *   Copyright (C) 2005-2007 by Sergey S. Sergeev                          *
- *   	                                                                   *
+ *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Lesser General Public License as        *
  *   published by the Free Software Foundation; either version 3 of the    *
