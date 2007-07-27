90550275604551ca7cd23d2c1c74c3d54d872488
Konstantin V. Arkhipov <voxus@onphp.org>
2007-07-27 11:22:33 +0000

    [GPL-2+ -> LGPL-3+] branches/0.10/main/Utils
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.10@3913 db88c99c-74ec-0310-8720-8416fdbde762

--- main/Utils/TextUtils.class.php
+++ main/Utils/TextUtils.class.php
@@ -3,9 +3,9 @@
  *   Copyright (C) 2006-2007 by Anton E. Lebedevich                        *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
  *                                                                         *
  *    UrlSaveBase64* functions borrowed from comments on                   *
  *    http://www.php.net/manual/en/function.base64-encode.php              *
