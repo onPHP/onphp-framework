b0d5a0784391d79980bc95e55200909e55b3e79f
Konstantin V. Arkhipov <voxus@onphp.org>
2007-07-27 11:21:13 +0000

    [GPL-2+ -> LGPL-3+] trunk/main/Utils
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@3894 db88c99c-74ec-0310-8720-8416fdbde762

--- main/Utils/Logging/LogRecord.class.php
+++ main/Utils/Logging/LogRecord.class.php
@@ -1,11 +1,11 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2007 by Ivan Khvostishkov, Denis Gabaidulin             *
+ *   Copyright (C) 2007 by Ivan Y, Khvostishkov, Denis M. Gabaidulin       *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
  *                                                                         *
  ***************************************************************************/
 /* $Id$ */
