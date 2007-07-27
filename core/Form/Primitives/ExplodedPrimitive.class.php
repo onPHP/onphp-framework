1a306d8101ada6e4336dbac2e53bb6bd9695ead5
Konstantin V. Arkhipov <voxus@onphp.org>
2007-07-27 11:24:26 +0000

    [GPL-2+ -> GPL-3+] branches/0.6
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.6@3920 db88c99c-74ec-0310-8720-8416fdbde762

--- core/Form/Primitives/ExplodedPrimitive.class.php
+++ core/Form/Primitives/ExplodedPrimitive.class.php
@@ -1,13 +1,13 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2005-2006 by Sveta Smirnova                             *
+ *   Copyright (C) 2005-2007 by Sveta A. Smirnova                          *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
+ *   the Free Software Foundation; either version 3 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
-***************************************************************************/
+ ***************************************************************************/
 /* $Id$ */
 
 	/**
