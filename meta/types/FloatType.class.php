8a6e29bb87fd8ad42367fdf202d2444c6906348e
Konstantin V. Arkhipov <voxus@onphp.org>
2007-07-27 11:22:41 +0000

    [GPL-2+ -> LGPL-3+] branches/0.10/meta
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.10@3914 db88c99c-74ec-0310-8720-8416fdbde762

--- meta/types/FloatType.class.php
+++ meta/types/FloatType.class.php
@@ -1,11 +1,11 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2006 by Nickolay G. Korolyov                            *
+ *   Copyright (C) 2006-2007 by Nickolay G. Korolyov                       *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
  *                                                                         *
  ***************************************************************************/
 /* $Id$ */
