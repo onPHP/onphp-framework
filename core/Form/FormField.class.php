f5340d457d55413b40c9482c9c9179d0e2deb494
Konstantin V. Arkhipov <voxus@onphp.org>
2007-07-27 11:25:46 +0000

    [GPL-2+ -> GPL-3+] branches/0.2
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.2@3922 db88c99c-74ec-0310-8720-8416fdbde762

--- core/Form/FormField.class.php
+++ core/Form/FormField.class.php
@@ -1,13 +1,13 @@
 <?php
-/***************************************************************************
- *   Copyright (C) 2005 by Anton E. Lebedevich, Konstantin V. Arkhipov     *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+/****************************************************************************
+ *   Copyright (C) 2005-2007 by Anton E. Lebedevich, Konstantin V. Arkhipov *
+ *                                                                          *
+ *   This program is free software; you can redistribute it and/or modify   *
+ *   it under the terms of the GNU General Public License as published by   *
+ *   the Free Software Foundation; either version 3 of the License, or      *
+ *   (at your option) any later version.                                    *
+ *                                                                          *
+ ****************************************************************************/
 /* $Id$ */
 
 	/**
--- main/Net/Url.class.php
+++ main/Net/Url.class.php
@@ -1,10 +1,10 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2005 by Scheglov K.                                     *
+ *   Copyright (C) 2005-2007 by Scheglov K.                                *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
+ *   the Free Software Foundation; either version 3 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
  *   Based on Net/URL.php (C) PEAR: Richard Heyes <richard at php net>     *
