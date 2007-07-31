c741c2fd8ceb369c653727b2932cc87a8f30a09a
Anton E. Lebedevich <antony@onphp.org>
2007-07-31 09:51:12 +0000

    interfaces for http clients added
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.10@3944 db88c99c-74ec-0310-8720-8416fdbde762

---
+++ main/Utils/Http/HttpResponse.class.php
@@ -0,0 +1,30 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2007 by Anton E. Lebedevich                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id$ */
+
+	interface HttpResponse
+	{
+		/**
+		 * @return HttpStatus
+		**/
+		public function getStatusCode();
+		public function getReasonPhrase();
+		
+		
+		/**
+		 * @return array
+		**/
+		public function getHeaders();
+		public function getHeader($name);
+		
+		public function getBody();
+	}
+?>
