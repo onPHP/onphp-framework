d2b14a190dd26b38a30e8b72d0b8914a97d936a2
Anton E. Lebedevich <antony@onphp.org>
2007-07-31 14:36:13 +0000

    HeaderParser added
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.10@3946 db88c99c-74ec-0310-8720-8416fdbde762

---
+++ test/main/HeaderParserTest.class.php
@@ -0,0 +1,51 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2007 by Anton E. Lebedevich                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id$ */
+
+	class HeaderParserTest extends UnitTestCase
+	{
+		public function testSimple()
+		{
+			$raw = <<<EOT
+Date: Tue, 31 Jul 2007 14:30:10 GMT
+Server: Apache/1.3.27 (Unix) PHP/3.0.18 rus/PL30.16
+X-Powered-By: PHP/3.0.18
+Location: /eng/index.html
+Connection: close
+Transfer-Encoding: chunked
+Content-Type: text/html; charset=koi8-r
+Content-Language: ru
+Vary: accept-charset, user-agent
+EOT;
+			$headers = HeaderParser::create()->parse($raw);
+			$this->assertEqual(9, count($headers));
+			$this->assertEqual('close', $headers['connection']);
+		}
+		
+		public function testMultiline()
+		{
+			$raw = <<<EOT
+Date: Tue, Jul 31 18:23:39 MSD 2007
+Server: Nginx
+Content-Length: 123
+Keep-Alive: timeout=20, 
+	max=200
+Connection: Keep-Alive
+Content-Type: text/html; 
+  charset=utf-8
+EOT;
+			$headers = HeaderParser::create()->parse($raw);
+			$this->assertEqual(6, count($headers));
+			$this->assertEqual($headers['keep-alive'], 'timeout=20, max=200');
+			$this->assertEqual($headers['content-length'], '123');
+		}
+	}
+?>
