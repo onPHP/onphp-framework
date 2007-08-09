021eaaed9a7dda8978a0d15452dfd2fabcceb4f6
Nickolay G. Korolyov <nk@bessia.net>
2007-08-09 07:16:27 +0000

    + simple methods
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.10@4000 db88c99c-74ec-0310-8720-8416fdbde762

--- main/Utils/FileUtils.class.php
+++ main/Utils/FileUtils.class.php
@@ -164,5 +164,44 @@
 				}
 			}
 		}
+		
+		public static function upload($source, $target)
+		{
+			if (
+				is_readable($source)
+				&& is_writable(pathinfo($target, PATHINFO_DIRNAME))
+			)
+				return move_uploaded_file($source, $target);
+			
+			throw new WrongArgumentException(
+				"can not move {$source} to {$target}"
+			);
+		}
+		
+		public static function move($source, $target)
+		{
+			if (
+				is_readable($source)
+				&& is_writable(pathinfo($target, PATHINFO_DIRNAME))
+			)
+				return rename($source, $target);
+			
+			throw new WrongArgumentException(
+				"can not move {$source} to {$target}"
+			);
+		}
+		
+		public static function unlink($filePath)
+		{
+			if (
+				file_exists($filePath)
+				&& is_writable(pathinfo($filePath, PATHINFO_DIRNAME))
+			)
+				return unlink($filePath);
+			
+			throw new WrongArgumentException(
+				"can not unlink {$filePath}"
+			);
+		}
 	}
 ?>
