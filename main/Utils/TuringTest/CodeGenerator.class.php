8c953644457e2e5b51553a48b309e47bb13e01d3
Dmitry A. Lomash <groomycian@oemdesign.ru>
2007-08-09 12:15:45 +0000

    * added setCharactersAllowed() to CodeGenerator class
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.10@4002 db88c99c-74ec-0310-8720-8416fdbde762

--- main/Utils/TuringTest/CodeGenerator.class.php
+++ main/Utils/TuringTest/CodeGenerator.class.php
@@ -86,6 +86,17 @@
 			return $this;
 		}		
 		
+		/**
+		 * @return CodeGenerator
+		**/
+		public function setCharactersAllowed($charactersAllowed = true)
+		{
+			$this->setLowerAllowed($charactersAllowed);
+			$this->setUpperAllowed($charactersAllowed);
+			
+			return $this;
+		}
+		
 	    private function generateOneSymbol()
 	    {
 			$variants = array();
