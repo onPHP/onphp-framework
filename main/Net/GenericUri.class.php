7a02e423d336ad184e223d61c25ca17bdb2fca61
Konstantin V. Arkhipov <voxus@onphp.org>
2007-08-14 11:17:38 +0000

    * PercentEncodingNormalizator merged back to GenericUri, as requested by antony
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@4067 db88c99c-74ec-0310-8720-8416fdbde762

--- main/Net/GenericUri.class.php
+++ main/Net/GenericUri.class.php
@@ -733,4 +733,54 @@
 			return $result;
 		}
 	}
+	
+	/**
+	 * @ingroup Net
+	**/
+	final class PercentEncodingNormalizator
+	{
+		private $unreservedPartChars = null;
+		
+		/**
+		 * @return PercentEncodingNormalizator
+		**/
+		public static function create()
+		{
+			return new self;
+		}
+		
+		/**
+		 * @return PercentEncodingNormalizator
+		**/
+		public function setUnreservedPartChars($unreservedPartChars)
+		{
+			$this->unreservedPartChars = $unreservedPartChars;
+			return $this;
+		}
+		
+		public function normalize($matched)
+		{
+			$char = $matched[0];
+			if (mb_strlen($char) == 1) {
+				if (
+					!preg_match(
+						'/^['.$this->unreservedPartChars.']$/',
+						$char
+					)
+				)
+					$char = rawurlencode($char);
+			} else {
+				if (
+					preg_match(
+						'/^['.GenericUri::CHARS_UNRESERVED.']$/', 
+						rawurldecode($char)
+					)
+				)
+					$char = rawurldecode($char);
+				else
+					$char = strtoupper($char);
+			}
+			return $char;
+		}
+	}
 ?>
