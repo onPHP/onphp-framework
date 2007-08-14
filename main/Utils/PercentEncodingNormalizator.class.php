f0bf57af6a8cc739062b5f4b4ec5478858e8e963
Konstantin V. Arkhipov <voxus@onphp.org>
2007-08-14 08:35:50 +0000

    * random cosmetics
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.10@4062 db88c99c-74ec-0310-8720-8416fdbde762

---
+++ main/Utils/PercentEncodingNormalizator.class.php
@@ -0,0 +1,62 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2007 by Ivan Y. Khvostishkov                            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id$ */
+
+	/**
+	 * @ingroup Net
+	**/
+	final class PercentEncodingNormalizator
+	{
+		private $unreservedPartChars = null;
+		
+		/**
+		 * @return PercentEncodingNormalizator
+		**/
+		public static function create()
+		{
+			return new self;
+		}
+		
+		/**
+		 * @return PercentEncodingNormalizator
+		**/
+		public function setUnreservedPartChars($unreservedPartChars)
+		{
+			$this->unreservedPartChars = $unreservedPartChars;
+			return $this;
+		}
+		
+		public function normalize($matched)
+		{
+			$char = $matched[0];
+			if (mb_strlen($char) == 1) {
+				if (
+					!preg_match(
+						'/^['.$this->unreservedPartChars.']$/',
+						$char
+					)
+				)
+					$char = rawurlencode($char);
+			} else {
+				if (
+					preg_match(
+						'/^['.GenericUri::CHARS_UNRESERVED.']$/', 
+						rawurldecode($char)
+					)
+				)
+					$char = rawurldecode($char);
+				else
+					$char = strtoupper($char);
+			}
+			return $char;
+		}
+	}
+?>
