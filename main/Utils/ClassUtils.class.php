f91f4722ecfcb903adab27859e41ec7dc4ad3d1e
Konstantin V. Arkhipov <voxus@onphp.org>
2007-08-16 10:39:16 +0000

    + class name checking
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.8@4083 db88c99c-74ec-0310-8720-8416fdbde762

--- main/Utils/ClassUtils.class.php
+++ main/Utils/ClassUtils.class.php
@@ -98,5 +98,14 @@
 				}
 			}
 		}
+		
+		public static function isClassName($className)
+		{
+			return
+				preg_match(
+					'/^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*$/', 
+					$className
+				);
+		}
 	}
 ?>
