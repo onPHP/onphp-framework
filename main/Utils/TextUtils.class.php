8e25a1332d28da5a86cc21b4a61378990a45e3cc
Ivan Y. Khvostishkov <dedmajor@gmail.com>
2007-08-29 11:48:26 +0000

    + TextUtils::safeAmp() to avoid double encoding of html entities
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@4158 db88c99c-74ec-0310-8720-8416fdbde762

--- main/Utils/TextUtils.class.php
+++ main/Utils/TextUtils.class.php
@@ -137,5 +137,25 @@
 			
 			return $out;
 		}
+		
+		/**
+		 * @see http://www.w3.org/TR/REC-html40/charset.html#entities
+		 * @see http://www.w3.org/TR/REC-html40/sgml/entities.html
+		 * @see http://php.net/htmlentities
+		 *
+		 * htmlentities() do not support hexadecimal numeric character
+		 * references yet.
+		**/
+		public static function safeAmp($text)
+		{
+			$result = preg_replace(
+				'/&(?!(#(([0-9]+)|(x[0-9A-F]+))'
+				.'|([a-z][a-z0-9]*));)/i',
+				'&amp;',
+				$text
+			);
+			
+			return $result;
+		}
 	}
 ?>
