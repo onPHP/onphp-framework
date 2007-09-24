cdebdf21c2f8f41dc53b3ac2ba741f40c02b2e55
Vladimir A. Altuchov <sirvantos@oemdesign.ru>
2007-09-24 15:20:22 +0000

    * tests
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@4221 db88c99c-74ec-0310-8720-8416fdbde762

---
+++ test/main/Ip/IpBaseRangeTest.class.php
@@ -0,0 +1,63 @@
+<?php
+	/* $Id$ */
+	
+	final class IpBaseRangeTest extends UnitTestCase
+	{
+		public function testContains()
+		{
+			$ipRange = 
+				IpRange::create(
+					IpAddress::create('127.0.0.1'),
+					IpAddress::create('127.0.0.10')
+				);
+			
+			$this->assertTrue(
+				$ipRange->contains(
+					IpAddress::create(
+						'127.0.0.1'
+					)
+				)
+			);
+			
+			$this->assertTrue(
+				$ipRange->contains(
+					IpAddress::create(
+						'127.0.0.9'
+					)
+				)
+			);
+			
+			$this->assertTrue(
+				$ipRange->contains(
+					IpAddress::create(
+						'127.0.0.10'
+					)
+				)
+			);
+			
+			$this->assertFalse(
+				$ipRange->contains(
+					IpAddress::create(
+						'127.0.0.0'
+					)
+				)
+			);
+			
+			$this->assertFalse(
+				$ipRange->contains(
+					IpAddress::create(
+						'127.0.0.11'
+					)
+				)
+			);
+			
+			$this->assertFalse(
+				$ipRange->contains(
+					IpAddress::create(
+						'127.0.0.255'
+					)
+				)
+			);
+		}
+	}
+?>
