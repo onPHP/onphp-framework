c452aa43ffbe1c78dae45e09ce65ee4f7f9b5d0a
Denis M. Gabaidulin <gabaden@gmail.com>
2007-09-28 16:16:42 +0000

    + toDialectString method implemented (@ext), need the test
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@4288 db88c99c-74ec-0310-8720-8416fdbde762

--- ext/src/core/OSQL/QuerySkeleton.c
+++ ext/src/core/OSQL/QuerySkeleton.c
@@ -18,6 +18,7 @@
 #include "core/Exceptions.h"
 #include "core/OSQL/QuerySkeleton.h"
 #include "core/Logic/LogicalObject.h"
+#include "core/DB/Dialect.h"
 
 ONPHP_METHOD(QuerySkeleton, __construct)
 {
@@ -175,8 +176,105 @@ ONPHP_METHOD(QuerySkeleton, orWhere)
 	RETURN_ZVAL(retval, 1, 0);
 }
 
+ONPHP_METHOD(QuerySkeleton, toDialectString)
+{
+	zval *where, *whereLogic, *dialect;
+	
+	if (
+		zend_parse_parameters(
+			ZEND_NUM_ARGS() TSRMLS_CC,
+			"z",
+			&dialect
+		)
+		== FAILURE
+	) {
+		WRONG_PARAM_COUNT;
+	}
+	
+	where = ONPHP_READ_PROPERTY(getThis(), "where");
+	whereLogic = ONPHP_READ_PROPERTY(getThis(), "whereLogic");
+	
+	if (Z_TYPE_P(where) != IS_NULL) {
+		zval *outputLogic, *exp = NULL;
+		zval **data;
+		int i, array_count, retval_len;
+		char *retval = NULL;
+
+		MAKE_STD_ZVAL(outputLogic);
+		ZVAL_FALSE(outputLogic);
+		
+		smart_str clause = {0};
+		smart_str_appendl(&clause, " WHERE", 6);
+		
+		array_count = zend_hash_num_elements(Z_ARRVAL_P(where));
+		
+		for (i = 0; i < array_count; i++) {
+			if (
+				zend_hash_index_find(
+					Z_ARRVAL_P(where),
+					i,
+					(void **)&data
+				) == SUCCESS
+			) {
+		    	zend_call_method_with_1_params(
+					data,
+					Z_OBJCE_PP(data),
+					NULL,
+					"todialectstring",
+					&exp,
+					dialect
+				);
+				
+				if (EG(exception)) {
+					return;
+				}
+				
+				if (exp) {
+					if (
+						zend_hash_index_find(
+							Z_ARRVAL_P(whereLogic),
+							i,
+							(void **)&data
+						) == SUCCESS
+					) {
+						onphp_append_zval_to_smart_string(&clause, (zval*)data);
+						smart_str_appendl(&clause, " ", 1);
+						onphp_append_zval_to_smart_string(&clause, exp);
+						smart_str_appendl(&clause, ".", 1);
+						
+						ZVAL_TRUE(outputLogic);
+					}
+					
+					ZVAL_FREE(exp);
+				}
+				
+				if (
+					!Z_BVAL_P(outputLogic)
+					&& (
+						zend_hash_index_find(
+							Z_ARRVAL_P(whereLogic),
+							i + 1,
+							(void **)&data
+						) == SUCCESS
+					)
+				) {
+					data = NULL;
+				}
+		   	}
+		}
+		
+		retval = (char*) php_trim(clause.c, clause.len, " ", 1, NULL, 2);
+		smart_str_0(&clause);
+		retval_len = strlen(retval);
+		RETURN_STRINGL(retval, retval_len, 0);
+	}
+	
+	return;
+}
+
 static ONPHP_ARGINFO_LOGICAL_OBJECT;
 static ONPHP_ARGINFO_LOGICAL_OBJECT_AND_ONE;
+static ONPHP_ARGINFO_DIALECT;
 
 zend_function_entry onphp_funcs_QuerySkeleton[] = {
 	ONPHP_ME(QuerySkeleton, __construct, NULL, ZEND_ACC_PUBLIC)
@@ -184,5 +282,6 @@ zend_function_entry onphp_funcs_QuerySkeleton[] = {
 	ONPHP_ME(QuerySkeleton, where, arginfo_logical_object_and_one, ZEND_ACC_PUBLIC)
 	ONPHP_ME(QuerySkeleton, andWhere, arginfo_logical_object, ZEND_ACC_PUBLIC)
 	ONPHP_ME(QuerySkeleton, orWhere, arginfo_logical_object, ZEND_ACC_PUBLIC)
+	ONPHP_ME(QuerySkeleton, toDialectString, arginfo_dialect, ZEND_ACC_PUBLIC)
 	{NULL, NULL, NULL}
 };
--- ext/src/onphp_core.c
+++ ext/src/onphp_core.c
@@ -44,7 +44,7 @@
 #include "core/OSQL/SQLTableName.h"
 #include "core/OSQL/Query.h"
 #include "core/OSQL/QueryIdentification.h"
-//#include "core/OSQL/QuerySkeleton.h"
+#include "core/OSQL/QuerySkeleton.h"
 #include "core/Logic/LogicalObject.h"
 
 #include "core/Exceptions.h"
@@ -146,9 +146,10 @@ PHP_MINIT_FUNCTION(onphp_core)
 	REGISTER_ONPHP_IMPLEMENTS(QueryIdentification, Query);
 	onphp_ce_QueryIdentification->ce_flags |= ZEND_ACC_EXPLICIT_ABSTRACT_CLASS;
 	
-	/*REGISTER_ONPHP_STD_CLASS_EX(QuerySkeleton);
+	REGISTER_ONPHP_SUB_CLASS_EX(QuerySkeleton, QueryIdentification);
 	REGISTER_ONPHP_PROPERTY(QuerySkeleton, "where", ZEND_ACC_PROTECTED);
-	REGISTER_ONPHP_PROPERTY(QuerySkeleton, "whereLogic", ZEND_ACC_PROTECTED);*/
+	REGISTER_ONPHP_PROPERTY(QuerySkeleton, "whereLogic", ZEND_ACC_PROTECTED);
+	onphp_ce_QuerySkeleton->ce_flags |= ZEND_ACC_EXPLICIT_ABSTRACT_CLASS;
 	
 	return PHP_MINIT(Exceptions)(INIT_FUNC_ARGS_PASSTHRU);
 }
