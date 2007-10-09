9a26de6554dc265d0ff13e22ccec85ccbaf5fc24
Ivan Y. Khvostishkov <dedmajor@gmail.com>
2007-10-09 17:47:07 +0000

    dedmajor: new dto scheme
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@4343 db88c99c-74ec-0310-8720-8416fdbde762

--- core/Base/DTOProto.class.php
+++ core/Base/DTOProto.class.php
@@ -5,18 +5,50 @@
  ***************************************************************************/
 /* $Id$ */
 
-	abstract class DTOProto extends Singleton
+	class DTOProto extends Singleton
 	{
 		private $formMapping	= array();
 		
-		public function makeForm()
+		final public function toForm($object)
 		{
-			return Form::create();
+			if ($this->baseProto())
+				$result = $this->baseProto()->toForm($object);
+			else
+				$result = Form::create();
+			
+			return
+				$this->attachPrimitives($result)->
+					importMore($this->buildScope($object));
+		}
+		
+		final public function makeForm()
+		{
+			if ($this->baseProto())
+				$result = $this->baseProto()->makeForm();
+			else
+				$result = Form::create();
+			
+			return $this->attachPrimitives($result);
+		}
+		
+		public function baseProto()
+		{
+			return null;
 		}
 		
 		public function getFormMapping()
 		{
 			return $this->formMapping;
+		}
+		
+		protected function attachPrimitives(Form $form)
+		{
+			return $form;
+		}
+		
+		protected function buildScope($object)
+		{
+			return array();
 		}
 	}
 ?>
