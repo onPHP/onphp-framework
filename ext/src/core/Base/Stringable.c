8c633ffd4b33e55a8a294f245b9575b7c3aa5434
Konstantin V. Arkhipov <voxus@onphp.org>
2007-11-04 20:45:27 +0000

    - typos
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.6@4502 db88c99c-74ec-0310-8720-8416fdbde762

--- ext/src/core/Base/Stringable.c
+++ ext/src/core/Base/Stringable.c
@@ -16,6 +16,6 @@
 PHPAPI zend_class_entry *onphp_ce_Stringable;
 
 zend_function_entry onphp_funcs_Stringable[] = {
-	ONPHP_ABSTRACT_ME(Identifiable, toString, NULL, ZEND_ACC_PUBLIC)
+	ONPHP_ABSTRACT_ME(Stringable, toString, NULL, ZEND_ACC_PUBLIC)
 	{NULL, NULL, NULL}
 };
