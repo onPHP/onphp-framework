eb54276cf8b8dff1819b559b5e7a96bdbe787039
Konstantin V. Arkhipov <voxus@onphp.org>
2007-11-04 20:39:55 +0000

    + class name collision detection @ meta
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.10@4458 db88c99c-74ec-0310-8720-8416fdbde762

--- meta/classes/MetaConfiguration.class.php
+++ meta/classes/MetaConfiguration.class.php
@@ -1008,8 +1008,14 @@ XML;
 			}
 			
 			foreach ($xml->classes[0] as $xmlClass) {
+				$name = (string) $xmlClass['name'];
 				
-				$class = new MetaClass((string) $xmlClass['name']);
+				Assert::isFalse(
+					isset($this->classes[$name]),
+					'class name collision found for '.$name
+				);
+				
+				$class = new MetaClass($name);
 				
 				if (isset($xmlClass['source']))
 					$class->setSourceLink((string) $xmlClass['source']);
