6b451478289ff8e49ebcbf996d7a0af757284034
Konstantin V. Arkhipov <voxus@onphp.org>
2007-11-29 14:22:22 +0000

    * trivial optimization
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.10@4654 db88c99c-74ec-0310-8720-8416fdbde762

--- main/DAOs/Workers/CommonDaoWorker.class.php
+++ main/DAOs/Workers/CommonDaoWorker.class.php
@@ -31,8 +31,10 @@
 			if (
 				($expires !== Cache::DO_NOT_CACHE)
 				&& ($object = $this->getCachedById($id))
-				&& ($object !== Cache::NOT_FOUND)
 			) {
+				if ($object === Cache::NOT_FOUND)
+					throw new ObjectNotFoundException();
+				
 				return $object;
 			} else {
 				$query =
@@ -88,10 +90,12 @@
 			if (
 				($expires !== Cache::DO_NOT_CACHE) &&
 				($object = $this->getCachedByQuery($query))
-				&& ($object !== Cache::NOT_FOUND)
-			)
+			) {
+				if ($object === Cache::NOT_FOUND)
+					throw new ObjectNotFoundException();
+				
 				return $object;
-			elseif ($object = $this->fetchObject($query)) {
+			} elseif ($object = $this->fetchObject($query)) {
 				if ($expires === Cache::DO_NOT_CACHE)
 					return $object;
 				else
@@ -126,10 +130,12 @@
 			if (
 				($expires !== Cache::DO_NOT_CACHE) &&
 				($object = $this->getCachedByQuery($query))
-				&& ($object !== Cache::NOT_FOUND)
-			)
+			) {
+				if ($object === Cache::NOT_FOUND)
+					throw new ObjectNotFoundException();
+				
 				return $object;
-			elseif ($object = $db->queryRow($query)) {
+			} elseif ($object = $db->queryRow($query)) {
 				if ($expires === Cache::DO_NOT_CACHE)
 					return $object;
 				else
@@ -171,11 +177,13 @@
 
 				foreach ($ids as $id) {
 					$cached = $this->getCachedById($id);
-
-					if ($cached && ($cached !== Cache::NOT_FOUND))
-						$list[] = $cached;
-					else
-						$toFetch[] = $id;
+					
+					if ($cached !== Cache::NOT_FOUND) {
+						if ($cached)
+							$list[] = $cached;
+						else
+							$toFetch[] = $id;
+					}
 				}
 				
 				if (!$toFetch)
@@ -225,10 +233,12 @@
 			if (
 				($expires !== Cache::DO_NOT_CACHE) &&
 				($list = $this->getCachedByQuery($query))
-				&& ($list !== Cache::NOT_FOUND)
-			)
+			) {
+				if ($list === Cache::NOT_FOUND)
+					throw new ObjectNotFoundException();
+				
 				return $list;
-			elseif ($list = $this->fetchList($query)) {
+			} elseif ($list = $this->fetchList($query)) {
 				if (Cache::DO_NOT_CACHE === $expires) {
 					return $list;
 				} else {
