1c227dded10627547adc613a2d2a01c0fc5ff975
Konstantin V. Arkhipov <voxus@onphp.org>
2008-01-08 04:03:22 +0000

    * CacheSegmentHandler is now the default handler for VoodooDaoWorker
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@4750 db88c99c-74ec-0310-8720-8416fdbde762

--- main/DAOs/Workers/VoodooDaoWorker.class.php
+++ main/DAOs/Workers/VoodooDaoWorker.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2006-2007 by Konstantin V. Arkhipov                     *
+ *   Copyright (C) 2006-2008 by Konstantin V. Arkhipov                     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Lesser General Public License as        *
@@ -145,7 +145,7 @@
 					} elseif (extension_loaded('xcache')) {
 						$handlerName = 'XCacheSegmentHandler';
 					} else {
-						$handlerName = 'FileSystemSegmentHandler';
+						$handlerName = 'CacheSegmentHandler';
 					}
 				}
 			} else {
