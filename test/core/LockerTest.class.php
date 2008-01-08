ca7ffaae347ed9e08f70adeec0b673cd271b07fe
Konstantin V. Arkhipov <voxus@onphp.org>
2008-01-08 04:03:49 +0000

    + simple test for FileLocker + forgotten file unlinking @ FileLocker
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.10@4759 db88c99c-74ec-0310-8720-8416fdbde762

---
+++ test/core/LockerTest.class.php
@@ -0,0 +1,25 @@
+<?php
+	/* $Id$ */
+	
+	final class LockerTest extends UnitTestCase
+	{
+		public function testFileLocker()
+		{
+			$directory = ONPHP_TEMP_PATH.'file-locking/';
+			
+			$locker = new FileLocker('file-locking/');
+			
+			$this->assertTrue($locker->get('test'));
+			
+			$this->assertTrue(file_exists($directory.'test'));
+			
+			$this->assertTrue($locker->free('test'));
+			
+			$this->assertTrue(file_exists($directory.'test'));
+			
+			$this->assertTrue($locker->clean());
+			
+			$this->assertFalse(file_exists($directory.'test'));
+		}
+	}
+?>
