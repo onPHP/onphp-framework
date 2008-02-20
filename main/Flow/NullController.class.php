8251c9064626fa6876e7c465be97e69e4b0db538
Ivan Y. Khvostishkov <dedmajor@gmail.com>
2008-02-20 15:19:13 +0000

    NullController implemented
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@4841 db88c99c-74ec-0310-8720-8416fdbde762

---
+++ main/Flow/NullController.class.php
@@ -0,0 +1,40 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2008 by Ivan Y. Khvostishkov, Konstantin V. Arkhipov    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id$ */
+
+	/**
+	 * @ingroup Flow
+	**/
+	final class NullController implements Controller
+	{
+		private $model = null;
+		
+		public static function create(Model $model = null)
+		{
+			return new self($model);
+		}
+		
+		public function __construct(Model $model = null)
+		{
+			$this->model = $model;
+		}
+		
+		public function handleRequest(HttpRequest $request)
+		{
+			$result = ModelAndView::create();
+			
+			if ($this->model)
+				$result->setModel($this->model);
+			
+			return $result;
+		}
+	}
+?>
