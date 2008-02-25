f3def168ec9c00decfeca7be58010a9f2c2f0819
Konstantin V. Arkhipov <voxus@onphp.org>
2008-02-25 09:42:12 +0000

    - typo, spotted by Sergej Stolbovsky
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@4848 db88c99c-74ec-0310-8720-8416fdbde762

--- core/Base/Assert.class.php
+++ core/Base/Assert.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2005-2007 by Konstantin V. Arkhipov                     *
+ *   Copyright (C) 2005-2008 by Konstantin V. Arkhipov                     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Lesser General Public License as        *
@@ -36,7 +36,7 @@
 		public static function isNotFalse($boolean, $message = null)
 		{
 			if ($boolean === false)
-				self::fail(
+				throw new WrongArgumentException(
 					$message.', '.self::dumpArgument($boolean)
 				);
 		}
