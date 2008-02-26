5a6399a9410f7c085020a2dc0be9657ae4021bb3
Konstantin V. Arkhipov <voxus@onphp.org>
2008-02-26 11:04:01 +0000

    * correct return value, spotted by Sergej Stolbovsky
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.10@4853 db88c99c-74ec-0310-8720-8416fdbde762

--- core/Cache/SystemFiveLocker.class.php
+++ core/Cache/SystemFiveLocker.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2005-2007 by Konstantin V. Arkhipov                     *
+ *   Copyright (C) 2005-2008 by Konstantin V. Arkhipov                     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Lesser General Public License as        *
@@ -23,9 +23,7 @@
 				if (!isset($this->pool[$key]))
 					$this->pool[$key] = sem_get($key, 1, ONPHP_IPC_PERMS, false);
 				
-				sem_acquire($this->pool[$key]);
-				
-				return $this->pool[$key];
+				return sem_acquire($this->pool[$key]);
 			} catch (BaseException $e) {
 				return null;
 			}
