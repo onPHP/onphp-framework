1cb48403548f999832967e46b5f43f9263dda1ff
Konstantin V. Arkhipov <voxus@onphp.org>
2008-02-28 18:20:03 +0000

    * 1.0.2, 0.10.10
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@4863 db88c99c-74ec-0310-8720-8416fdbde762

--- ext/src/onphp.h
+++ ext/src/onphp.h
@@ -16,7 +16,7 @@
 #include "zend_interfaces.h"
 #include "Zend/zend_globals.h" // executor_globals
 
-#define ONPHP_VERSION "1.0.1.99"
+#define ONPHP_VERSION "1.0.2.99"
 #define ONPHP_MODULE_NAME "onPHP"
 
 /// dumb copy of zend_call_method
