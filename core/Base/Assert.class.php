e6b9a775c1fbdb1aedcfe937abdb454bed3ae33e
Konstantin V. Arkhipov <voxus@onphp.org>
2008-03-06 14:30:12 +0000

    * cosmetics
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.10@4884 db88c99c-74ec-0310-8720-8416fdbde762

--- core/Base/Assert.class.php
+++ core/Base/Assert.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2005-2007 by Konstantin V. Arkhipov                     *
+ *   Copyright (C) 2005-2008 by Konstantin V. Arkhipov                     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Lesser General Public License as        *
@@ -67,7 +67,7 @@
 		{
 			self::isArray($variable, $message);
 			
-			if (empty($variable))
+			if (!$variable)
 				self::fail(
 					$message.', '.self::dumpArgument($variable)
 				);
