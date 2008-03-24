be2ef1738d66e8619e80038e70130c06753d8820
Konstantin V. Arkhipov <voxus@onphp.org>
2008-03-24 13:40:34 +0000

    + custom clean() for list-based primitives, which preserves default value
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/1.0@4975 db88c99c-74ec-0310-8720-8416fdbde762

--- core/Form/Primitives/PrimitiveEnumerationList.class.php
+++ core/Form/Primitives/PrimitiveEnumerationList.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2007 by Ivan Y. Khvostishkov                            *
+ *   Copyright (C) 2007-2008 by Ivan Y. Khvostishkov                       *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Lesser General Public License as        *
@@ -16,6 +16,19 @@
 	final class PrimitiveEnumerationList extends PrimitiveEnumeration
 	{
 		protected $value = array();
+		
+		/**
+		 * @return PrimitiveEnumerationList
+		**/
+		public function clean()
+		{
+			parent::clean();
+			
+			// restoring our very own default
+			$this->value = array();
+			
+			return $this;
+		}
 		
 		public function importValue($value)
 		{
