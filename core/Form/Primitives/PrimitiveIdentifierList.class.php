e324ae4359c80bd6bcbb68e92fcbb5e0245beccd
Konstantin V. Arkhipov <voxus@onphp.org>
2008-03-24 13:40:34 +0000

    + custom clean() for list-based primitives, which preserves default value
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@4975 db88c99c-74ec-0310-8720-8416fdbde762

--- core/Form/Primitives/PrimitiveIdentifierList.class.php
+++ core/Form/Primitives/PrimitiveIdentifierList.class.php
@@ -1,13 +1,13 @@
 <?php
-/***************************************************************************
- *   Copyright (C) 2007 by Denis M. Gabaidulin, Konstantin V. Arkhipov     *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU Lesser General Public License as        *
- *   published by the Free Software Foundation; either version 3 of the    *
- *   License, or (at your option) any later version.                       *
- *                                                                         *
- ***************************************************************************/
+/****************************************************************************
+ *   Copyright (C) 2007-2008 by Denis M. Gabaidulin, Konstantin V. Arkhipov *
+ *                                                                          *
+ *   This program is free software; you can redistribute it and/or modify   *
+ *   it under the terms of the GNU Lesser General Public License as         *
+ *   published by the Free Software Foundation; either version 3 of the     *
+ *   License, or (at your option) any later version.                        *
+ *                                                                          *
+ ****************************************************************************/
 /* $Id$ */
 
 	/**
@@ -16,6 +16,19 @@
 	final class PrimitiveIdentifierList extends PrimitiveIdentifier
 	{
 		protected $value = array();
+		
+		/**
+		 * @return PrimitiveIdentifierList
+		**/
+		public function clean()
+		{
+			parent::clean();
+			
+			// restoring our very own default
+			$this->value = array();
+			
+			return $this;
+		}
 		
 		public function setValue($value)
 		{
