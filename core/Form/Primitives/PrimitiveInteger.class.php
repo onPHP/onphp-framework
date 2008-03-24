41cf309271ff6dcd89d4866e42dc88ebd04c0f40
Konstantin V. Arkhipov <voxus@onphp.org>
2008-03-24 11:10:30 +0000

    + integer's limits checking @ meta
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@4969 db88c99c-74ec-0310-8720-8416fdbde762

--- core/Form/Primitives/PrimitiveInteger.class.php
+++ core/Form/Primitives/PrimitiveInteger.class.php
@@ -1,6 +1,6 @@
 <?php
 /****************************************************************************
- *   Copyright (C) 2004-2007 by Konstantin V. Arkhipov, Anton E. Lebedevich *
+ *   Copyright (C) 2004-2008 by Konstantin V. Arkhipov, Anton E. Lebedevich *
  *                                                                          *
  *   This program is free software; you can redistribute it and/or modify   *
  *   it under the terms of the GNU Lesser General Public License as         *
@@ -20,6 +20,9 @@
 		
 		const SIGNED_MIN = -2147483648;
 		const SIGNED_MAX = +2147483647;
+		
+		const SIGNED_BIG_MIN = -9223372036854775808;
+		const SIGNED_BIG_MAX = 9223372036854775807;
 		
 		const UNSIGNED_SMALL_MAX = 65535;
 		const UNSIGNED_MAX = 4294967295;
--- meta/types/UnsignedSmallIntegerType.class.php
+++ meta/types/UnsignedSmallIntegerType.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2007 by Konstantin V. Arkhipov                          *
+ *   Copyright (C) 2007-2008 by Konstantin V. Arkhipov                     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Lesser General Public License as        *
@@ -15,6 +15,11 @@
 	**/
 	final class UnsignedSmallIntegerType extends SmallIntegerType
 	{
+		public function getSize()
+		{
+			return 2 & LightMetaProperty::UNSIGNED_FLAG;
+		}
+		
 		public function toColumnType()
 		{
 			return
