68f8d36aedf8ce32b7e4f59b48359d33fb71ed9f
Ivan Y. Khvostishkov <dedmajor@gmail.com>
2008-03-25 14:42:52 +0000

    new exception to be used instead of View('error')
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@4984 db88c99c-74ec-0310-8720-8416fdbde762

---
+++ core/Exceptions/BadRequestException.class.php
@@ -0,0 +1,24 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2008 by Ivan Y. Khvostishkov                            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id$ */
+
+	/**
+	 * Example:
+	 *
+	 *	$form->import($request->getGet());
+	 *
+	 *	if ($form->getErrors())
+	 *		throw new BadRequestException('wrong arguments passed');
+	 *
+	 * Use this exception instead of View('error')
+	 */
+	class BadRequestException extends BaseException {/* nop */}
+?>
