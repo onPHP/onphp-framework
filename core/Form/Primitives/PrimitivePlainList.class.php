6658ef83c0bf1f92d4c87b2a402411ebe09b9472
Konstantin V. Arkhipov <voxus@onphp.org>
2008-03-26 11:57:59 +0000

    - typo
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@4990 db88c99c-74ec-0310-8720-8416fdbde762

--- core/Form/Primitives/PrimitivePlainList.class.php
+++ core/Form/Primitives/PrimitivePlainList.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2007 by Ivan Y. Khvostishkov                            *
+ *   Copyright (C) 2007-2008 by Ivan Y. Khvostishkov                       *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Lesser General Public License as        *
@@ -20,7 +20,7 @@
 		**/
 		public function setList($list)
 		{
-			$this->list = array_combine($list, $list);
+			$this->list = array_combine($this->list, $list);
 			
 			return $this;
 		}
