904dc863a7c2b0e1878908271652682f84de49bc
Konstantin V. Arkhipov <voxus@onphp.org>
2008-03-27 11:40:59 +0000

    * r4999:r5000 propagated
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@5001 db88c99c-74ec-0310-8720-8416fdbde762

--- core/DB/MySQL.class.php
+++ core/DB/MySQL.class.php
@@ -165,7 +165,74 @@
 		
 		public function getTableInfo($table)
 		{
-			throw new UnimplementedFeatureException();
+			static $types = array(
+				'tinyint'	=> DataType::SMALLINT,
+				'int'		=> DataType::INTEGER,
+				'bigint'	=> DataType::BIGINT,
+				
+				'varchar'	=> DataType::VARCHAR,
+				'text'		=> DataType::TEXT,
+				
+				'timestamp' => DataType::TIMESTAMP,
+				'date'		=> DataType::DATE
+			);
+			
+			$result = $this->queryRaw('SHOW COLUMNS FROM '.$table);
+			
+			if (!(mysql_num_rows($result) > 0)) {
+				throw new ObjectNotFoundException(
+					"no info for table '{$table}'"
+				);
+			}
+			
+			$table = new DBTable($table);
+			
+			while ($row = mysql_fetch_assoc($result)) {
+				$name = strtolower($row['Field']);
+				$matches = array();
+				$info = array('type' => null, 'extra' => null);
+				if (
+					preg_match(
+						'~(\w+)(\((\d+?)\)){0,1}\s*(\w*)~',
+						strtolower($row['Type']), 
+						$matches
+					)
+				) {
+					$info['type'] = $matches[1];
+					$info['size'] = $matches[3];
+					$info['extra'] = $matches[4];
+				}
+				
+				Assert::isTrue(
+					array_key_exists($info['type'], $types),
+					
+					'unknown type "'
+					.$types[$info['type']]
+					.'" found in column "'.$name.'"'
+				);
+				
+				if (empty($types[$info['type']]))
+					continue;
+				
+				$column = DBColumn::create(
+					DataType::create($types[$info['type']])->
+						setUnsigned(
+							strtolower($info['extra']) == 'unsigned'
+						)->
+						setNull(strtolower($row['Null']) == 'yes'),
+					
+					$name
+				)->
+				setAutoincrement(strtolower($row['Extra']) == 'auto_increment')->
+				setPrimaryKey(strtolower($row['Null']) == 'pri');
+				
+				if ($row['Default'])
+					$column->setDefault($row['Default']);
+				
+				$table->addColumn($column);
+			}
+			
+			return $table;
 		}
 		
 		public function hasQueue()
