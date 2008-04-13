09b2db089e681a9b3aec1b25649d72e546ca2b1c
Konstantin V. Arkhipov <voxus@onphp.org>
2008-04-13 12:09:05 +0000

    * DirectoryLocker is the default locker in SemaphorePool now
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/1.0@5061 db88c99c-74ec-0310-8720-8416fdbde762

--- core/Cache/SemaphorePool.class.php
+++ core/Cache/SemaphorePool.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2005-2007 by Konstantin V. Arkhipov                     *
+ *   Copyright (C) 2005-2008 by Konstantin V. Arkhipov                     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Lesser General Public License as        *
@@ -22,7 +22,7 @@
 	**/
 	final class SemaphorePool extends BaseLocker implements Instantiatable
 	{
-		private static $lockerName	= 'SystemFiveLocker';
+		private static $lockerName	= 'DirectoryLocker';
 		private static $locker		= null;
 		
 		protected function __construct()
