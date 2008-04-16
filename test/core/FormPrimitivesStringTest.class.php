2f20fa8d6e92b9e6aa0dd97c22e72c6c8d863216
Ivan Y. Khvostishkov <dedmajor@gmail.com>
2008-04-16 11:29:03 +0000

    poc failed test
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@5064 db88c99c-74ec-0310-8720-8416fdbde762

---
+++ test/core/FormPrimitivesStringTest.class.php
@@ -0,0 +1,114 @@
+<?php
+	/* $Id$ */
+	
+	final class FormPrimitivesStringTest extends TestCase
+	{
+		const MAX = 10;
+		
+		public function testClean()
+		{
+			$form = $this->makeForm();
+			
+			$this->formAssertsClean($form, 'property');
+		}
+		
+		public function testValid()
+		{
+			$form = $this->makeForm();
+			
+			$raw = 'ooOOoOo';
+			
+			$form->import(
+				array('property' => $raw)
+			);
+			
+			// value, safe, raw, form, export
+			$this->formAsserts($form, 'property', $raw, $raw, $raw, $raw, $raw);
+			$this->formErrorAsserts($form, false, false);
+		}
+		
+		public function testInvalidFormat()
+		{
+			$form = $this->makeForm();
+			
+			$raw = '0';
+			
+			$form->import(
+				array('property' => $raw)
+			);
+			
+			// NOTE: '0' is valid string, not a missing value
+			
+			// value, safe, raw, form, export
+			$this->formAsserts($form, 'property', $raw, $raw, $raw, $raw, $raw);
+			$this->formErrorAsserts($form, false, false);
+		}
+		
+		protected function getPrimitive()
+		{
+			return Primitive::string(null)->setMax(self::MAX);
+		}
+		
+		protected function makeForm()
+		{
+			return
+				Form::create()->
+				add($this->getPrimitive()->spawn('property'));
+		}
+		
+		
+		protected function formAssertsClean(Form $form, $prm)
+		{
+			$this->formErrorAsserts($form, false, false);
+			
+			// value, safe, raw, form, export
+			$this->formAsserts($form, 'property', null, null, null, null, null);
+		}
+		
+		protected function formAssertsMissing(Form $form, $prm, $default = null)
+		{
+			$this->formErrorAsserts($form, true, true);
+			
+			// value, safe, raw, form, export
+			$this->formAsserts($form, 'property', null, $default, null, null, null);
+		}
+		
+		protected function formAssertsWrong(Form $form, $prm, $raw, $default = null)
+		{
+			$this->formErrorAsserts($form, true, true);
+			
+			// value, safe, raw, form, export
+			$this->formAsserts($form, 'property', null, $default, $raw, $raw, null);
+		}
+		
+		private function formAsserts(
+			Form $form,
+			$prm,
+			$getValue,
+			$getSafeValue,
+			$getRawValue,
+			$getFormValue,
+			$exportValue
+		)
+		{
+			$this->assertEquals($getValue, $form->getValue($prm));
+			$this->assertEquals($getSafeValue, $form->getSafeValue($prm));
+			$this->assertEquals($getRawValue, $form->getRawValue($prm));
+			$this->assertEquals($getFormValue, $form->getFormValue($prm));
+			$this->assertEquals($exportValue, $form->exportValue($prm));
+		}
+		
+		private function formErrorAsserts(Form $form, $errors, $innerErrors)
+		{
+			if ($errors)
+				$this->assertFalse(!$form->getErrors());
+			else
+				$this->assertTrue(!$form->getErrors());
+			
+			if ($innerErrors)
+				$this->assertFalse(!$form->getInnerErrors());
+			else
+				$this->assertTrue(!$form->getInnerErrors());
+		}
+	}
+?>
