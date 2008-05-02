c36367a0a76aba4d34e530630c31480b7811155f
Konstantin V. Arkhipov <voxus@onphp.org>
2008-05-02 10:34:37 +0000

    + even more Integer-based types
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@5102 db88c99c-74ec-0310-8720-8416fdbde762

---
+++ core/Types/BigInteger.class.php
@@ -0,0 +1,21 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2008 by Konstantin V. Arkhipov                          *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id$ */
+
+	/**
+	 * @ingroup Types
+	**/
+	final class BigInteger extends Integer
+	{
+		protected $min = Integer::SIGNED_BIG_MIN;
+		protected $max = Integer::SIGNED_BIG_MAX;
+	}
+?>
---
+++ core/Types/SmallInteger.class.php
@@ -0,0 +1,21 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2008 by Konstantin V. Arkhipov                          *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id$ */
+
+	/**
+	 * @ingroup Types
+	**/
+	final class SmallInteger extends Integer
+	{
+		protected $min = Integer::SIGNED_SMALL_MIN;
+		protected $max = Integer::SIGNED_SMALL_MAX;
+	}
+?>
