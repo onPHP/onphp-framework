699282d12fe42eb75ecd7bc2fc78c0f19f305474
Konstantin V. Arkhipov <voxus@onphp.org>
2008-05-02 10:36:48 +0000

    + correct return value
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.6@5125 db88c99c-74ec-0310-8720-8416fdbde762

--- main/Base/Range.class.php
+++ main/Base/Range.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2004-2007 by Konstantin V. Arkhipov                     *
+ *   Copyright (C) 2004-2008 by Konstantin V. Arkhipov                     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -86,7 +86,7 @@
 			else
 				$this->max = $max;
 			
-			return $max;
+			return $this;
 		}
 
 		/// atavism wrt BC
