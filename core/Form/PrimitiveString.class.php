6fc39b854e5ba928b5afbfeca5b30868d531757c
Konstantin V. Arkhipov <voxus@onphp.org>
2008-05-02 10:27:38 +0000

    * PrimitiveString now supports '0' as a valid string too; bug was spotted by dedmajor
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.2@5082 db88c99c-74ec-0310-8720-8416fdbde762

--- core/Form/PrimitiveString.class.php
+++ core/Form/PrimitiveString.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2004-2007 by Konstantin V. Arkhipov, Sveta A. Smirnova  *
+ *   Copyright (C) 2004-2008 by Konstantin V. Arkhipov, Sveta A. Smirnova  *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -17,31 +17,37 @@
 	{
 		private $pattern = null;
 		
-		// hint: /^[^@]*\w+@[\w|\.|\-]+\.\w{2,6}$/
+		// mail hint: /^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$/
 		public function setAllowedPattern($pattern)
 		{
 			$this->pattern = $pattern;
 			
 			return $this;
 		}
-
+		
 		public function import(&$scope)
 		{
 			if (!BasePrimitive::import($scope))
 				return null;
-
-			if (is_string($scope[$this->name]) && !empty($scope[$this->name]) &&
-				!($this->max && mb_strlen($scope[$this->name]) > $this->max) &&
-				!($this->min && mb_strlen($scope[$this->name]) < $this->min) &&
-				(!$this->pattern || preg_match($this->pattern, $scope[$this->name]))
+			
+			$this->value = (string) $scope[$this->name];
+			
+			$this->selfFilter();
+			
+			if (
+				is_string($this->value)
+				// zero is quite special value here
+				&& ($this->value === '0' || !empty($this->value))
+				&& ($length = mb_strlen($this->value))
+				&& !($this->max && $length > $this->max)
+				&& !($this->min && $length < $this->min)
+				&& (!$this->pattern || preg_match($this->pattern, $this->value))
 			) {
-				$this->value = (string) $scope[$this->name];
-				
-				$this->selfFilter();
-
 				return true;
+			} else {
+				$this->value = null;
 			}
-
+			
 			return false;
 		}
 	}
