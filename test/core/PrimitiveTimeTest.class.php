3039b5164f6d24260fddb8cb0991ed0b98752269
Konstantin V. Arkhipov <voxus@onphp.org>
2008-05-02 10:36:29 +0000

    * cosmetics
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/1.0@5123 db88c99c-74ec-0310-8720-8416fdbde762

--- test/core/PrimitiveTimeTest.class.php
+++ test/core/PrimitiveTimeTest.class.php
@@ -13,12 +13,12 @@
 			
 			$array =
 				array (
-					0 => array('test'=>'00:12:01'),
-					1 => array('test'=>'00:14:00'),
-					2 => array('test'=>'00:09:59'),
-					3 => array('test'=>'00:11:00'),
-					4 => array('test'=>'00:10:00'),
-					5 => array('test'=>'00:12:00'),
+					0 => array('test' => '00:12:01'),
+					1 => array('test' => '00:14:00'),
+					2 => array('test' => '00:09:59'),
+					3 => array('test' => '00:11:00'),
+					4 => array('test' => '00:10:00'),
+					5 => array('test' => '00:12:00'),
 				);
 			
 			$this->assertFalse($prm->import($array[0]));
