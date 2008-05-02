9b3d91857df38a664a66126f89ec77cfee22b130
Konstantin V. Arkhipov <voxus@onphp.org>
2008-05-02 10:27:38 +0000

    * PrimitiveString now supports '0' as a valid string too; bug was spotted by dedmajor
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/1.0@5082 db88c99c-74ec-0310-8720-8416fdbde762

--- core/Form/Primitives/PrimitiveString.class.php
+++ core/Form/Primitives/PrimitiveString.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2004-2007 by Konstantin V. Arkhipov, Sveta A. Smirnova  *
+ *   Copyright (C) 2004-2008 by Konstantin V. Arkhipov, Sveta A. Smirnova  *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Lesser General Public License as        *
@@ -40,7 +40,10 @@
 			
 			$this->selfFilter();
 			
-			if (!empty($this->value) && is_string($this->value)
+			if (
+				is_string($this->value)
+				// zero is quite special value here
+				&& ($this->value === '0' || !empty($this->value))
 				&& ($length = mb_strlen($this->value))
 				&& !($this->max && $length > $this->max)
 				&& !($this->min && $length < $this->min)
