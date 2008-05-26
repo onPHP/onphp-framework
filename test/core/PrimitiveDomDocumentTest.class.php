d1bcc9ec3e66cb17c80b7d9afa69bf97c428ddc3
Ivan Y. Khvostishkov <dedmajor@gmail.com>
2008-05-26 19:23:35 +0000

    corresponding tests
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@5151 db88c99c-74ec-0310-8720-8416fdbde762

---
+++ test/core/PrimitiveDomDocumentTest.class.php
@@ -0,0 +1,139 @@
+<?php
+	/* $Id$ */
+	
+	final class PrimitiveDomDocumentTest extends TestCase
+	{
+		private $schema = null;
+		
+		public function __construct()
+		{
+			$this->schema =
+				ONPHP_TEST_PATH.'misc'.DIRECTORY_SEPARATOR
+				.'text.xsd';
+		}
+		
+		public function testGoodDocument()
+		{
+			$raw = <<<EOF
+<body>
+	<p>
+	Hello! <a href="http://example.com/">world!</a>
+	</p>
+
+	<p>
+	<em>em em em</em>
+	
+	normal text <a href="urn:project:href?area=profile&amp;arg1=value1">profile link</a>
+
+	<strong>strong strong</strong>
+	</p>
+</body>
+EOF;
+			
+			$prm = $this->makePrimitive();
+			
+			$this->assertTrue($prm->importValue($raw));
+			
+			$this->assertNull($prm->getError());
+			
+			Assert::isInstance($prm->getValue(), 'DomDocument');
+		}
+		
+		public function testInvalidDocument()
+		{
+			$raw = <<<EOF
+<body>
+	<p>
+		<a href=http://example.org/test>href attribute without quotes</a>
+	</p>
+</body>
+EOF;
+			
+			$prm = $this->makePrimitive();
+			
+			$this->assertFalse($prm->importValue($raw));
+			
+			$this->assertNull($prm->getValue());
+		}
+
+		public function testBadDocument()
+		{
+			$raw = <<<EOF
+<body>
+	<b>not allowed tag</b>
+	<a href="http://example.org/"><em>tags are not allowed inside A</em></a>
+	<br/>
+	line breaks are not allowed, use paragraphs
+</body>
+EOF;
+			
+			$prm = $this->makePrimitive();
+			
+			$this->assertFalse($prm->importValue($raw));
+			
+			$this->assertNull($prm->getValue());
+			
+			$this->assertEquals(
+				$prm->getError(),
+				PrimitiveDomDocument::ERROR_VALIDATION_FAILED
+			);
+		}
+		
+		public function testFormErrors()
+		{
+			$raw = '<body><b>not allowed tag</b></body>';
+			
+			$form = Form::create()->
+				add($this->makePrimitive());
+				
+			$form->import(array('dom' => $raw));
+			
+			$this->assertNotNull($form->getErrors());
+			
+			$this->assertNull($form->getValue('dom'));
+			
+			$this->assertEquals(
+				$form->getPrimitiveError('dom'),
+				BasePrimitive::WRONG
+			);
+			
+			$this->assertNotNull($form->getPrimitiveErrorLabel('dom'));
+			
+			$this->assertNotEquals($form->getTextualErrors(), array());
+			$this->assertNotNull($form->getTextualErrorFor('dom'));
+		}
+		
+		
+		public function testInvalidSchemaPath()
+		{
+			$this->setExpectedException('IOException');
+			
+			Primitive::domDocument('dom')->
+			ofXsd('http://nonexistenturl/');
+		}
+		
+		public function testBadSchemaFormat()
+		{
+			$prm =
+				Primitive::domDocument('dom')->
+				ofXsd('http://example.com/');
+			
+			$this->assertFalse($prm->importValue('<body />'));
+			
+			$this->assertNull($prm->getValue());
+			
+			$this->assertEquals(
+				$prm->getError(),
+				PrimitiveDomDocument::ERROR_VALIDATION_FAILED
+			);
+		}
+		
+		private function makePrimitive()
+		{
+			return 
+				Primitive::domDocument('dom')->
+				ofXsd($this->schema);
+			
+		}
+	}
+?>
