628cfc2cbb0a0edcb50f69d1e0d9b7e56ed17727
Denis M. Gabaidulin <gabaden@gmail.com>
2008-05-30 10:14:44 +0000

    + Currency list added
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/1.0@5171 db88c99c-74ec-0310-8720-8416fdbde762

---
+++ main/Base/IsoCurrency.class.php
@@ -0,0 +1,85 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2007 by Dmitry Pismenny                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id: Currency.class.php 37033 2007-11-19 17:39:05Z dedmajor $ */
+
+	final class IsoCurrency extends Enumeration
+	{
+		const RUB	= 643;
+		const USD	= 840;
+		const EUR	= 978;
+		const UAH	= 980;
+		const ILS	= 376;
+		const LVL	= 428;
+		const LTL	= 440;
+		const BYR	= 974;
+		const EEK	= 233;
+		const GBP	= 826;
+		const KZT	= 398;
+		const DKK	= 208;
+		const SEK	= 752;
+		const NOK	= 578;
+		const KGS	= 417;
+		
+		protected $names = array(
+			self::RUB	=> 'Russian Ruble',
+			self::USD	=> 'US Dollar',
+			self::EUR	=> 'Euro',
+			self::UAH 	=> 'Hryvnia',
+			self::ILS 	=> 'New Israeli Sheqel',
+			self::LVL 	=> 'Latvian Lats',
+			self::LTL 	=> 'Lithuanian Litas',
+			self::BYR 	=> 'Belarussian Ruble',
+			self::EEK 	=> 'Kroon',
+			self::GBP 	=> 'Pound Sterling',
+			self::KZT 	=> 'Tenge',
+			self::DKK 	=> 'Danish Krone',
+			self::SEK 	=> 'Swedish Krona',
+			self::NOK 	=> 'Norwegian Krone',
+			self::KGS 	=> 'Som'
+		);
+		
+		protected $signs = array(
+			self::RUB	=> 'руб.',
+			self::USD	=> '$',
+			self::EUR	=> '€',
+			self::UAH 	=> 'грн.',
+			self::ILS 	=> 'ILS',
+			self::LVL 	=> 'LVL',
+			self::LTL 	=> 'LTL',
+			self::BYR 	=> 'BYR',
+			self::EEK 	=> 'EEK',
+			self::GBP 	=> 'GBP',
+			self::KZT 	=> 'KZT',
+			self::DKK 	=> 'DKK',
+			self::SEK 	=> 'SEK',
+			self::NOK 	=> 'NOK',
+			self::KGS 	=> 'KGS'
+		);
+		
+		/**
+		 * @return Currency
+		**/
+		public static function create($id)
+		{
+			return new self($id);
+		}
+		
+		public static function getAnyId()
+		{
+			return self::RUB;
+		}
+		
+		public function getSign()
+		{
+			return $this->signs[$this->id];
+		}
+	}
+?>
