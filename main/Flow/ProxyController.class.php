d10d189157a55d5994dc649d8cce01642e01f110
Konstantin V. Arkhipov <voxus@onphp.org>
2008-06-25 14:03:10 +0000

    + propagated from 1.0
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@5234 db88c99c-74ec-0310-8720-8416fdbde762

---
+++ main/Flow/ProxyController.class.php
@@ -0,0 +1,106 @@
+<?php
+/****************************************************************************
+ *   Copyright (C) 2008 by Dmitry V. Sokolov, Denis M. Gabaidulin           *
+ *                                                                          *
+ *   This program is free software; you can redistribute it and/or modify   *
+ *   it under the terms of the GNU Lesser General Public License as         *
+ *   published by the Free Software Foundation; either version 3 of the     *
+ *   License, or (at your option) any later version.                        *
+ *                                                                          *
+ ****************************************************************************/
+/* $Id$ */
+
+	/**
+	 * @ingroup Flow
+	**/
+	final class ProxyController implements Controller
+	{
+		private $innerController	= null;
+		private $request			= null;
+		private $requestType		= null;
+		private $requestGetter		= null;
+		
+		private static $requestGetterMap = array(
+			RequestType::ATTACHED	=> 'Attached',
+			RequestType::GET		=> 'Get',
+			RequestType::POST		=> 'Post'
+		);
+		
+		/**
+		 * @return ProxyController
+		**/
+		public static function create()
+		{
+			return new self;
+		}
+		
+		public function __construct()
+		{
+			$this->requestType = RequestType::post();
+		}
+		
+		/**
+		 * @return ProxyController
+		**/
+		public function setInner(Controller $controller)
+		{
+			$this->innerController = $controller;
+			
+			return $this;
+		}
+		
+		/**
+		 * @return Controller
+		**/
+		public function getInner()
+		{
+			return $this->innerController;
+		}
+		
+		/**
+		 * @return ModelAndView
+		**/
+		public function handleRequest(HttpRequest $request)
+		{
+			return $this->getInner()->handleRequest($request);
+		}
+		
+		/**
+		 * @return ProxyController
+		**/
+		public function setRequestType(RequestType $requestType)
+		{
+			$this->requestType = $requestType;
+			
+			return $this;
+		}
+		
+		public function isActive($request)
+		{
+			return $this->fetchVariable('controller', $request)
+				? (
+					$this->fetchVariable('controller', $request)
+					== get_class($this->getInner())
+				)
+				: false;
+		}
+		
+		public function getRequestGetter()
+		{
+			Assert::isNotNull($this->requestType);
+			
+			if (!$this->requestGetter)
+				$this->requestGetter =
+					self::$requestGetterMap[$this->requestType->getId()];
+			
+			return $this->requestGetter;
+		}
+		
+		private function fetchVariable($name, HttpRequest $request)
+		{
+			return $request->{'has'.$this->getRequestGetter().'Var'}($name)
+				? $request->{'get'.$this->getRequestGetter().'Var'}($name)
+				: false;
+		}
+	}
+?>
