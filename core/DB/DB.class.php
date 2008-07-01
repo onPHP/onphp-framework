631a2dfd556ae91b64b290780f7b5541a293d676
Konstantin V. Arkhipov <voxus@onphp.org>
2008-07-01 13:58:59 +0000

    * be more universal
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@5255 db88c99c-74ec-0310-8720-8416fdbde762

--- core/DB/DB.class.php
+++ core/DB/DB.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2004-2007 by Konstantin V. Arkhipov                     *
+ *   Copyright (C) 2004-2008 by Konstantin V. Arkhipov                     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Lesser General Public License as        *
@@ -112,7 +112,7 @@
 			/* AccessMode */ $mode = null
 		)
 		{
-			$begin = 'start transaction';
+			$begin = 'begin';
 			
 			if ($level && $level instanceof IsolationLevel)
 				$begin .= ' '.$level->toString();
