c91f6bece5509f160d59e7dcc15152fe856b1227
Konstantin V. Arkhipov <voxus@onphp.org>
2008-07-16 22:19:50 +0000

    + more dao workers to test * correct place for dropWorkers()
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@5297 db88c99c-74ec-0310-8720-8416fdbde762

--- test/AllTests.php
+++ test/AllTests.php
@@ -25,6 +25,7 @@
 			if (AllTests::$workers) {
 				$worker = array_pop(AllTests::$workers);
 				echo "\nProcessing with {$worker}\n";
+				Cache::dropWorkers();
 				Cache::setDefaultWorker($worker);
 			} else {
 				$this->markTestSuiteSkipped('No more workers available.');
@@ -33,7 +34,6 @@
 		
 		public function tearDown()
 		{
-			Cache::dropWorkers();
 			echo "\n";
 		}
 	}
