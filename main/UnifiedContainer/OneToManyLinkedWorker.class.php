db3e5ff4e440b0f964bac8847b2eb117033bb4f7
Konstantin V. Arkhipov <voxus@onphp.org>
2008-07-17 09:07:31 +0000

    * added missing table while targetizing; fix suggested by Alexey D <alexeydsov@gmail.com>
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.10@5303 db88c99c-74ec-0310-8720-8416fdbde762

--- main/UnifiedContainer/OneToManyLinkedWorker.class.php
+++ main/UnifiedContainer/OneToManyLinkedWorker.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2005-2007 by Konstantin V. Arkhipov                     *
+ *   Copyright (C) 2005-2008 by Konstantin V. Arkhipov                     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Lesser General Public License as        *
@@ -23,7 +23,10 @@
 			return
 				$query->andWhere(
 					Expression::eqId(
-						new DBField($this->container->getParentIdField()),
+						new DBField(
+							$this->container->getParentIdField(),
+							$this->container->getDao()->getTable()
+						),
 						$this->container->getParentObject()
 					)
 				);
