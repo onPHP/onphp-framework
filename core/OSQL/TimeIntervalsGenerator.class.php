6e0ca4546b4d25c3a03ab31a24b6a8a6afc243ff
Ivan Y. Khvostishkov <dedmajor@gmail.com>
2008-07-30 14:28:28 +0000

    time intervals generator
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/1.0@5363 db88c99c-74ec-0310-8720-8416fdbde762

---
+++ core/OSQL/TimeIntervalsGenerator.class.php
@@ -0,0 +1,114 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2008 by Ivan Y. Khvostishkov                            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id$ */
+
+	final class TimeIntervalsGenerator extends QueryIdentification
+	{
+		private $range		= null;
+		private $interval	= null;
+		
+		private $overlapped	= true;
+		
+		private $field		= 'time';
+		
+		public static function create()
+		{
+			return new self;
+		}
+		
+		public function setRange(TimestampRange $range)
+		{
+			$this->range = $range;
+			
+			return $this;
+		}
+		
+		/**
+		 * @return TimestampRange
+		**/
+		public function getRange()
+		{
+			return $this->range;
+		}
+		
+		public function setInterval(IntervalUnit $interval)
+		{
+			$this->interval = $interval;
+			
+			return $this;
+		}
+		
+		public function setOverlapped($overlapped = true)
+		{
+			Assert::isBoolean($overlapped);
+			
+			$this->overlapped = ($overlapped === true);
+			
+			return $this;
+		}
+		
+		public function isOverlapped()
+		{
+			return $this->overlapped;
+		}
+		
+		public function getField()
+		{
+			return $this->field;
+		}
+		
+		public function setField($field)
+		{
+			$this->field = $field;
+			
+			return $this;
+		}
+		
+		/**
+		 * @return IntervalUnit
+		**/
+		public function getInterval()
+		{
+			return $this->interval;
+		}
+		
+		public function toDialectString(Dialect $dialect)
+		{
+			if (!$this->range || !$this->interval)
+				throw new WrongStateException(
+					'define time range and interval units first'
+				);
+			
+			if (!$dialect instanceof PostgresDialect)
+				throw new UnimplementedFeatureException(
+					'only tested with postgres'
+				);
+			
+			$firstIntervalStart =
+				$this->interval->truncate(
+					$this->range->getStart(), !$this->overlapped
+				);
+				
+			$maxIntervals =
+				$this->interval->countInRange(
+					$this->range, $this->overlapped
+				) - 1;
+			
+			$result = "SELECT "
+				."'{$firstIntervalStart->toString()}'::timestamp "
+				."+ '1 {$this->interval->getName()}'::interval * i "
+				."AS ".$dialect->quoteField($this->field)." "
+				."FROM generate_series(0, {$maxIntervals}) AS i";
+			
+			return $result;
+		}
+	}
+?>
