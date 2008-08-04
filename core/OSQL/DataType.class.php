48b6162bb0a0dcbcbfa1d0cc4d894deb5f565c79
Konstantin V. Arkhipov <voxus@onphp.org>
2008-08-04 11:50:22 +0000

    * respect const order
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/1.0@5388 db88c99c-74ec-0310-8720-8416fdbde762

--- core/OSQL/DataType.class.php
+++ core/OSQL/DataType.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2006-2007 by Konstantin V. Arkhipov                     *
+ *   Copyright (C) 2006-2008 by Konstantin V. Arkhipov                     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Lesser General Public License as        *
@@ -34,9 +34,9 @@
 		const DATE				= 0x00000B;
 		const TIME				= 0x000A0C;
 		const TIMESTAMP			= 0x000A0D;
-		const INTERVAL			= 0x00000F;
+		const INTERVAL			= 0x00000E;
 		
-		const BINARY			= 0x00000E;
+		const BINARY			= 0x00000F;
 		
 		// NEXT					= 0x00000H;
 		
