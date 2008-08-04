6a6155c5c1f10560afb6428657b2d99381a44013
Konstantin V. Arkhipov <voxus@onphp.org>
2008-08-04 11:51:28 +0000

    * copyright fixed
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/1.0@5391 db88c99c-74ec-0310-8720-8416fdbde762

--- core/OSQL/SQLRightJoin.class.php
+++ core/OSQL/SQLRightJoin.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2005-2007 by Anton E. Lebedevich                        *
+ *   Copyright (C) 2008 by Ivan Y. Khvostishkov                            *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Lesser General Public License as        *
