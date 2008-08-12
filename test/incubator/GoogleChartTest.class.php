7667afe356cd0660b964ec9643bc8a828c2a09c3
Denis M. Gabaidulin <gabaden@gmail.com>
2008-08-12 21:22:05 +0000

    + GooglePieChart test
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@5416 db88c99c-74ec-0310-8720-8416fdbde762

---
+++ test/incubator/GoogleChartTest.class.php
@@ -0,0 +1,40 @@
+<?php
+	/* $Id$ */
+	
+	final class GoogleChartTest extends TestCase
+	{
+		public function testPieChart()
+		{
+			$chart =
+				GooglePieChart::create()->
+				setSize(
+					GoogleChartSize::create()->
+					setWidth(300)->
+					setHeight(300)
+				)->
+				addPiece(
+					GoogleChartPiece::create()->
+					setTitle('Nokia')->
+					setColor(Color::create('ff0000'))->
+					setValue(40)
+				)->
+				addPiece(
+					GoogleChartPiece::create()->
+					setTitle('Samsung')->
+					setColor(Color::create('336677'))->
+					setValue(35)
+				)->
+				addPiece(
+					GoogleChartPiece::create()->
+					setTitle('Opera')->
+					setColor(Color::create('112200'))->
+					setValue(25)
+				);
+		
+			$this->assertEquals(
+				$chart->toString(),
+				'http://chart.apis.google.com/chart?cht=p&chs=300x300&chco=FF0000,336677,112200,&chd=t:40,35,25&chl=Nokia|Samsung|Opera'
+			);
+		}
+	}
+?>
