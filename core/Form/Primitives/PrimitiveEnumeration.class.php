5a1b8deff3b9fb926bf2fe05a197ab5261afcf2c
Konstantin V. Arkhipov <voxus@onphp.org>
2008-08-28 15:29:24 +0000

    * PrimitiveEnumeration now tries to preserve type for integer values
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/1.0@5457 db88c99c-74ec-0310-8720-8416fdbde762

--- core/Form/Primitives/PrimitiveEnumeration.class.php
+++ core/Form/Primitives/PrimitiveEnumeration.class.php
@@ -1,6 +1,6 @@
 <?php
 /*****************************************************************************
- *   Copyright (C) 2006-2007 by Ivan Y. Khvostishkov, Konstantin V. Arkhipov *
+ *   Copyright (C) 2006-2008 by Ivan Y. Khvostishkov, Konstantin V. Arkhipov *
  *                                                                           *
  *   This program is free software; you can redistribute it and/or modify    *
  *   it under the terms of the GNU Lesser General Public License as          *
@@ -95,6 +95,9 @@
 		
 		protected function castNumber($number)
 		{
+			if (Assert::checkInteger($number))
+				return (int) $number;
+			
 			return $number;
 		}
 	}
