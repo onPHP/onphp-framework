723c7b58fa59bc3cab027f79e0cfa8f79fb860a0
Denis M. Gabaidulin <gabaden@gmail.com>
2008-08-30 11:02:07 +0000

    * DateTest refactored
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@5459 db88c99c-74ec-0310-8720-8416fdbde762

--- test/core/DateTest.class.php
+++ test/core/DateTest.class.php
@@ -7,61 +7,33 @@
 		{
 			$today = Date::makeToday();
 			
-			$this->assertEquals(
-				Date::dayDifference($today, $today),
-				$this->oldDayDifference($today, $today)
-			);
-			
-			$this->assertEquals(Date::dayDifference($today, $today), 0);
+			$this->dayDifferenceTest($today, $today, 0);
 			
 			$timestamp = Timestamp::makeNow();
 			
-			$this->assertEquals(
-				Date::dayDifference($timestamp, $timestamp),
-				$this->oldDayDifference($timestamp, $timestamp)
-			);
-			
-			$this->assertEquals(Date::dayDifference($timestamp, $timestamp), 0);
+			$this->dayDifferenceTest($timestamp, $timestamp, 0);
 			
 			$left = Date::create('2008-01-12');
 			$right = Date::create('2008-01-13');
 			
-			$this->assertEquals(
-				Date::dayDifference($left, $right),
-				$this->oldDayDifference($left, $right)
-			);
-			
-			$this->assertEquals(Date::dayDifference($left, $right), 1);
+			$this->dayDifferenceTest($left, $right, 1);
 			
 			$left = Date::create('2008-01-12');
 			$right = Date::create('2009-01-13');
 			
-			$this->assertEquals(
-				Date::dayDifference($left, $right),
-				$this->oldDayDifference($left, $right)
-			);
-			
-			$this->assertEquals(Date::dayDifference($left, $right), 367);
+			$this->dayDifferenceTest($left, $right, 367);
 			
 			$left = Date::create('2008-01-12');
 			$right = Date::create('2008-01-11');
 			
-			$this->assertEquals(
-				Date::dayDifference($left, $right),
-				$this->oldDayDifference($left, $right)
-			);
-			
-			$this->assertEquals(Date::dayDifference($left, $right), -1);
+			$this->dayDifferenceTest($left, $right, -1);
 			
 			$left = Timestamp::create('2008-01-12 01:23:00');
 			$right = Timestamp::create('2008-01-13 13:01:17');
 			
-			$this->assertEquals(
-				Date::dayDifference($left, $right),
-				$this->oldDayDifference($left, $right)
-			);
+			$this->dayDifferenceTest($left, $right, 1);
 			
-			$this->assertEquals(Date::dayDifference($left, $right), 1);
+			return $this;
 		}
 		
 		private function oldDayDifference(Date $left, Date $right)
@@ -77,6 +49,18 @@
 					$left->getDay(),
 					$left->getYear()
 				);
+		}
+		
+		private function dayDifferenceTest(Date $left, Date $right, $expected)
+		{
+			$this->assertEquals(
+				Date::dayDifference($left, $right),
+				$this->oldDayDifference($left, $right)
+			);
+			
+			$this->assertEquals(Date::dayDifference($left, $right), $expected);
+			
+			return $this;
 		}
 	}
 ?>
