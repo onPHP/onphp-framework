aaa64954db358fead6e89a1b56eeb46a0c27488b
Ivan Y. Khvostishkov <dedmajor@gmail.com>
2008-09-05 10:57:12 +0000

    partial csv implementation
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@5494 db88c99c-74ec-0310-8720-8416fdbde762

---
+++ test/main/CsvTest.class.php
@@ -0,0 +1,47 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2008 by Michael V. Tchervyakov                          *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id$ */
+	
+	final class CsvTest extends PHPUnit_Framework_TestCase
+	{
+		public function testRender()
+		{
+			$array = array(
+				array(1, 2.2, -3, null),
+				array("5\n5", "6'6", '7"7', '8,8')
+			);
+			
+			$string =
+				'1,2.2,-3,'.Csv::CRLF
+				."\"5\n5\",6'6,\"7\"\"7\",\"8,8\"".Csv::CRLF;
+			
+			$csv = Csv::create()->setArray($array);
+			$this->assertEquals($csv->render(), $string);
+		}
+		
+		public function testContentHeader()
+		{
+			$csv = Csv::create(false);
+			
+			$this->assertEquals(
+				$csv->getContentTypeHeader()->toString(),
+				'text/csv; header="absent"'
+			);
+			
+			$csv = Csv::create(true);
+			
+			$this->assertEquals(
+				$csv->getContentTypeHeader()->toString(),
+				'text/csv; header="present"'
+			);
+		}
+	}
+?>
