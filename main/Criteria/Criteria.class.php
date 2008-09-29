8da69d1da5370f6aa5f11444aa332702d1770e57
Konstantin V. Arkhipov <voxus@onphp.org>
2008-09-29 08:31:52 +0000

    * r5519 ressurected with correct regression fix
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@5526 db88c99c-74ec-0310-8720-8416fdbde762

--- main/Criteria/Criteria.class.php
+++ main/Criteria/Criteria.class.php
@@ -15,7 +15,7 @@
 	 * 
 	 * @ingroup Criteria
 	**/
-	final class Criteria implements Stringable, DialectString
+	final class Criteria extends QueryIdentification
 	{
 		private $dao		= null;
 		private $daoClass	= null;
