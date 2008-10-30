518c2b5cfbc2f64044d3978eab19e301ad7b4956
Denis M. Gabaidulin <gabaden@gmail.com>
2008-10-30 23:25:54 +0000

    + line chart test
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@5541 db88c99c-74ec-0310-8720-8416fdbde762

--- test/incubator/GoogleChartTest.class.php
+++ test/incubator/GoogleChartTest.class.php
@@ -30,10 +30,58 @@
 					setColor(Color::create('112200'))->
 					setValue(25)
 				);
-		
+			
 			$this->assertEquals(
 				$chart->toString(),
 				'http://chart.apis.google.com/chart?cht=p&chs=500x300&chco=FF0000,336677,112200&chd=t:40,35,25&chl=Nokia|Samsung|Opera'
+			);
+		}
+		
+		public function testTwoAxisLineChart()
+		{
+			$views =
+				GoogleChartDataSet::create()->
+				setData(array(195, 191, 197, 183, 199, 195));
+			
+			$clicks =
+				GoogleChartDataSet::create()->
+				setData(array(3, 1, 1, 3, 1, 3));
+			
+			$chart =
+				GoogleLineChart::create()->
+				setSize(
+					GoogleChartSize::create()->
+					setWidth(300)->
+					setHeight(300)
+				)->
+				addAxis(
+					GoogleChartAxis::create(
+						new GoogleChartAxisType(GoogleChartAxisType::Y)
+					)->
+					setRange(IntegerSet::create(0, $views->getMax()))
+				)->
+				addAxis(
+					GoogleChartAxis::create(
+						new GoogleChartAxisType(GoogleChartAxisType::R)
+					)->
+					setRange(IntegerSet::create(0, $clicks->getMax()))
+				)->
+				addLine(
+					GoogleChartLine::create()->
+					setTitle('Показы')->
+					setColor(Color::create('336699'))->
+					setValue($views)
+				)->
+				addLine(
+					GoogleChartLine::create()->
+					setTitle('Клики')->
+					setColor(Color::create('996633'))->
+					setValue($clicks)
+				);
+			
+			$this->assertEquals(
+				$chart->toString(),
+				'http://chart.apis.google.com/chart?cht=lc&chs=300x300&chco=336699,996633&chd=t:195,191,197,183,199,195|3,1,1,3,1,3&chds=0,199,0,3&chdl=Показы|Клики&chdlp=b&chxt=y,r&chxr=0,0,199|1,0,3'
 			);
 		}
 	}
