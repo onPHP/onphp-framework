544aec5fec66af41902d05832e1ac48b72862b17
Konstantin V. Arkhipov <voxus@onphp.org>
2008-10-31 22:08:36 +0000

    * cosmetics
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@5551 db88c99c-74ec-0310-8720-8416fdbde762

--- incubator/classes/Routers/RouterStaticRule.class.php
+++ incubator/classes/Routers/RouterStaticRule.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2005-2008 by Sergey S. Sergeev                          *
+ *   Copyright (C) 2008 by Sergey S. Sergeev                               *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Lesser General Public License as        *
@@ -13,38 +13,42 @@
 	class RouterStaticRule extends RouterBaseRule
 	{
 		protected $route	= null;
-
+		
 		/**
 		 * @return RouterStaticRule
 		**/
-		public static function create($route /** [,array $defaults] **/)
+		public static function create($route /* , array $defaults */)
 		{
 			$defaults = array();
 			$list = func_get_args();
-
+			
 			if (!empty($list[1]))
 				$defaults = $list[1];
-
+			
 			return new self($route, $defaults);
 		}
-
+		
 		public function __construct($route, $defaults = array())
 		{
 			$this->route = trim($route, '/');
 			$this->defaults = (array) $defaults;
 		}
-
+		
 		public function match(HttpRequest $request)
 		{
 			$path = $this->processPath($request)->toString();
-
+			
 			if (trim(urldecode($path), '/') == $this->route)
 				return $this->defaults;
-
+			
 			return false;
 		}
-
-		public function assemble($data = array(), $reset = false, $encode = false)
+		
+		public function assemble(
+			$data = array(),
+			$reset = false,
+			$encode = false
+		)
 		{
 			return $this->route;
 		}
