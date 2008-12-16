f9d9dbb66a16d1909730de5571288171acf351c5
Tsyrulnik Y. Viatcheslav <neemah86@gmail.com>
2008-12-16 07:59:24 +0000

    Copyright text fix
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@5635 db88c99c-74ec-0310-8720-8416fdbde762

--- incubator/classes/GeoCoding/GoogleAPI/GoogleGeoCoding.class.php
+++ incubator/classes/GeoCoding/GoogleAPI/GoogleGeoCoding.class.php
@@ -1,9 +1,14 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2008 by Shimizu                                    *
- *   neemah86@gmail.com                                                    *
+ *   Copyright (C) 2008 by Shimizu                                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
  ***************************************************************************/
-/* $id$ */
+/* $Id$ */
 
 	class GoogleGeoCoding
 	{
