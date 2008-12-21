d5e30fca9002aef18c218f5e4dbedbdfee1497d4
Vladlen Y. Koshelev <vlad.kosh@gmail.com>
2008-12-21 04:16:43 +0000

    r5647 propagated
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.10@5648 db88c99c-74ec-0310-8720-8416fdbde762

--- main/Criteria/Criteria.class.php
+++ main/Criteria/Criteria.class.php
@@ -60,6 +60,7 @@
 		{
 			$this->logic = clone $this->logic;
 			$this->order = clone $this->order;
+			$this->projection = clone $this->projection;
 		}
 		
 		public function __sleep()
@@ -227,7 +228,11 @@
 		**/
 		public function addProjection(ObjectProjection $projection)
 		{
-			$this->projection->add($projection);
+			if (
+				!$projection instanceof ProjectionChain
+				|| !$projection->isEmpty()
+			)
+				$this->projection->add($projection);
 			
 			return $this;
 		}
