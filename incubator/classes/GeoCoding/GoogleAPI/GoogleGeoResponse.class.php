5b3722a45f275602b46ba1e0ffc7249762937ccd
Konstantin V. Arkhipov <voxus@onphp.org>
2008-12-29 15:54:37 +0000

    * cosmetics + type-hints
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@5688 db88c99c-74ec-0310-8720-8416fdbde762

--- incubator/classes/GeoCoding/GoogleAPI/GoogleGeoResponse.class.php
+++ incubator/classes/GeoCoding/GoogleAPI/GoogleGeoResponse.class.php
@@ -15,55 +15,76 @@
 		protected $name = null;
 		protected $status = null;
 		protected $placeMarks = null;
-
+		
 		public function getName()
 		{
 			return $this->name;
 		}
-
+		
+		/**
+		 * @return GoogleGeoResponse
+		**/
 		public function setName($name)
 		{
 			$this->name = $name;
 			return $this;
 		}
-
+		
+		/**
+		 * @return GoogleGeoStatusCode
+		**/
 		public function getStatus()
 		{
 			return $this->status;
 		}
-
+		
+		/**
+		 * @return GoogleGeoResponse
+		**/
 		public function setStatus(GoogleGeoStatusCode $status)
 		{
 			$this->status = $status;
 			return $this;
 		}
-
+		
 		public function getPlaceMarks()
 		{
 			return $this->placeMarks;
 		}
-
+		
 		public function getIterator()
 		{
 			return $this->getPlaceMarks();
 		}
-
+		
+		/**
+		 * @return GoogleGeoResponse
+		**/
 		function setPlaceMarks($marks)
 		{
 			$this->placeMarks = $marks;
 			return $this;
 		}
-
+		
+		/**
+		 * @return GoogleGeoResponse
+		**/
 		public static function createFromSimpleXml(SimpleXMLElement $object)
 		{
 			$instance = new GoogleGeoResponse();
-			$instance->setName((string)$object->Response->name);
-			$instance->setStatus(new GoogleGeoStatusCode((int)$object->Response->Status->code));
-			$instance->setPlaceMarks(
-				new GoogleGeoPlacemarkIterator($object->Response->Placemark)
-			);
+			
+			$instance->
+				setName((string) $object->Response->name)->
+				setStatus(
+					new GoogleGeoStatusCode(
+						(int) $object->Response->Status->code
+					)
+				)->
+				setPlaceMarks(
+					new GoogleGeoPlacemarkIterator($object->Response->Placemark)
+				);
+			
 			return $instance;
 		}
-		
 	}
 ?>
