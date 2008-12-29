46409343d823f016e05e4a776c545634fa5f0d13
Konstantin V. Arkhipov <voxus@onphp.org>
2008-12-29 15:53:58 +0000

    + fixme * cosmetics
    git-svn-id: https://svn.shadanakar.org/onPHP/trunk@5681 db88c99c-74ec-0310-8720-8416fdbde762

--- incubator/classes/GeoCoding/GoogleAPI/GoogleGeoAddressAccuracyLevel.class.php
+++ incubator/classes/GeoCoding/GoogleAPI/GoogleGeoAddressAccuracyLevel.class.php
@@ -12,6 +12,7 @@
 
 	final class GoogleGeoAddressAccuracyLevel extends Enumeration
 	{
+		// FIXME: it's a bad practice to use zero as enumeration's value
 		const LEVEL_0 = 0;
 		const LEVEL_1 = 1;
 		const LEVEL_2 = 2;
@@ -24,17 +25,16 @@
 		const LEVEL_9 = 9;
 		
 		protected $names = array(
-			self::LEVEL_0 => "Unknown location.",
-			self::LEVEL_1 => "Country level.",
-			self::LEVEL_2 => "Region (state, province, prefecture, etc.) level.",
-			self::LEVEL_3 => "Sub-region (county, municipality, etc.) level",
-			self::LEVEL_4 => "Town (city, village) level",
-			self::LEVEL_5 => "Post code (zip code) level",
-			self::LEVEL_6 => "Street level",
-			self::LEVEL_7 => "Intersection level",
-			self::LEVEL_8 => "Address level",
-			self::LEVEL_9 => "Premise (building name, property name, shopping center, etc.) level"
+			self::LEVEL_0 => 'Unknown location.',
+			self::LEVEL_1 => 'Country level.',
+			self::LEVEL_2 => 'Region (state, province, prefecture, etc.) level.',
+			self::LEVEL_3 => 'Sub-region (county, municipality, etc.) level',
+			self::LEVEL_4 => 'Town (city, village) level',
+			self::LEVEL_5 => 'Post code (zip code) level',
+			self::LEVEL_6 => 'Street level',
+			self::LEVEL_7 => 'Intersection level',
+			self::LEVEL_8 => 'Address level',
+			self::LEVEL_9 => 'Premise (building name, property name, shopping center, etc.) level'
 		);
-		
 	}
 ?>
