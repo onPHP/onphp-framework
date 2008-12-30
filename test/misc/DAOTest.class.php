43bc5f422538842022137443e1138c39de13c765
Konstantin V. Arkhipov <voxus@onphp.org>
2008-12-30 14:05:49 +0000

    - partial revert of r5698, since there is no support for binary data yet
    git-svn-id: https://svn.shadanakar.org/onPHP/branches/0.10@5700 db88c99c-74ec-0310-8720-8416fdbde762

--- test/misc/DAOTest.class.php
+++ test/misc/DAOTest.class.php
@@ -20,7 +20,6 @@
 				Cache::me()->clean();
 				$this->getSome();
 				
-				$this->nonIntegerIdentifier();
 				$this->persistenceInIdentityMap();
 				
 				$this->racySave();
@@ -145,32 +144,6 @@
 				$this->getListByIdsTest();
 				$this->getListByIdsTest();
 			}
-		}
-		
-		public function nonIntegerIdentifier()
-		{
-			$id = 'non-integer-one';
-			
-			$bin =
-				TestBinaryStuff::create()->
-				setId($id)->
-				setData("\0!bbq!\0");
-			
-			try {
-				TestBinaryStuff::dao()->import($bin);
-			} catch (DatabaseException $e) {
-				die();
-			}
-			
-			Cache::me()->clean();
-			
-			$prm = Primitive::identifier('id')->of('TestBinaryStuff');
-			
-			$this->assertTrue($prm->import(array('id' => $id)));
-			$this->assertIdentical($prm->getValue()->getId(), $id);
-			
-			$this->assertEqual(TestBinaryStuff::dao()->getById($id), $bin);
-			$this->assertEqual(TestBinaryStuff::dao()->dropById($id), 1);
 		}
 		
 		protected function getSome()
