50475741e40bd937ce0389a91ed3deca7d35ed74
Konstantin V. Arkhipov <voxus@onphp.org>
2009-01-19 01:22:36 +0300

    * inet meta type is now sizeless

--- meta/types/InetType.class.php
+++ meta/types/InetType.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2007 by Konstantin V. Arkhipov                          *
+ *   Copyright (C) 2007-2009 by Konstantin V. Arkhipov                     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Lesser General Public License as        *
@@ -18,6 +18,11 @@
 		public function getPrimitiveName()
 		{
 			return 'inet';
+		}
+		
+		public function getSize()
+		{
+			return null;
 		}
 		
 		/**
