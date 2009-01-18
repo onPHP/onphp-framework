0132160773416b4e08216f0f56349c8bf04a5e7d
Vladlen Y. Koshelev <vlad.kosh@gmail.com>
2009-01-19 02:48:10 +0300

    * special handling for '0'

--- core/Form/Primitives/PrimitiveString.class.php
+++ core/Form/Primitives/PrimitiveString.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2004-2007 by Konstantin V. Arkhipov, Sveta A. Smirnova  *
+ *   Copyright (C) 2004-2009 by Konstantin V. Arkhipov, Sveta A. Smirnova  *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Lesser General Public License as        *
@@ -39,9 +39,13 @@
 			
 			$this->selfFilter();
 			
-			if (!empty($this->value) && is_string($this->value)
-				&& !($this->max && mb_strlen($this->value) > $this->max)
-				&& !($this->min && mb_strlen($this->value) < $this->min)
+			if (
+				is_string($this->value)
+				// zero is quite special value here
+				&& ($this->value === '0' || !empty($this->value))
+				&& ($length = mb_strlen($this->value))
+				&& !($this->max && $length > $this->max)
+				&& !($this->min && $length < $this->min)
 				&& (!$this->pattern || preg_match($this->pattern, $this->value))
 			) {
 				return true;
