bdc55d3a712537e0b9a4050050392cec35472bfa
Denis M. Gabaidulin <gabaden@gmail.com>
2009-01-19 23:44:42 +0300

    + unserialize filter

---
+++ core/Form/Filters/UnserializeFilter.class.php
@@ -0,0 +1,32 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2009 by Denis M. Gabaidulin                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+
+	/**
+	 * Unserialize string
+	 * 
+	 * @ingroup Filters
+	**/
+	final class UnserializeFilter extends BaseFilter
+	{
+		/**
+		 * @return UnserializeFilter
+		**/
+		public static function me()
+		{
+			return Singleton::getInstance(__CLASS__);
+		}
+		
+		public function apply($value)
+		{
+			return unserialize($value);
+		}
+	}
+?>
