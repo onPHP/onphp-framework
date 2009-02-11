eaad01b3c543dbb17a12870d13e1e1352825ab74
Konstantin V. Arkhipov <voxus@onphp.org>
2009-02-11 22:38:35 +0300

    - redundant interface

--- incubator/classes/Charts/Google/GoogleChartAxisType.class.php
+++ incubator/classes/Charts/Google/GoogleChartAxisType.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2008 by Denis M. Gabaidulin                             *
+ *   Copyright (C) 2008-2009 by Denis M. Gabaidulin                        *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Lesser General Public License as        *
@@ -13,7 +13,7 @@
 	/**
 	 * @ingroup GoogleChart
 	**/
-	final class GoogleChartAxisType extends Enumeration implements Stringable
+	final class GoogleChartAxisType extends Enumeration
 	{
 		const X	= 0x1;
 		const Y	= 0x2;
