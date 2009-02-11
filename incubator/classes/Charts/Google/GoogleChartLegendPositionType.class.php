e9c6c7a0ad69346cc53175b17f53cf4e8f1bf53e
Konstantin V. Arkhipov <voxus@onphp.org>
2009-02-11 22:49:06 +0300

    - redundant interface

--- incubator/classes/Charts/Google/GoogleChartLegendPositionType.class.php
+++ incubator/classes/Charts/Google/GoogleChartLegendPositionType.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2008 by Denis M. Gabaidulin                             *
+ *   Copyright (C) 2008-2009 by Denis M. Gabaidulin                        *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Lesser General Public License as        *
@@ -14,7 +14,6 @@
 	 * @ingroup GoogleChart
 	**/
 	final class GoogleChartLegendPositionType extends Enumeration
-		implements Stringable
 	{
 		const BOTTOM	= 0x1;
 		const TOP		= 0x2;
