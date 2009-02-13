e96688c30dd66b837ce79e0cf6867b92db8d0010
dedmajor <dedmajor@mailib.oemdesign.ru>
2009-02-13 22:22:27 +0300

    clean redirect view

---
+++ main/Flow/CleanRedirectView.class.php
@@ -0,0 +1,48 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2006-2007 by Konstantin V. Arkhipov                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id$ */
+
+	/**
+	 * @ingroup Flow
+	**/
+	class CleanRedirectView implements View
+	{
+		protected $url = null;
+		
+		public function __construct($url)
+		{
+			$this->url = $url;
+		}
+		
+		/**
+		 * @return CleanRedirectView
+		**/
+		public static function create($url)
+		{
+			return new self($url);
+		}
+		
+		public function render($model = null)
+		{
+			HeaderUtils::redirectRaw($this->getLocationUrl($model));
+		}
+		
+		public function getUrl()
+		{
+			return $this->url;
+		}
+		
+		protected function getLocationUrl($model = null)
+		{
+			return $this->getUrl();
+		}
+	}
+?>
