8fd260d08340676686a0f82e44e3787af12f7a8a
Konstantin V. Arkhipov <voxus@onphp.org>
2009-02-18 20:43:52 +0300

    + correct author

--- main/Flow/CleanRedirectView.class.php
+++ main/Flow/CleanRedirectView.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2006-2007 by Konstantin V. Arkhipov                     *
+ *   Copyright (C) 2009 by Ivan Y. Khvostishkov                            *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Lesser General Public License as        *
