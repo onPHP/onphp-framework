e2723cff2ab10b003f57c6bfd18a2eff13988eb1
Vladlen Y. Koshelev <vlad.kosh@gmail.com>
2009-02-25 16:15:05 +0300

    * extends BaseException

--- core/Exceptions/SoapFault.class.php
+++ core/Exceptions/SoapFault.class.php
@@ -1,6 +1,6 @@
 <?php
 /****************************************************************************
- *   Copyright (C) 2008 by Konstantin V. Arkhipov                           *
+ *   Copyright (C) 2008-2009 by Konstantin V. Arkhipov                      *
  *                                                                          *
  *   This program is free software; you can redistribute it and/or modify   *
  *   it under the terms of the GNU Lesser General Public License as         *
@@ -15,5 +15,5 @@
 	 * 
 	 * @ingroup Exceptions
 	**/
-	class SoapFault extends Exception {/*_*/}
+	class SoapFault extends BaseException {/*_*/}
 ?>
