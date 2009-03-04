f7c212d71feda8bd94e5e4ba13af790e0346f84a
Konstantin V. Arkhipov <voxus@onphp.org>
2009-03-04 20:17:36 +0300

    - typo, spotted by Denis Shumeev <denis@lemon-tree.ru>

--- main/DAOs/Workers/NullDaoWorker.class.php
+++ main/DAOs/Workers/NullDaoWorker.class.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2005-2008 by Konstantin V. Arkhipov                     *
+ *   Copyright (C) 2005-2009 by Konstantin V. Arkhipov                     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Lesser General Public License as        *
@@ -88,7 +88,7 @@
 		
 		public function getCustomRowList(SelectQuery $query)
 		{
-			return parent::getCustomList($query, Cache::DO_NOT_CACHE);
+			return parent::getCustomRowList($query, Cache::DO_NOT_CACHE);
 		}
 		//@}
 		
