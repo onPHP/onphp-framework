573962e1b9750ee1e73cb86d19813dfc0ffb53c2
Vladlen Y. Koshelev <vlad.kosh@gmail.com>
2009-03-10 19:54:48 +0300

    + PostgresDatabaseException with string representation of error code

---
+++ core/Exceptions/PostgresDatabaseException.class.php
@@ -0,0 +1,24 @@
+<?php
+/****************************************************************************
+ *   Copyright (C) 2009 by Vladlen Y. Koshelev                              *
+ *                                                                          *
+ *   This program is free software; you can redistribute it and/or modify   *
+ *   it under the terms of the GNU Lesser General Public License as         *
+ *   published by the Free Software Foundation; either version 3 of the     *
+ *   License, or (at your option) any later version.                        *
+ *                                                                          *
+ ****************************************************************************/
+/* $Id$ */
+
+	/**
+	 * @ingroup Exceptions
+	**/
+	class PostgresDatabaseException extends DatabaseException
+	{
+		public function __construct($message = null, $code = null)
+		{
+			parent::__construct($message);
+			$this->code = $code;
+		}
+	}
+?>
