b5ff91a247470f925d8b6ba8c38e74b6028ab07f
Vladlen Y. Koshelev <vlad.kosh@gmail.com>
2009-03-23 15:00:29 +0300

    * year fixed

--- main/OQL/Parsers/OqlTokenType.class.php
+++ main/OQL/Parsers/OqlTokenType.class.php
@@ -1,6 +1,6 @@
 <?php
 /****************************************************************************
- *   Copyright (C) 2008-2009 by Vladlen Y. Koshelev                         *
+ *   Copyright (C) 2009 by Vladlen Y. Koshelev                              *
  *                                                                          *
  *   This program is free software; you can redistribute it and/or modify   *
  *   it under the terms of the GNU Lesser General Public License as         *
