f799d54d6081e1175c82c83fe4f70b6f0e6898af
Vladlen Y. Koshelev <vlad.kosh@gmail.com>
2009-03-26 01:25:57 +0300

    - typo * cosmetics

--- test/main/DateUtilsTest.class.php
+++ test/main/DateUtilsTest.class.php
@@ -1,9 +1,9 @@
 <?php
 	final class DateUtilsTest extends PHPUnit_Framework_TestCase
 	{
-    	/**
-     	* @dataProvider alignToSecondsDataProvider
-     	*/
+		/**
+		 * @dataProvider alignToSecondsDataProvider
+		**/
 		public function testAlignToSeconds(Timestamp $stamp, $expected)
 		{
 			$this->assertEquals(
@@ -12,26 +12,26 @@
 			);
 		}
 		
-		public function alignToSecondsDataProvider()
-	    {
-	        return array(
-	          array(
-	          	Timestamp::create('2009-01-01 10:00:42'),
-	          	'2009-01-01 10:00:42'
-	          ),
-	          array(
-	          	Timestamp::create('2009-01-01 10:00:41'),
-	          	'2009-01-01 10:00:00'
-	          ),
-	          array(
-				Timestamp::create('2009-01-01 10:01:34'),
-	          	'2009-01-01 10:01:24'
-	          ),
-	          array(
-				Timestamp::create('2009-01-01 10:10:01'),
-	          	'2009-01-01 10:09:48'
-	          )
-	        );
-	    }
+		public static function alignToSecondsDataProvider()
+		{
+			return array(
+				array(
+					Timestamp::create('2009-01-01 10:00:42'),
+					'2009-01-01 10:00:42'
+				),
+				array(
+					Timestamp::create('2009-01-01 10:00:41'),
+					'2009-01-01 10:00:00'
+				),
+				array(
+					Timestamp::create('2009-01-01 10:01:34'),
+					'2009-01-01 10:01:24'
+				),
+				array(
+					Timestamp::create('2009-01-01 10:10:01'),
+					'2009-01-01 10:09:48'
+				)
+			);
+		}
 	}
 ?>
