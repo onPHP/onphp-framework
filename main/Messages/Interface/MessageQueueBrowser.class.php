a59a0635d157f8e62d8be4bd9a28fb38abd46de5
Ivan Y. Khvostishkov <dedmajor@oemdesign.ru>
2009-04-01 23:26:28 +0400

    simple messaging api

---
+++ main/Messages/Interface/MessageQueueBrowser.class.php
@@ -0,0 +1,23 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2009 by Ivan Y. Khvostishkov                            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id$ */
+
+
+	interface MessageQueueBrowser
+	{
+		/**
+		 * @return Message
+		 */
+		public function getNextMessage();
+	}
+
+?>
+
