bdb4e1d9a328a61bdf8feeff39ff681794b42a99
Vladlen Y. Koshelev <vlad.kosh@gmail.com>
2009-04-02 13:53:24 +0400

    + optional rule

---
+++ main/OQL/Parsers/Strategies/OqlOptionalRuleParseStrategy.class.php
@@ -0,0 +1,40 @@
+<?php
+/****************************************************************************
+ *   Copyright (C) 2009 by Vladlen Y. Koshelev                              *
+ *                                                                          *
+ *   This program is free software; you can redistribute it and/or modify   *
+ *   it under the terms of the GNU Lesser General Public License as         *
+ *   published by the Free Software Foundation; either version 3 of the     *
+ *   License, or (at your option) any later version.                        *
+ *                                                                          *
+ ****************************************************************************/
+
+	/**
+	 * @ingroup OQL
+	**/
+	class OqlOptionalRuleParseStrategy extends OqlGrammarRuleParseStrategy
+	{
+		/**
+		 * @return OqlOptionalRuleParseStrategy
+		**/
+		public static function me()
+		{
+			return Singleton::getInstance(__CLASS__);
+		}
+		
+		/**
+		 * @return OqlSyntaxNode
+		**/
+		public function parse(
+			OqlGrammarRule $rule,
+			OqlTokenizer $tokenizer,
+			$silent = false
+		)
+		{
+			Assert::isTrue($rule instanceof OqlOptionalRule);
+			Assert::isNotNull($innerRule = $rule->getRule());
+			
+			return $innerRule->process($tokenizer, true);
+		}
+	}
+?>
