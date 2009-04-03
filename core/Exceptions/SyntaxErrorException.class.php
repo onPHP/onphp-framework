16b7d397be323959a922f2cdb282d79730aecb4d
Vladlen Y. Koshelev <vlad.kosh@gmail.com>
2009-04-03 18:56:13 +0400

    * default code is null

--- core/Exceptions/SyntaxErrorException.class.php
+++ core/Exceptions/SyntaxErrorException.class.php
@@ -1,6 +1,6 @@
 <?php
 /****************************************************************************
- *   Copyright (C) 2008 by Vladlen Y. Koshelev                              *
+ *   Copyright (C) 2008-2009 by Vladlen Y. Koshelev                         *
  *                                                                          *
  *   This program is free software; you can redistribute it and/or modify   *
  *   it under the terms of the GNU Lesser General Public License as         *
@@ -19,7 +19,7 @@
 		private $errorPosition	= null;
 		
 		public function __construct(
-			$message, $errorLine = null, $errorPosition = null, $code = 0
+			$message, $errorLine = null, $errorPosition = null, $code = null
 		)
 		{
 			parent::__construct($message, $code);
