1c34ee378c890b27df2841f0872c3203a8c10c28
Vladlen Y. Koshelev <vlad.kosh@gmail.com>
2009-04-07 03:22:32 +0400

    + in expression mutator

---
+++ incubator/classes/OQL2/SyntaxTree/Mutators/OqlInExpressionNodeMutator.class.php
@@ -0,0 +1,54 @@
+<?php
+/****************************************************************************
+ *   Copyright (C) 2009 by Vladlen Y. Koshelev                              *
+ *                                                                          *
+ *   This program is free software; you can redistribute it and/or modify   *
+ *   it under the terms of the GNU Lesser General Public License as         *
+ *   published by the Free Software Foundation; either version 3 of the     *
+ *   License, or (at your option) any later version.                        *
+ *                                                                          *
+ ****************************************************************************/
+
+	/**
+	 * @ingroup OQL
+	**/
+	final class OqlInExpressionNodeMutator extends OqlSyntaxNodeMutator
+	{
+		/**
+		 * @return OqlInExpressionNodeMutator
+		**/
+		public static function me()
+		{
+			return Singleton::getInstance(__CLASS__);
+		}
+		
+		/**
+		 * @return OqlLogicalObjectNode
+		**/
+		public function process(OqlSyntaxNode $node)
+		{
+			$iterator = OqlSyntaxTreeDeepRecursiveIterator::me();
+			
+			if (($field = $iterator->reset($node)) === null)
+				return $node;
+			
+			if (($operator = $iterator->next()) === null)
+				return $node;
+			
+			$recursiveIterator = OqlSyntaxTreeRecursiveIterator::me();
+			$recursiveIterator->reset($operator);
+			$values = array();
+			
+			while ($value = $recursiveIterator->next()) {
+				if ($value->toValue() !== ',')
+					$values[] = $value->toValue();
+			}
+			
+			return OqlLogicalObjectNode::create()->setObject(
+				$operator->toValue() == InExpression::IN
+					? Expression::in($field->toValue(), $values)
+					: Expression::notIn($field->toValue(), $values)
+			);
+		}
+	}
+?>
