ded00d3a4956eae8f5d91d1d40e1a14cd067ad86
Denis M. Gabaidulin <gabaden@gmail.com>
2009-04-07 14:31:09 +0400

    + float range implementation

---
+++ test/main/FloatRangeTest.class.php
@@ -0,0 +1,36 @@
+<?php
+	final class FloatRangeTest extends TestCase
+	{
+		/**
+		 * @dataProvider rangeDataProvider
+		**/
+		public function testCreation($min, $max, $throwsException)
+		{
+			if ($throwsException)
+				$this->setExpectedException('WrongArgumentException');
+			
+			$range = FloatRange::create($min, $max);
+		} 
+		
+		public static function rangeDataProvider()
+		{
+			return array(
+				array(
+					1, 1, false
+				),
+				array(
+					1, 222222222222222222222222222, false
+				),
+				array(
+					0.1, 1, false
+				),
+				array(
+					0, 1, false
+				),
+				array(
+					2, 1, false
+				)
+			);
+		}
+	}
+?>		
---
+++ test/main/RangeTest.class.php
@@ -0,0 +1,36 @@
+<?php
+	final class RangeTest extends TestCase
+	{
+		/**
+		 * @dataProvider rangeDataProvider
+		**/
+		public function testCreation($min, $max, $throwsException)
+		{
+			if ($throwsException)
+				$this->setExpectedException('WrongArgumentException');
+			
+			$range = Range::create($min, $max);
+		} 
+		
+		public static function rangeDataProvider()
+		{
+			return array(
+				array(
+					1, 1, false
+				),
+				array(
+					1, 222222222222222222222222222, true
+				),
+				array(
+					0.1, 1, true
+				),
+				array(
+					0, 1, false
+				),
+				array(
+					2, 1, false
+				)
+			);
+		}
+	}
+?>		
