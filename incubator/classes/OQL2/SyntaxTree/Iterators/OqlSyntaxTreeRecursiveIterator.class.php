64fcbbff86380740853ccd395ca6fa17f765f7ef
Vladlen Y. Koshelev <vlad.kosh@gmail.com>
2009-04-07 17:53:40 +0400

    * iterator fixed

--- incubator/classes/OQL2/SyntaxTree/Iterators/OqlSyntaxTreeRecursiveIterator.class.php
+++ incubator/classes/OQL2/SyntaxTree/Iterators/OqlSyntaxTreeRecursiveIterator.class.php
@@ -14,7 +14,8 @@
 	**/
 	final class OqlSyntaxTreeRecursiveIterator extends OqlSyntaxTreeIterator
 	{
-		private $stack = array();
+		private $stack		= array();
+		private $visited	= array();
 		
 		/**
 		 * @return OqlSyntaxTreeRecursiveIterator
@@ -29,7 +30,8 @@
 		**/
 		public function reset(OqlSyntaxNode $node)
 		{
-			$this->stack = array();
+			$this->stack = array($node);
+			$this->visited = array($node->getId() => true);
 			
 			return parent::reset($node);
 		}
@@ -39,29 +41,55 @@
 		**/
 		public function next()
 		{
-			if ($this->node === null)
-				return null;
-			
-			$node = $this->node;
+			$node = null;
 			
 			do {
-				if ($child = $node->getFirstChild()) {
-					array_push($this->stack, $node);
-					$node = $child;
-				} else {
-					$node = $node->getNextSibling();
+				if ($node === null)
+					$node = array_pop($this->stack);
+				
+				if ($node === null)
+					break;
+				
+				$nextNode = $node;
+				if (
+					($child = $nextNode->getFirstChild())
+					&& !$this->isNodeVisited($child)
+				) {
+					array_push($this->stack, $nextNode);
+					$nextNode = $child;
+				
+				} elseif (
+					($nextNode = $nextNode->getNextSibling())
+					&& $this->isNodeVisited($nextNode)
+				) {
+					$nextNode = null;
 				}
 				
-				if (
-					$node === null
-					&& ($node = array_pop($this->stack))
-				) {
-					$node = $node->getNextSibling();
+				if ($nextNode === null) {
+					$node = $node->getParent();
+				} else {
+					$node = $nextNode;
+					$this->markNodeVisited($node);
 				}
 			
 			} while ($node instanceof OqlNonterminalNode);
 			
 			return $this->node = $node;
+		}
+		
+		/**
+		 * @return OqlSyntaxTreeRecursiveIterator
+		**/
+		private function markNodeVisited(OqlSyntaxNode $node)
+		{
+			$this->visited[$node->getId()] = true;
+			
+			return $this;
+		}
+		
+		private function isNodeVisited(OqlSyntaxNode $node)
+		{
+			return isset($this->visited[$node->getId()]);
 		}
 	}
 ?>
