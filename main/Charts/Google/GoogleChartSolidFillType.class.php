f3bcffeb45a5f99b86403fb6964b667e2846ee91
Denis M. Gabaidulin <gabaden@gmail.com>
2009-04-13 16:02:08 +0400

    + solid fill parameter

---
+++ main/Charts/Google/GoogleChartSolidFillType.class.php
@@ -0,0 +1,40 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2009 by Denis M. Gabaidulin                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+
+	/**
+	 * @ingroup GoogleChart
+	**/
+	final class GoogleChartSolidFillType extends Enumeration
+	{
+		const CHART_AREA		= 0x1;
+		const BACKGROUND		= 0x2;
+		const TRANSPARENCY		= 0x3;
+		
+		protected $names = array(
+			self::CHART_AREA 	=> 'c',
+			self::BACKGROUND	=> 'b',
+			self::TRANSPARENCY	=> 'a'
+		);
+		
+		/**
+		 * @return GoogleChartSolidFill
+		**/
+		public static function create()
+		{
+			return new self;
+		}
+		
+		public function toString()
+		{
+			return $this->name;
+		}
+	}
+?>
