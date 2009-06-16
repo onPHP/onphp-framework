d9b778f9465eb6618731ca135f3d5bb6b788d282
Sergey S. Sergeev <webs.support@gmail.com>
2009-06-16 18:55:38 +0400

    + hstore type implemented

---
+++ meta/types/PgHstoreType.class.php
@@ -0,0 +1,42 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2009 by Sergey S. Sergeev                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+/* $Id$ */
+
+	/**
+	 * @ingroup Types
+	 * @see http://www.postgresql.org/docs/8.3/interactive/hstore.html
+	**/
+	final class PgHstoreType extends BasePropertyType
+	{
+		public function getPrimitiveName()
+		{
+			return 'hstore';
+		}
+		
+		public function isMeasurable()
+		{
+			return true;
+		}
+		
+		public function getDeclaration()
+		{
+			if ($this->hasDefault())
+				return "'{$this->default}'";
+		
+			return 'null';
+		}
+		
+		public function toColumnType()
+		{
+			return 'DataType::create(DataType::PGHSTORE)';
+		}
+	}
+?>
