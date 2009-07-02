a3593fb0f8bf12a986b37c94382a04f17a4cad3f
Denis M. Gabaidulin <gabaden@gmail.com>
2009-07-02 10:56:13 +0400

    + Comparator interface

---
+++ main/Base/Comparator.class.php
@@ -0,0 +1,17 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2009 by Denis M. Gabaidulin                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+
+	interface Comparator
+	{
+		public function compare($one, $two);
+	}
+
+?>
