a30417644a9ef17da523af95e1d5236b441c19c4
Denis M. Gabaidulin <gabaden@gmail.com>
2009-08-30 23:20:16 +0400

    * chlog merged

---
+++ main/Base/DateObjectComparator.class.php
@@ -0,0 +1,31 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2009 by Denis M. Gabaidulin                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+
+	final class DateObjectComparator extends Singleton
+		implements Comparator, Instantiatable
+	{
+		public static function me()
+		{
+			return Singleton::getInstance(__CLASS__);
+		}
+
+		public function compare(Date $one, Date $two)
+		{
+			$stamp1 = $one->toStamp();
+			$stamp2 = $two->toStamp();
+
+			if ($stamp1 == $stamp2)
+				return 0;
+
+			return ($stamp1 < $stamp2) ? -1 : 1;
+		}
+	}
+?>
