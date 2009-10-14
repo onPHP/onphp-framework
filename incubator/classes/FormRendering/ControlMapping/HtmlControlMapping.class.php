cd04138314d8e94036f46a46da5ccfec7565f55b
Petr A. Korobeinikov <petruha@petruha.(none)>
2009-10-14 14:41:26 +0000

    Some improvements of FormRendering.

--- incubator/classes/FormRendering/ControlMapping/HtmlControlMapping.class.php
+++ incubator/classes/FormRendering/ControlMapping/HtmlControlMapping.class.php
@@ -2,7 +2,16 @@
 
 	final class HtmlControlMapping extends BaseControlMapping
 	{
-		
+		public function getList()
+		{
+			return array(
+				'PrimitiveIntegerIdentifier'  => HtmlControl::hidden(),
+				'PrimitiveString'             => HtmlControl::text(),
+				'PrimitiveInteger'            => HtmlControl::text(),
+				'PrimitiveFile'               => HtmlControl::file(),
+				'PrimitiveChoice'             => HtmlControl::select(),
+			);
+		}
 	}
 
 ?>
--- incubator/classes/FormRendering/FormRenderer.class.php
+++ incubator/classes/FormRendering/FormRenderer.class.php
@@ -5,6 +5,11 @@
 		private $form            = null;
 		private $controlMapping  = null;
 
+		public static function me()
+		{
+			return self::getInstance(__CLASS__);
+		}
+
 		public function setForm(Form $form)
 		{
 			$this->form = $form;
@@ -31,7 +36,23 @@
 
 		public function render()
 		{
-			//implement me
+			//echo 'Rendering form with '.get_class($this->controlMapping).' mapping.';
+			//echo '<pre>'.DebugUtils::ec($this->form->getList());
+
+			foreach ($this->form->getList() as $primitive) {
+				$class = get_class($primitive);
+				$mappingList = $this->controlMapping->
+					getList();
+
+				//$output .= $class . ' - render to - ' . $mappingList[$class] . '<br>';
+
+				echo '<div style="padding: 3px;"><label for="" style="width: 150px; display: block; float: left;">'._($primitive->getName()).':</label>';
+				echo $mappingList[$class];
+				echo '</div>';
+				
+			}
+
+			echo HtmlControl::submit();
 		}
 	}
 
