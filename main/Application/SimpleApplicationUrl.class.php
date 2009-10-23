ba65e9ee00a3a2e7cdca0e23c8aa427e6657e5e6
Denis M. Gabaidulin <gabaden@gmail.com>
2009-10-23 22:40:30 +0400

    + simple app url impl

---
+++ main/Application/SimpleApplicationUrl.class.php
@@ -0,0 +1,38 @@
+<?php
+	final class SimpleApplicationUrl extends ApplicationUrl
+	{
+		/**
+		 * @return SimpleApplicationUrl
+		**/
+		public static function create()
+		{
+			return new self;
+		}
+		
+		/**
+		 * @return SimpleApplicationUrl
+		**/
+		public function setPathByRequestUri($requestUri, $normalize = true)
+		{
+			throw new UnimplementedFeatureException(__CLASS__.'::setPathByRequestUri');
+		}
+		
+		public function href($url, $absolute = null)
+		{
+			if ($absolute === null)
+				$absolute = $this->absolute;
+			
+			$baseUrl = $this->base->getPath().$url;
+			
+			if ($this->applicationScope)
+				$baseUrl .=
+					$this->getArgSeparator()
+					.$this->buildQuery($this->applicationScope);
+			
+			if ($absolute)
+				$baseUrl = 'http://'.$baseUrl;
+			
+			return rtrim($baseUrl, '?');
+		}
+	}
+?>
