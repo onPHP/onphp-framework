5d102406a9cdd04cf465cec998b132abd0815dd8
Evgeniy V. Kokovikhin <dovg@dovg.ru>
2010-03-23 13:23:11 +0300

    * PrimitiveInet import fixed

---
+++ test/core/PrimitiveInetTest.class.php
@@ -0,0 +1,19 @@
+<?php
+	
+	final class PrimitiveInetTest extends TestCase
+	{
+		public function testInet()
+		{
+			$prm = Primitive::inet('inet');
+			
+			$this->assertTrue($prm->importValue('127.0.0.1'));
+			$this->assertTrue($prm->importValue('254.254.254.254'));
+			$this->assertTrue($prm->importValue('0.0.0.0'));
+			
+			$this->assertFalse($prm->importValue('10.0.0'));
+			$this->assertFalse($prm->importValue('42.42.42.360'));
+			$this->assertFalse($prm->importValue('10.0.256'));
+			
+		}
+	}
+?>
