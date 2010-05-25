6cff1a8b23bcd67d7d4911610fc438106cc3daf8
Evgeniy V. Kokovikhin <dovg@dovg.ru>
2010-05-25 12:55:24 +0400

    * SimpleAggregateCache : new Cache Peer added

--- test/core/CacheTest.class.php
+++ test/core/CacheTest.class.php
@@ -4,11 +4,54 @@
 	final class CacheTest extends TestCase
 	{
 		const QUERIES = 100;
+
+		public function testAggregateCache()
+		{
+			return $this->doTestMemcached(AggregateCache::create());
+		}
+
+		public function testSimpleAggregateCache()
+		{
+			return $this->doTestMemcached(SimpleAggregateCache::create());
+		}
 		
-		public function testMemcached()
+		public function testIntegerChanges()
+		{
+			Cache::me()->set('test_integer', 1);
+
+			for ($i = 0; $i < self::QUERIES; ++$i) {
+				$this->assertEquals(
+					$i + 2,
+					Cache::me()->increment('test_integer', 1)
+				);
+
+				$this->assertEquals($i + 2, Cache::me()->get('test_integer'));
+			}
+
+			$this->assertEquals(
+				self::QUERIES + 1,
+				Cache::me()->get('test_integer')
+			);
+
+			for ($i = 0; $i < self::QUERIES; ++$i) {
+				$this->assertEquals(
+					self::QUERIES - $i,
+					Cache::me()->decrement('test_integer', 1)
+				);
+
+				$this->assertEquals(
+					self::QUERIES - $i,
+					Cache::me()->get('test_integer')
+				);
+			}
+
+			$this->assertEquals(Cache::me()->get('test_integer'), 1);
+		}
+		
+		private function doTestMemcached(AggregateCache $cache)
 		{
 			Cache::setPeer(
-				AggregateCache::create()->
+				$cache->
 					addPeer('low', Memcached::create(), AggregateCache::LEVEL_LOW)->
 					addPeer('normal1', Memcached::create())->
 					addPeer('normal2', Memcached::create())->
@@ -38,39 +81,6 @@
 			
 			$this->assertEquals($oneHit, $twoHit);
 			$this->assertEquals($twoHit, self::QUERIES);
-		}
-		
-		public function testIntegerChanges()
-		{
-			Cache::me()->set('test_integer', 1);
-			
-			for ($i = 0; $i < self::QUERIES; ++$i) {
-				$this->assertEquals(
-					$i + 2,
-					Cache::me()->increment('test_integer', 1)
-				);
-				
-				$this->assertEquals($i + 2, Cache::me()->get('test_integer'));
-			}
-			
-			$this->assertEquals(
-				self::QUERIES + 1,
-				Cache::me()->get('test_integer')
-			);
-			
-			for ($i = 0; $i < self::QUERIES; ++$i) {
-				$this->assertEquals(
-					self::QUERIES - $i,
-					Cache::me()->decrement('test_integer', 1)
-				);
-				
-				$this->assertEquals(
-					self::QUERIES - $i,
-					Cache::me()->get('test_integer')
-				);
-			}
-			
-			$this->assertEquals(Cache::me()->get('test_integer'), 1);
 		}
 	}
 ?>
