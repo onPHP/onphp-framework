3cb84e6efddc5a50b93b30cde2841faf2a6f1408
Evgeniy V. Kokovikhin <dovg@dovg.ru>
2010-12-01 11:53:07 +0300

    * Socket: eat base exception thrown in close()

---
+++ test/main/Utils/SocketTest.class.php
@@ -0,0 +1,26 @@
+<?php
+	final class SocketTest extends TestCase
+	{
+		public function testShutdown()
+		{
+			Socket::create()->
+				setHost('localhost')->
+				setPort(80)->
+				close();
+			
+			$timedOutSocket =
+				Socket::create()->
+					setHost('google.com')->
+					setPort(80)->
+					setTimeout(1)->
+					connect();
+			
+			$timedOutSocket->write("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n");
+			$timedOutSocket->read(256);
+			
+			$timedOutSocket->close();
+			
+			$this->assertEquals(42, 42);
+		}
+	}
+?>
