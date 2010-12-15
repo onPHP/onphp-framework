de091b066b0cd7e19ede1d52ad0e7265c0854863
Evgeniy V. Kokovikhin <dovg@dovg.ru>
2010-12-15 15:28:32 +0300

    + first realisation of PeclMemcached. All tests passed now!

--- core/Cache/PeclMemcached.class.php
+++ core/Cache/PeclMemcached.class.php
@@ -17,23 +17,41 @@
 	 * @ingroup Cache
 	**/
 	final class PeclMemcached extends BaseMemcache
-	{		
+	{
+		/**
+		 * @var Memcached
+		 */
+		private $instance = null;
+		
 		/**
 		 * @return PeclMemcached
 		**/
-		public static function create()
+		public static function create(
+			$host = BaseMemcache::DEFAULT_HOST,
+			$port = BaseMemcache::DEFAULT_PORT,
+			$persistentId = null
+		)
 		{
-			return new self();
+			return new self($persistentId, $host, $port);
 		}
 
-		public function __construct()
+		public function __construct(
+			$host = BaseMemcache::DEFAULT_HOST,
+			$port = BaseMemcache::DEFAULT_PORT,
+			$persistentId = null
+		)
 		{
-			
+			$this->instance = new Memcached($persistentId);
+			$this->instance->addServer($host, $port);
+
+			$this->alive = true;
 		}
 
 		public function __destruct()
 		{
-			
+			$this->instance = null;
+
+			$this->alive = false;
 		}
 
 		/**
@@ -41,44 +59,59 @@
 		**/
 		public function clean()
 		{
-			
+			$this->instance->flush();
+
+			return $this;
 		}
 
 		public function increment($key, $value)
 		{
-			
+			return $this->instance->increment($key, $value);
 		}
 
 		public function decrement($key, $value)
 		{
-			
+			return $this->instance->decrement($key, $value);
 		}
 
 		public function getList($indexes)
 		{
-			
+			$result = $this->instance->getMulti($indexes);
+
+			if ($result !== false)
+				return $result;
+
+			return array();
 		}
 
 		public function get($index)
 		{
+			$result = $this->instance->get($index);
 			
+			if ($result !== false)
+				return $result;
+			
+			if ($this->instance->getResultCode() == Memcached::RES_NOTFOUND)
+				return null;
+
+			return false;
 		}
 
 		public function delete($index)
 		{
-			
+			$this->instance->delete($index);
 		}
 
 		public function append($key, $data)
 		{
-			
+			$this->instance->append($key, $data);
 		}
 
 		protected function store(
 			$action, $key, $value, $expires = Cache::EXPIRES_MEDIUM
 		)
 		{
-			
+			return $this->instance->$action($key, $value, $expires);
 		}
 	}
 ?>
