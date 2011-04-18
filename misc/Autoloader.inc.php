d1ffa8b82300bdd6bbb23f6aa480b5b85db1a1a6
Slava Tsyrulnik <neemah86@gmail.com>
2011-04-18 23:03:06 +0400

    + misc/Autoloader.inc.php: factory for all cache types + misc/BaseException.inc.php: separated file with BasException needed for Autoloader::wholeClassCache() - misc/autoload.*.php: all logic moved to Autoloader.inc.php * global.inc.php.tpl: spl_autoload_register() instead of cache type file inclusion

--- misc/Autoloader.inc.php
+++ misc/Autoloader.inc.php
@@ -1,17 +1,10 @@
 <?php
-/***************************************************************************
- *   Copyright (C) 2008 by Konstantin V. Arkhipov                          *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU Lesser General Public License as        *
- *   published by the Free Software Foundation; either version 3 of the    *
- *   License, or (at your option) any later version.                       *
- *                                                                         *
- ***************************************************************************/
 
-	define('ONPHP_CLASS_CACHE_CHECKSUM', '__occc');
-	
-	/* void */ function __autoload($classname)
+define('ONPHP_CLASS_CACHE_CHECKSUM', '__occc');
+
+abstract class Autoloader
+{
+	public static function classPathCache($classname)
 	{
 		// numeric indexes for directories, literal indexes for classes
 		static $cache 		= null;
@@ -112,4 +105,105 @@
 			}
 		}
 	}
+	
+	public static function noCache($classname)
+	{
+		if (strpos($classname, "\0") !== false) {
+			/* are you sane? */
+			return;
+		}
+		
+		try {
+			include $classname.EXT_CLASS;
+			return /* void */;
+		} catch (BaseException $e) {
+			return __autoload_failed($classname, $e->getMessage());
+		}
+	}
+	
+	public static function wholeClassCache($classname)
+	{
+		// we must duplicate this one here, otherwise any fail will be silent
+		require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'BaseException'.EXT_MOD;
+		
+		static $path = null;
+		static $checksum = null;
+		static $included = array();
+		
+		if (strpos($classname, "\0") !== false) {
+			/* are you sane? */
+			return;
+		}
+		
+		$currentPath = get_include_path();
+		
+		if ($currentPath != $path) {
+			$checksum = crc32($currentPath.ONPHP_VERSION);
+			$path = $currentPath;
+		}
+		
+		$cacheFile = ONPHP_CLASS_CACHE.$checksum.'.occ';
+		
+		if (!isset($included[$cacheFile])) {
+			try {
+				include $cacheFile;
+				$included[$cacheFile] = true;
+			} catch (BaseException $e) {
+				/* ignore */
+			}
+		}
+		
+		if (!class_exists($classname)) {
+			static $pid = null;
+			
+			if (!$pid) {
+				$pid = getmypid();
+				
+				register_shutdown_function(array('Autoloader', 'autoloadCleanup'));
+			}
+			
+			try {
+				$classPath = null;
+				
+				foreach (
+					explode(PATH_SEPARATOR, get_include_path())
+					as $directory
+				) {
+					$location = $directory.'/'.$classname.EXT_CLASS;
+					
+					if (is_readable($location)) {
+						$classPath = $location;
+						break;
+					}
+				}
+				
+				if (!$classPath)
+					throw new BaseException('failed to find requested class');
+				
+				$class = file_get_contents($classPath);
+				
+				eval('?>'.$class);
+			} catch (BaseException $e) {
+				return __autoload_failed($classname, $e->getMessage());
+			}
+			
+			file_put_contents($cacheFile.'-'.$pid, $class, FILE_APPEND);
+			
+			$included[$cacheFile] = true;
+		}
+	}
+		
+	public static function autoloadCleanup()
+	{
+		$pid = getmypid();
+		
+		if ($paths = glob(ONPHP_CLASS_CACHE.'*-'.$pid, GLOB_NOSORT)) {
+			foreach ($paths as $file) {
+				rename($file, ONPHP_CLASS_CACHE.basename($file, '-'.$pid));
+			}
+		}
+	}
+
+	
+}
 ?>
