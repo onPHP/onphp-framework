bac01a36da405da33748ba6dd9c074da301753b7
Evgeniy V. Kokovikhin <dovg@dovg.ru>
2011-07-19 16:04:45 +0400

    + core/Cache/PinbedPeclMemcached.class.php 	one more wrapper

---
+++ core/Cache/PinbedPeclMemcached.class.php
@@ -0,0 +1,47 @@
+<?php
+/****************************************************************************
+ *   Copyright (C) 2011 by Evgeny V. Kokovikhin                             *
+ *                                                                          *
+ *   This program is free software; you can redistribute it and/or modify   *
+ *   it under the terms of the GNU Lesser General Public License as         *
+ *   published by the Free Software Foundation; either version 3 of the     *
+ *   License, or (at your option) any later version.                        *
+ *                                                                          *
+ ****************************************************************************/
+	
+	/**
+	 *  @ingroup Cache
+	**/
+	final class PinbedPeclMemcached extends PeclMemcached
+	{
+		/**
+		 * @return PinbedPeclMemcached 
+		**/
+		public static function create(
+			$host = Memcached::DEFAULT_HOST,
+			$port = Memcached::DEFAULT_PORT
+		)
+		{
+			return new self($host, $port);
+		}
+		
+		public function __construct(
+			$host = Memcached::DEFAULT_HOST,
+			$port = Memcached::DEFAULT_PORT
+		)
+		{
+			if (PinbaClient::isEnabled())
+				PinbaClient::me()->timerStart(
+					'pecl_memcached_'.$host.'_'.$port.'_connect',
+					array('pecl_memcached_connect' => $host.'_'.$port)
+				);
+			
+			parent::__construct($host, $port, $buffer);
+			
+			if (PinbaClient::isEnabled())
+				PinbaClient::me()->timerStop(
+					'pecl_memcached_'.$host.'_'.$port.'_connect'
+				);
+		}
+	}
+?>
