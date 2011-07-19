ca686885cad30ad64fe4610a1bb23283f2e241ed
Evgeniy V. Kokovikhin <dovg@dovg.ru>
2011-07-19 16:10:51 +0400

    forgot to unfinalize

--- core/Cache/PeclMemcached.class.php
+++ core/Cache/PeclMemcached.class.php
@@ -17,7 +17,7 @@
 	 *
 	 * @ingroup Cache
 	**/
-	final class PeclMemcached extends CachePeer
+	class PeclMemcached extends CachePeer
 	{
 		const DEFAULT_PORT		= 11211;
 		const DEFAULT_HOST		= '127.0.0.1';
