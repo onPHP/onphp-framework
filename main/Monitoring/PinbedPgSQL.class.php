c73b814211fce3ec463b3afff3cda974c806e1de
Evgeniy V. Kokovikhin <dovg@dovg.ru>
2011-07-20 10:43:00 +0400

    + main/Monitoring/PinbedPgSQL.class.php: wrapper for PgSql

---
+++ main/Monitoring/PinbedPgSQL.class.php
@@ -0,0 +1,62 @@
+<?php
+/****************************************************************************
+ *   Copyright (C) 2011 by Evgeny V. Kokovikhin                             *
+ *                                                                          *
+ *   This program is free software; you can redistribute it and/or modify   *
+ *   it under the terms of the GNU Lesser General Public License as         *
+ *   published by the Free Software Foundation; either version 3 of the     *
+ *   License, or (at your option) any later version.                        *
+ *                                                                          *
+ ****************************************************************************/
+	
+	/**
+	 *  @ingroup Cache
+	**/
+	final class PinbedPgSQL extends PgSQL
+	{
+		public function connect()
+		{
+			if (PinbaClient::isEnabled())
+				PinbaClient::me()->timerStart(
+					'pg_sql_connect_'.$this->basename,
+					array('pg_sql_connect' => $this->basename)
+				);
+			
+			$result = parent::connect();
+			
+			if (PinbaClient::isEnabled())
+				PinbaClient::me()->timerStop('pg_sql_connect_'.$this->basename);
+			
+			return $result;
+		}
+		
+		public function queryRaw($queryString)
+		{
+			if (PinbaClient::isEnabled()) {
+				$queryLabel = substr($queryString, 0, 5);
+				
+				PinbaClient::me()->timerStart(
+					'pg_sql_query_'.$this->basename,
+					array('pg_sql_query' => $this->basename.'_'.$queryLabel)
+				);
+			}
+			
+			try {
+				$result = parent::queryRaw($queryString);
+				
+				if (PinbaClient::isEnabled())
+					PinbaClient::me()->timerStop('pg_sql_query_'.$this->basename);
+				
+				return $result;
+				
+			} catch (Exception $e) {
+				if (PinbaClient::isEnabled())
+					PinbaClient::me()->timerStop('pg_sql_query_'.$this->basename);
+				
+				throw $e;
+			}
+			
+			Assert::isUnreachable();
+		}
+	}
+?>
