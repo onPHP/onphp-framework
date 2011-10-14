a10a6fa13f808f42b8826b7e9ef95d404758e916
AlexeyDsov <alexeydsov@gmail.com>
2011-10-14 21:53:13 +0400

    CallbackLogicalObject: copyrights and changelog

--- core/Logic/CallbackLogicalObject.class.php
+++ core/Logic/CallbackLogicalObject.class.php
@@ -1,6 +1,6 @@
 <?php
 /****************************************************************************
- *   Copyright (C) 2011 Victor Bolshov                                      *
+ *   Copyright (C) 2011 Victor V. Bolshov                                   *
  *                                                                          *
  *   This program is free software; you can redistribute it and/or modify   *
  *   it under the terms of the GNU Lesser General Public License as         *
