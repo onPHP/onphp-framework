c2f2a3a3a4b837c6fb78e6c1b31f6aef35539c2f
Igor V. Gulyaev <igulyaev@gmail.com>
2012-01-16 17:41:26 +0400

    * bit more cases tested. remove svn artifacts

--- test/core/BaseCachesTest.class.php
+++ test/core/BaseCachesTest.class.php
@@ -89,12 +89,12 @@
 
 			$value = 1;
 			$cache->set('a', $value, Cache::EXPIRES_MEDIUM);
-			$cache->increment('a', 1);
+			$this->assertEquals($cache->increment('a', 1), $value+1);
 			$this->assertEquals($cache->get('a'), $value+1);
-			$cache->increment('a', 2);
+			$this->assertEquals($cache->increment('a', 2), $value+3);
 			$this->assertEquals($cache->get('a'), $value+3);
 			$cache->decrement('a', 2);
-			$this->assertEquals($cache->get('a'), $value+1);
+			$this->assertEquals($cache->decrement('a', 2), $value+1);
 
 			$value = '25';
 			$cache->set('a', $value, Cache::EXPIRES_MEDIUM);
