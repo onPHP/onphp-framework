60dda3de7b7edc662a767ccde898f552bf6a25b7
Alexey Gorbylev <alex@gorbylev.ru>
2012-03-13 20:28:29 +0400

    PgSQL profiler

--- core/DB/PgSQL.class.php
+++ core/DB/PgSQL.class.php
@@ -115,6 +115,7 @@
 
 		public function queryRaw($queryString)
 		{
+			if( defined('DB_PROFILE_LOG') ) { file_put_contents(DB_PROFILE_LOG, $queryString."\n", FILE_APPEND); }
 			try {
 				return pg_query($this->link, $queryString);
 			} catch (BaseException $e) {
