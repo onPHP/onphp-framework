6e3ae22eca0eff01fcc224edac007289f2c09889
Evgeny V. Kokovikhin <dovg@dovg.ru>
2012-04-26 00:50:43 -0700

    Merge pull request #93 from dovg/sequenceCacheOneCommit
    * + Sequential cache. See changelog for details

--- test/main/Utils/PinbaTest.class.php
+++ test/main/Utils/PinbaTest.class.php
@@ -2,28 +2,22 @@
 	
 	final class PinbaTest extends TestCase
 	{
-		protected function setUp()
+		protected static $skipMessage	= 'unknown error';
+		protected static $skipped		= false;
+		
+		public static function setUpBeforeClass()
 		{
 			if (!extension_loaded('pinba'))
-				$this->markTestSkipped(
-					'The pinba extension is not available.'
-				);
+				return self::skip('The pinba extension is not available.');
 			
 			if (!PinbaClient::isEnabled())
-				$this->markTestSkipped(
-					'The pinba is not enabled at php.ini (pinba.enabled=1).'
-				);
+				return self::skip('The pinba is not enabled at php.ini (pinba.enabled=1).');
 			
-			if (!extension_loaded('runkit')) {
-				$this->markTestSkipped(
-					'The runkit extension is not available.'
-				);
-			}
+			if (!extension_loaded('runkit'))
+				return self::skip('The runkit extension is not available.');
 			
 			if (!ini_get('runkit.internal_override'))
-				$this->markTestSkipped(
-					'The runkit.internal_override is not enabled (enabled it at php.ini).'
-				);
+				return self::skip('The runkit.internal_override is not enabled (enabled it at php.ini).');
 			
 			runkit_function_rename('pinba_timer_start', 'pinba_timer_start_bak');
 			runkit_function_rename('pinba_timer_stop', 'pinba_timer_stop_bak');
@@ -32,12 +26,9 @@
 			runkit_function_rename('pinba_timer_stop_callback', 'pinba_timer_stop');
 		}
 		
-		public static function tearDownAfterClass(){
-			
-			if (
-				!extension_loaded('runkit') 
-				|| !ini_get('runkit.internal_override')
-			)
+		public static function tearDownAfterClass()
+		{
+			if (self::$skipped)
 				return;
 			
 			runkit_function_rename('pinba_timer_start', 'pinba_timer_start_callback');
@@ -45,6 +36,13 @@
 			
 			runkit_function_rename('pinba_timer_start_bak', 'pinba_timer_start');
 			runkit_function_rename('pinba_timer_stop_bak', 'pinba_timer_stop');
+		}
+		
+		public function setUp()
+		{
+			if (self::$skipped) {
+				$this->markTestSkipped(self::$skipMessage);
+			}
 		}
 		
 		public function testTreeLog()
@@ -55,14 +53,14 @@
 			
 			PinbaClient::me()->timerStart(
 				'test',
-				array("test" => "main")
+				array("test" => 'main')
 			);
 			
 			$this->assertEquals(count(PinbaClient::me()->getTreeQueue()), 1);
 			
 			PinbaClient::me()->timerStart(
 				'subtest',
-				array("test" => "submain")
+				array("test" => 'submain')
 			);
 			
 			$this->assertEquals(count(PinbaClient::me()->getTreeQueue()), 2);
@@ -76,6 +74,12 @@
 			$this->assertEquals(count(PinbaClient::me()->getTreeQueue()), 0);
 			
 		}
+		
+		protected static function skip($message)
+		{
+			self::$skipMessage = $message;
+			self::$skipped = true;
+		}
 	}
 	
 	final class RunkitCallback
@@ -83,14 +87,17 @@
 		public static $queue = array();
 		public static $log = array();
 		
-		public static function start($tags, $data = array())
+		public static function start($tags, array $data = array())
 		{
 			self::$log[] = $tags;
 			end(self::$log);
 			
-			if (!empty($tags['treeParentId']) && $tags['treeParentId'] != "root") {
+			if (
+				!empty($tags['treeParentId'])
+				&& $tags['treeParentId'] != "root"
+			) {
 				if ($tags['treeParentId'] != end(self::$queue)) {
-					throw new Exception("Error generatin tree");
+					throw new Exception('Error generatin tree');
 				}
 			}
 			
@@ -107,7 +114,7 @@
 			$tree_id = $current['treeId'];
 			
 			if (end(self::$queue) != $tree_id) {
-				throw new Exception("Error generatin tree");
+				throw new Exception('Error generatin tree');
 			}
 			
 			array_pop(self::$queue);
@@ -115,11 +122,12 @@
 		}
 	}
 	
-	function pinba_timer_start_callback ($tags, $data = array()) {
+	function pinba_timer_start_callback ($tags, array $data = array())
+	{
 		return RunkitCallback::start($tags, $data);
 	}
 
-	function pinba_timer_stop_callback($id){
+	function pinba_timer_stop_callback($id)
+	{
 		return RunkitCallback::stop($id);
 	}
-?>
