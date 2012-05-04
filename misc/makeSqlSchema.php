1aba43736ebe94f8c271e3f464fce845cfbcfe2f
Evgeny M. Stepanov <from.stev@gmail.com>
2012-05-04 18:01:24 +0400

    optimization

--- misc/makeSqlSchema.php
+++ misc/makeSqlSchema.php
@@ -1,39 +1,23 @@
 #!/usr/bin/php
 <?php
 
-function helpMessage()
-{
-	echo "~~~ [Make sql schema] ~~~\nHelp: \nSet path to project config, exemple: \n\t$ ".$_SERVER['argv'][0]." conf/config.inc.php \n";
+/* Make sql schema
+ *
+ * Set path to project config
+ * exemple:
+ *	$ ./makeSqlSchema.php conf/config.inc.php
+ *
+ */
+
+if (!isset($argv[1])) {
+	exit("error: Set path to project config\n");
 }
 
-$pathConfig = null;
-
-if (isset($argv[1])) {
-	$pathConfig = $argv[1];
-}
-else {
-	helpMessage();
-	exit(1);
-}
 try {
-
-	include $pathConfig;
+	include $argv[1];
 	include PATH_CLASSES.'Auto'.DIRECTORY_SEPARATOR.'schema.php';
 
-	$sql = $schema->toDialectString(DBPool::me()->getLink()->getDialect());
-
-	$pathProject =
-		defined('PATH_PROJECT')? PATH_PROJECT:
-		(defined('PATH_PROJECT_ROOT')? PATH_PROJECT_ROOT:
-		(defined('POJECT_ROOT')? POJECT_ROOT: getcwd().DIRECTORY_SEPARATOR));
-
-	$pathFile = $pathProject.'db'.DIRECTORY_SEPARATOR.'schema.'.(get_class(DBPool::me()->getLink())).'.sql';
-	$file = fopen($pathFile ,'w+');
-	fputs($file, $sql);
-	fclose($file);
-
-	echo "\n Done. Save to $pathFile \n";
-
+	echo PHP_EOL . $schema->toDialectString(DBPool::me()->getLink()->getDialect());
 }catch (Exception $e) {
-	throw $e;
+	file_put_contents('php://outerr', "$e");
 }
