4ea43c82233118df000e9802f09cb83112e4c439
Evgeny M. Stepanov <from.stev@gmail.com>
2012-05-05 12:50:06 +0400

    LGPL

--- misc/makeSqlSchema.php
+++ misc/makeSqlSchema.php
@@ -1,5 +1,14 @@
 #!/usr/bin/php
 <?php
+/***************************************************************************
+ *   Copyright (C) by Evgeny M. Stepanov                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
 
 /* Make sql schema
  *
