c69fa9ffbabc6385d1e2f769d511d9b729df7a0c
Timofey A. Anisimov <anisimovt@gmail.com>
2012-12-11 18:43:19 +0400

    fix non existing enumeration import

--- test/core/PrimitiveEnumerationTest.class.php
+++ test/core/PrimitiveEnumerationTest.class.php
@@ -5,11 +5,7 @@
 	{
 		public function testIntegerValues()
 		{
-			$form =
-				Form::create()->
-				add(
-					Primitive::enumeration('enum')->of('DataType')
-				);
+			$form = $this->getForm();
 			
 			$form->import(array('enum' => '4097'));
 			
@@ -29,6 +25,32 @@
 			
 			$primitive->import(array('enum' => DataType::getAnyId()));
 			$this->assertEquals($primitive->getList(), $enum->getObjectList());
+		}
+		
+		public function testNonExsitingValue()
+		{
+			$form = $this->getForm();
+						
+			$form->get('enum')->
+				setDefault(DataType::create(DataType::getAnyId()));
+			
+			$form->import(array('enum' => -10000));
+			
+			$this->assertFalse($form->get('enum')->isImported());
+			$this->assertNull($form->getValue('enum'));
+			$this->assertEquals(
+				DataType::getAnyId(), 
+				$form->getActualValue('enum')->getId()
+			);
+		}
+		
+		private function getForm()
+		{
+			return
+				Form::create()->
+					add(
+						Primitive::enumeration('enum')->of('DataType')
+					);
 		}
 	}
 ?>
