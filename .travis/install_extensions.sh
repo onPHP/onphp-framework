eaec0a3669c894a4b4a598f595a0e68b1cc2411c
Georgiy Kutsurua <g.kutsurua@ostrovok.ru>
2012-12-20 23:54:53 +0400

    Add travis configuration files

--- .travis/install_extensions.sh
+++ .travis/install_extensions.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-EXT_PATH = `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+PHP_INI_PATH = `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 
 echo "Installing memcahe"
 pecl install memcache
-echo "extension=memcache.so" >> $(EXT_PATH)
+echo "extension=memcache.so" >> $(PHP_INI_PATH)
 
 
 echo "Installing memcahed"
 pecl install memcached
-echo "extension=memcached.so" >> $(EXT_PATH)
+echo "extension=memcached.so" >> $(PHP_INI_PATH)
 
 
