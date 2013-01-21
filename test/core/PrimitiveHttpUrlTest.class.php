b56eedd07c47e40fa76b4b48dab042edf824122f
Slava Tsyrulnik <neemah86@gmail.com>
2013-01-21 16:43:47 +0400

    HttpUrl::isValid skips privileged ports validation by default

---
+++ test/core/PrimitiveHttpUrlTest.class.php
@@ -0,0 +1,44 @@
+<?php
+/***************************************************************************
+ *   Copyright (C) 2013 by Vyacheslav Yu. Tsyrulnik                        *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU Lesser General Public License as        *
+*   published by the Free Software Foundation; either version 3 of the    *
+*   License, or (at your option) any later version.                       *
+*                                                                         *
+***************************************************************************/
+	
+	final class PrimitiveHttpUrlTest extends TestCase
+	{
+		private $urlWithPrivilegedPort = "https://path.to.some.com:444/hey.html";
+		
+		/**
+		 * @test
+		 */
+		public function privilegedPortIsOkByDefault()
+		{
+			$form = Form::create()->add(Primitive::httpUrl("url"));
+			
+			$form->import(array('url' => $this->urlWithPrivilegedPort));
+			$errors = $form->getErrors();
+			
+			$this->assertFalse(isset($errors["url"]));
+		}
+		
+		/**
+		 * @test
+		 */
+		public function privilegedPortInvalid()
+		{
+			$form = Form::create()->add(Primitive::httpUrl("url")->setCheckPrivilegedPorts());
+			
+			$form->import(array('url' => $this->urlWithPrivilegedPort));
+			$errors = $form->getErrors();
+			
+			$this->assertTrue(isset($errors["url"]));
+			$this->assertEquals(Form::WRONG, $errors["url"]);
+		}
+		
+	}
+?>
