6cda990233ef601c0d5beed87302859726671e29
Igor V. Gulyaev <igulyaev@setevisor.tv>
2013-02-15 11:31:21 +0400

    * bit cosmetic

--- main/Net/Http/HeaderUtils.class.php
+++ main/Net/Http/HeaderUtils.class.php
@@ -36,8 +36,9 @@
 				self::$headerSent = true;
 				self::$redirectSent = true;
 				return $_SERVER['HTTP_REFERER'];
-			} else
-				return false;
+			}
+
+			return false;
 		}
 
 		public static function getRequestHeaderList()
@@ -123,9 +124,7 @@
 		{
 			Assert::isInteger($length);
 			
-			header(
-				"Content-Length: {$length}"
-			);
+			header("Content-Length: {$length}");
 			
 			self::$headerSent = true;
 		}
