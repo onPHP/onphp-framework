bfa3fcbcf190a0d76d6c32dc8626dbabe9f987cc
klkvsk <m@klkvsk.ru>
2013-07-11 14:29:16 +0400

    Squashed 'lib/Fenom/' changes from 0920284..9a16fbe
    9a16fbe Merge branch 'fix-functions-args' into dev
    eb1fa83 add tests for custom functions in template
    803fd67 fix use var_export for default params in function (nulls and empty arrays broke templates)
    d307c80 fix parsing of array param in function (was falling in endless loop)
    
    git-subtree-dir: lib/Fenom
    git-subtree-split: 9a16fbe21f0adfa09bcbc029c71c97ec821bfb27

--- src/Fenom/Template.php
+++ src/Fenom/Template.php
@@ -1130,7 +1130,11 @@ class Template extends Render {
                 }
                 if($tokens->is("=")) {
                     $tokens->next();
-                    $params[ $key ] = $this->parseExp($tokens);
+					if ($tokens->is('[')) {
+						$params[ $key ] = $this->parseArray($tokens);
+					} else {
+						$params[ $key ] = $this->parseExp($tokens);
+					}
                 } else {
                     $params[ $key ] = 'true';
                 }
