5de0e4bcf9342b698d3c9343a4a623f0eb383208
bassta <vasily.stashko@gmail.com>
2013-08-29 13:28:12 +0400

    * database connection check fix

--- core/DB/MySQL.class.php
+++ core/DB/MySQL.class.php
@@ -96,6 +96,11 @@
 			return $this;
 		}
 		
+		public function isConnected()
+		{
+			return parent::isConnected() && mysql_ping($this->link);
+		}
+		
 		/**
 		 * Same as query, but returns number of
 		 * affected rows in insert/update queries
