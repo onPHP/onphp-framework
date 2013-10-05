fa2f5eedad162cb31e6be0dba8e637fdc99a8c26
Nikita Konstantinov <n.konstantinov@co.wapstart.ru>
2013-10-05 23:10:49 +0400

    Add HttpRequest::createFromGlobals()

--- main/Flow/HttpRequest.class.php
+++ main/Flow/HttpRequest.class.php
@@ -48,7 +48,7 @@
 		 * @var \Onphp\HttpUrl
 		 */
 		private $url		= null;
-		
+
 		//for CurlHttpClient if you need to send raw CURLOPT_POSTFIELDS
 		private $body		= null;
 		
@@ -57,7 +57,36 @@
 		**/
 		public static function create()
 		{
-			return new self;
+			return new static();
+		}
+
+		/**
+		 * @return HttpRequest
+		**/
+		public static function createFromGlobals()
+		{
+			$request =
+				static::create()->
+				setGet($_GET)->
+				setPost($_POST)->
+				setServer($_SERVER)->
+				setCookie($_COOKIE)->
+				setFiles($_FILES);
+
+			if (isset($_SESSION))
+				$request->setSession($_SESSION);
+
+			foreach ($_SERVER as $name => $value)
+				if (substr($name, 0, 5) === 'HTTP_')
+					$request->setHeaderVar(substr($name, 5), $value);
+
+			if (
+				$request->hasServerVar('CONTENT_TYPE')
+				&& $request->getServerVar('CONTENT_TYPE') !== 'application/x-www-form-urlencoded'
+			)
+				$request->setBody(file_get_contents('php://input'));
+
+			return $request;
 		}
 		
 		public function &getGet()
@@ -149,7 +178,7 @@
 		public function setServer(array $server)
 		{
 			$this->server = $server;
-			
+
 			return $this;
 		}
 		
