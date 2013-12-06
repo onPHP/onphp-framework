4b612013c9494e97403e7d4f8db9461eaebd918d
Evgeny V. Kokovikhin <dovg@dovg.ru>
2013-12-06 15:02:08 +0400

    cosmetics

--- core/OSQL/QuerySkeleton.class.php
+++ core/OSQL/QuerySkeleton.class.php
@@ -119,6 +119,14 @@
 			return null;
 		}
 		
+		/**
+		 * @return QuerySkeleton
+		 */
+		public function spawn()
+		{
+			return clone $this;
+		}
+		
 		protected function resolveSelectField($field, $alias, $table)
 		{
 			if (is_object($field)) {
