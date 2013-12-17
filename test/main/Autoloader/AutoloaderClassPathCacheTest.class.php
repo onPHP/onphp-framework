884643466fdffedee92895b29a8795bdafac4d34
AlexeyDsov <alexeydsov@gmail.com>
2013-12-17 12:54:25 +0400

    test fix AutoloaderClassPathCacheTest

--- test/main/Autoloader/AutoloaderClassPathCacheTest.class.php
+++ test/main/Autoloader/AutoloaderClassPathCacheTest.class.php
@@ -49,8 +49,7 @@
 			
 			//autoload without error, it's allow us to cache data
 			$service->autoload('\Onphp\Form');
-			
-			define('DEBUG', true);
+
 			//second autoload and here we throw error
 			$service->setNamespaceResolver($this->spawnResolver(array(
 				'getPathsCount' => 2,
