49ae65542952a5b4f0a6f53f542b9de0fe128717
klkvsk <m@klkvsk.ru>
2014-02-11 18:00:58 +0400

    merge PowerfullDaoWorker into LinkedDaoWorker

--- main/DAOs/Workers/LinkedDaoWorker.class.php
+++ main/DAOs/Workers/LinkedDaoWorker.class.php
@@ -6,7 +6,125 @@
  * Time: 21:18
  */
 
-class LinkedDaoWorker extends PowerfullDaoWorker {
+class LinkedDaoWorker extends TrickyDaoWorker {
+
+	const
+		SUFFIX_ITEM = '_item_',
+		SUFFIX_CUSTOM = '_custom_'
+	;
+
+	private $suffix;
+
+	public function getById($id, $expires = Cache::EXPIRES_MEDIUM) {
+		return $this->getByQuery($this->makeIdKey($id, false), $expires);
+	}
+
+	protected function makeIdKey($id, $toString = true) {
+		/** @var SelectQuery $query */
+		$query =
+			$this->dao->
+				makeSelectHead()->
+				andWhere(
+					Expression::eq(
+						DBField::create(
+							$this->dao->getIdName(),
+							$this->dao->getTable()
+						),
+						$id
+					)
+				);
+		if ($toString) {
+			return $this->makeQueryKey($query, self::SUFFIX_ITEM);
+		} else {
+			return $query;
+		}
+	}
+
+	/**
+	 * @return $this
+	 */
+	private function setSuffixItem() {
+		$this->suffix = self::SUFFIX_ITEM;
+		return $this;
+	}
+
+	/**
+	 * @return $this
+	 */
+	private function setSuffixList() {
+		$this->suffix = self::SUFFIX_LIST;
+		return $this;
+	}
+
+	/**
+	 * @return $this
+	 */
+	private function setSuffixCustom() {
+		$this->suffix = self::SUFFIX_CUSTOM;
+		return $this;
+	}
+
+	protected function getSuffixQuery() {
+		return $this->suffix;
+	}
+
+	public function getByQuery(SelectQuery $query, $expires = Cache::DO_NOT_CACHE) {
+		$this->setSuffixItem();
+		return parent::getByQuery($query, $expires);
+	}
+
+	public function getCustom(SelectQuery $query, $expires = Cache::DO_NOT_CACHE) {
+		$this->setSuffixCustom();
+		return parent::getCustom($query, $expires);
+	}
+
+	public function getListByQuery(SelectQuery $query, $expires = Cache::DO_NOT_CACHE) {
+		$this->setSuffixList();
+		return parent::getListByQuery($query, $expires);
+	}
+
+	public function getCustomList(SelectQuery $query, $expires = Cache::DO_NOT_CACHE) {
+		$this->setSuffixCustom();
+		return parent::getCustomList($query, $expires);
+	}
+
+	public function getCustomRowList(SelectQuery $query, $expires = Cache::DO_NOT_CACHE) {
+		$this->setSuffixCustom();
+		return parent::getCustomRowList($query, $expires);
+	}
+
+	public function getQueryResult(SelectQuery $query, $expires = Cache::DO_NOT_CACHE) {
+		$this->setSuffixCustom();
+		return parent::getQueryResult($query, $expires);
+	}
+
+	public function uncacheByIds($ids) {
+		$keys = array();
+		foreach ($ids as $id) {
+			$keys[] = $this->makeIdKey($id);
+		}
+
+		$isUncacheIds = Cache::me()
+			->mark($this->className)
+			->deleteList($keys)
+		;
+
+		return $isUncacheIds && $this->dao->uncacheLists();
+	}
+
+	public function uncacheLists() {
+		return Cache::me()
+			->mark($this->className)
+			->deleteByPattern($this->className . self::SUFFIX_LIST)
+			;
+	}
+
+	public function uncacheItems() {
+		return Cache::me()
+			->mark($this->className)
+			->deleteByPattern($this->className . self::SUFFIX_ITEM)
+			;
+	}
 
     protected function cacheByQuery(
         SelectQuery $query,
@@ -107,4 +225,4 @@ class LinkedDaoWorker extends PowerfullDaoWorker {
 
         return $object;
     }
-} 
+}
