72a11e17a0dbef194523a04dac076d061a2a5413
thefish <thefish@php--jedi.com>
2014-11-27 12:46:47 +0300

    more array ops

--- core/Logic/ArrayExpression.class.php
+++ core/Logic/ArrayExpression.class.php
@@ -2,13 +2,24 @@
 /**
  * PostgreSQL array expressions
  * @author Alex Gorbylev <alex@adonweb.ru>
- * @date 2013.04.03
+ * @author Anton Gurov <trashmailbox@e1.ru>
+ * @date 2014.11.27
+ * refer to
+ * http://www.postgresql.org/docs/current/interactive/functions-array.html#ARRAY-OPERATORS-TABLE
+ * for list of array operators
  */
 class ArrayExpression implements LogicalObject, MappableObject {
 
 	const FIELD = null;
 	const ANY = 'ANY';
 	const ALL = 'ALL';
+
+    const CONTAINS          = '@>';
+    const IS_CONTAINED_BY   = '<@';
+
+    const OVERLAP           = '&&';
+
+    const CONCAT            = '||';
 
 	protected
 		$subject = null,
@@ -44,22 +55,89 @@ class ArrayExpression implements LogicalObject, MappableObject {
 		return Expression::eq(DBValue::create($value), self::all($subject));
 	}
 
+    /**
+     * @param $field
+     * @param $value
+     * @return ArrayExpression
+     */
+    public static function contains($field, $value) {
+        return new self(
+            $field, $value, self::CONTAINS
+        );
+    }
+
+    /**
+     * @param $field
+     * @param $value
+     * @return ArrayExpression
+     */
+    public static function isContainedBy($field, $value) {
+        return new self(
+            $field, $value, self::IS_CONTAINED_BY
+        );
+    }
+
+    /**
+     * @param $field
+     * @param $value
+     * @return ArrayExpression
+     */
+    public static function overlap($field, $value) {
+        return new self(
+            $field, $value, self::OVERLAP
+        );
+    }
+
+    /**
+     * @param $field
+     * @param $value
+     * @return ArrayExpression
+     */
+    public static function concat($field, $value) {
+        return new self(
+            $field, $value, self::CONCAT
+        );
+    }
+
+
 	public function toDialectString(Dialect $dialect)
 	{
-		$tableField = $dialect->toFieldString($this->subject);
-		if( !is_null($this->field) ) {
-			$tableField .= "[{$this->field}]";
-		}
+        switch ($this->logic) {
+            //Обращение напрямую к полю
+            case self::FIELD:
+                return $tableField;
 
-		if( $this->logic === self::FIELD ) {
-			return $tableField;
-		}
+            //Операторы, которые между значениями
+            case self::CONTAINS:
+            case self::IS_CONTAINED_BY:
+            case self::OVERLAP:
+            case self::CONCAT:
+                if (!is_array($this->field->getValue())) {
+                    throw new WrongArgumentException("Trying to use array functions with non-array value:"
+                        . print_r($this->field,1));
+                }
+                $right = "ARRAY [".implode(',', $this->field->getValue())."]"; // @todo Dangerous!
+                return
+                    '('
+                    .$dialect->toFieldString($this->subject)
+                    .' '.$dialect->logicToString($this->logic).' '
+                    .$right //Dangerous!
+                    .')';
 
-		return
-			$dialect->logicToString($this->logic).
-			'('
-			.$tableField
-			.')';
+            // ANY и ALL
+            default:
+                $tableField = $dialect->toFieldString($this->subject);
+                if( !is_null($this->field) ) {
+                    $tableField .= "[{$this->field}]";
+                }
+                return
+                    $dialect->logicToString($this->logic).
+                    '('
+                    .$tableField
+                    .')';
+
+        }
+
 	}
 
 	/**
@@ -75,6 +153,29 @@ class ArrayExpression implements LogicalObject, MappableObject {
 	}
 
 	public function toBoolean(Form $form) {
-		throw new UnsupportedMethodException('Method tpBoolean is not supported by ArrayExpression');
+        $left	= $form->toFormValue($this->field);
+        $right	= $form->toFormValue($this->subject);
+
+        $both =
+            (null !== $left)
+            && (null !== $right);
+
+        switch ($this->logic) {
+            case self::CONTAINS:
+                return $both && in_array($left, $right);
+
+            case self::IS_CONTAINED_BY:
+                return $both && in_array($right, $left);
+
+            case self::OVERLAP:
+                $intersection = array_intersect($left , $right);
+                return !empty($intersection);
+
+            case self::CONCAT:
+                throw new UnsupportedMethodException("{$this->logic} cannot be converted to boolean ");
+
+            default:
+		        throw new UnsupportedMethodException('Method toBoolean is not supported by ArrayExpression');
+        }
 	}
 }
