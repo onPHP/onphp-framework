0daf933c790009d1fec4aea221d9f1c103848582
root <dewid@dewid.ru>
2015-12-21 21:19:46 +0300

    refactor code style

--- core/Form/Primitives/PrimitiveIdentifier.class.php
+++ core/Form/Primitives/PrimitiveIdentifier.class.php
@@ -9,146 +9,145 @@
  *                                                                         *
  ***************************************************************************/
 
-	/**
-	 * @ingroup Primitives
-	**/
-	class PrimitiveIdentifier extends IdentifiablePrimitive
-	{
-		private $methodName	= 'getById';
-		
-		/**
-		 * @throws WrongArgumentException
-		 * @return PrimitiveIdentifier
-		**/
-		public function of($class)
-		{
-			$className = $this->guessClassName($class);
-			
-			Assert::classExists($className);
-			
-			Assert::isInstance(
-				$className,
-				'DAOConnected',
-				"class '{$className}' must implement DAOConnected interface"
-			);
-			
-			$this->className = $className;
-			
-			return $this;
-		}
-		
-		/**
-		 * @return GenericDAO
-		**/
-		public function dao()
-		{
-			Assert::isNotNull(
-				$this->className,
-				'specify class name first of all'
-			);
-			
-			return call_user_func(array($this->className, 'dao'));
-		}
-		
-		/**
-		 * @return PrimitiveIdentifier
-		**/
-		public function setMethodName($methodName)
-		{
-			if (is_callable($methodName)) {
-				/* all ok, call what you want */
-			} elseif (strpos($methodName, '::') === false) {
-				$dao = $this->dao();
-				
-				Assert::isTrue(
-					method_exists($dao, $methodName),
-					"knows nothing about '".get_class($dao)
-					."::{$methodName}' method"
-				);
-			} else {
-				ClassUtils::checkStaticMethod($methodName);
-			}
-			
-			$this->methodName = $methodName;
-			
-			return $this;
-		}
-		
-		public function importValue($value)
-		{
-			if ($value instanceof Identifiable) {
-				try {
-					Assert::isInstance($value, $this->className);
-					
-					return
-						$this->import(
-							array($this->getName() => $this->actualExportValue($value))
-						);
-				
-				} catch (WrongArgumentException $e) {
-					return false;
-				}
-			}
-			
-			return parent::importValue($value);
-		}
-		
-		public function import($scope)
-		{
-			if (!$this->className)
-				throw new WrongStateException(
-					"no class defined for PrimitiveIdentifier '{$this->name}'"
-				);
-			
-			$className = $this->className;
-			
-			if (
-				isset($scope[$this->name])
-				&& $scope[$this->name] instanceof $className
-			) {
-				$value = $scope[$this->name];
-				
-				$this->raw = $this->actualExportValue($value);
-				$this->setValue($value);
-				
-				return $this->imported = true;
-			}
-			
-			$result = parent::import($scope);
-			
-			if ($result === true) {
-				try {
-					$result = $this->actualImportValue($this->value);
-					
-					Assert::isInstance($result, $className);
-					
-					$this->value = $result;
-					
-					return true;
-				
-				} catch (WrongArgumentException $e) {
-					// not imported
-				} catch (ObjectNotFoundException $e) {
-					// not imported
-				}
-				
-				$this->value = null;
-				
-				return false;
-			}
-			
-			return $result;
-		}
-		
-		protected function actualImportValue($value)
-		{
-			if (is_callable($this->methodName)) {
-				return call_user_func($this->methodName, $value);
-			} elseif (strpos($this->methodName, '::') === false) {
-				return $this->dao()->{$this->methodName}($value);
-			} else {
-				return ClassUtils::callStaticMethod($this->methodName, $value);
-			}
-		}
-	}
-?>
+/**
+ * @ingroup Primitives
+ **/
+class PrimitiveIdentifier extends IdentifiablePrimitive
+{
+    private $methodName = 'getById';
+
+    /**
+     * @throws WrongArgumentException
+     * @return PrimitiveIdentifier
+     **/
+    public function of($class)
+    {
+        $className = $this->guessClassName($class);
+
+        Assert::classExists($className);
+
+        Assert::isInstance(
+            $className,
+            'DAOConnected',
+            "class '{$className}' must implement DAOConnected interface"
+        );
+
+        $this->className = $className;
+
+        return $this;
+    }
+
+    /**
+     * @return GenericDAO
+     **/
+    public function dao()
+    {
+        Assert::isNotNull(
+            $this->className,
+            'specify class name first of all'
+        );
+
+        return call_user_func([$this->className, 'dao']);
+    }
+
+    /**
+     * @return PrimitiveIdentifier
+     **/
+    public function setMethodName($methodName)
+    {
+        if (is_callable($methodName)) {
+            /* all ok, call what you want */
+        } elseif (strpos($methodName, '::') === false) {
+            $dao = $this->dao();
+
+            Assert::isTrue(
+                method_exists($dao, $methodName),
+                "knows nothing about '" . get_class($dao)
+                . "::{$methodName}' method"
+            );
+        } else {
+            ClassUtils::checkStaticMethod($methodName);
+        }
+
+        $this->methodName = $methodName;
+
+        return $this;
+    }
+
+    public function importValue($value)
+    {
+        if ($value instanceof Identifiable) {
+            try {
+                Assert::isInstance($value, $this->className);
+
+                return
+                    $this->import(
+                        [$this->getName() => $this->actualExportValue($value)]
+                    );
+
+            } catch (WrongArgumentException $e) {
+                return false;
+            }
+        }
+
+        return parent::importValue($value);
+    }
+
+    public function import($scope)
+    {
+        if (!$this->className)
+            throw new WrongStateException(
+                "no class defined for PrimitiveIdentifier '{$this->name}'"
+            );
+
+        $className = $this->className;
+
+        if (
+            isset($scope[$this->name])
+            && $scope[$this->name] instanceof $className
+        ) {
+            $value = $scope[$this->name];
+
+            $this->raw = $this->actualExportValue($value);
+            $this->setValue($value);
+
+            return $this->imported = true;
+        }
+
+        $result = parent::import($scope);
+
+        if ($result === true) {
+            try {
+                $result = $this->actualImportValue($this->value);
+
+                Assert::isInstance($result, $className);
+
+                $this->value = $result;
+
+                return true;
+
+            } catch (WrongArgumentException $e) {
+                // not imported
+            } catch (ObjectNotFoundException $e) {
+                // not imported
+            }
+
+            $this->value = null;
+
+            return false;
+        }
+
+        return $result;
+    }
+
+    protected function actualImportValue($value)
+    {
+        if (is_callable($this->methodName)) {
+            return call_user_func($this->methodName, $value);
+        } elseif (strpos($this->methodName, '::') === false) {
+            return $this->dao()->{$this->methodName}($value);
+        } else {
+            return ClassUtils::callStaticMethod($this->methodName, $value);
+        }
+    }
+}
--- core/Form/Primitives/PrimitiveIdentifierList.class.php
+++ core/Form/Primitives/PrimitiveIdentifierList.class.php
@@ -9,163 +9,163 @@
  *                                                                          *
  ****************************************************************************/
 
-	/**
-	 * @ingroup Primitives
-	**/
-	final class PrimitiveIdentifierList extends PrimitiveIdentifier
-	{
-		protected $value = array();
-		private $ignoreEmpty = false;
-		private $ignoreWrong = false;
-		
-		/**
-		 * @return PrimitiveIdentifierList
-		**/
-		public function clean()
-		{
-			parent::clean();
-			
-			// restoring our very own default
-			$this->value = array();
-			
-			return $this;
-		}
-		
-		/**
-		 * @return PrimitiveIdentifierList
-		**/
-		public function setValue($value)
-		{
-			if ($value) {
-				Assert::isArray($value);
-				Assert::isInstance(current($value), $this->className);
-			}
-			
-			$this->value = $value;
-			
-			return $this;
-		}
-		
-		public function importValue($value)
-		{
-			if ($value instanceof UnifiedContainer) {
-				if ($value->isLazy())
-					return $this->import(
-						array($this->name => $value->getList())
-					);
-				elseif (
-					$value->getParentObject()->getId()
-					&& ($list = $value->getList())
-				) {
-					return $this->import(
-						array($this->name => ArrayUtils::getIdsArray($list))
-					);
-				} else {
-					return parent::importValue(null);
-				}
-			}
-			
-			if (is_array($value)) {
-				try {
-					if ($this->scalar)
-						Assert::isScalar(current($value));
-					else
-						Assert::isInteger(current($value));
-					
-					return $this->import(
-						array($this->name => $value)
-					);
-				} catch (WrongArgumentException $e) {
-					return $this->import(
-						array($this->name => ArrayUtils::getIdsArray($value))
-					);
-				}
-			}
-			
-			return parent::importValue($value);
-		}
-		
-		public function import($scope)
-		{
-			if (!$this->className)
-				throw new WrongStateException(
-					"no class defined for PrimitiveIdentifierList '{$this->name}'"
-				);
-			
-			if (!BasePrimitive::import($scope))
-				return null;
-			
-			if (!is_array($scope[$this->name]))
-				return false;
-			
-			$list = array_unique($scope[$this->name]);
-			
-			$values = array();
-			
-			foreach ($list as $id) {
-				if ((string) $id == "" && $this->isIgnoreEmpty())
-					continue;
+/**
+ * @ingroup Primitives
+ **/
+class PrimitiveIdentifierList extends PrimitiveIdentifier
+{
+    /** @var array  */
+    protected $value = [];
+    private $ignoreEmpty = false;
+    private $ignoreWrong = false;
 
-				if (
-					($this->scalar && !Assert::checkScalar($id))
-					|| (!$this->scalar && !Assert::checkInteger($id))
-				) {
-					if (!$this->isIgnoreWrong())
-						return false;
-					else
-						continue; //just skip it
-				}
-				
-				$values[] = $id;
-			}
-			
-			$objectList = $this->dao()->getListByIds($values);
-			
-			if (
-				(
-					(count($objectList) == count($values))
-					|| $this->isIgnoreWrong()
-				)
-				&& !($this->min && count($values) < $this->min)
-				&& !($this->max && count($values) > $this->max)
-			) {
-				$this->value = $objectList;
-				return true;
-			}
-			
-			return false;
-		}
-		
-		public function exportValue()
-		{
-			if (!$this->value)
-				return null;
-			
-			return ArrayUtils::getIdsArray($this->value);
-		}
+    /**
+     * @return PrimitiveIdentifierList
+     **/
+    public function clean()
+    {
+        parent::clean();
 
-		public function setIgnoreEmpty($orly = true)
-		{
-			$this->ignoreEmpty = ($orly === true);
+        // restoring our very own default
+        $this->value = [];
 
-			return $this;
-		}
+        return $this;
+    }
 
-		public function isIgnoreEmpty()
-		{
-			return $this->ignoreEmpty;
-		}
-		
-		public function setIgnoreWrong($orly = true)
-		{
-			$this->ignoreWrong = ($orly === true);
-			
-			return $this;
-		}
-		
-		public function isIgnoreWrong()
-		{
-			return $this->ignoreWrong;
-		}
-	}
-?>
+    /**
+     * @return PrimitiveIdentifierList
+     **/
+    public function setValue($value)
+    {
+        if ($value) {
+            Assert::isArray($value);
+            Assert::isInstance(current($value), $this->className);
+        }
+
+        $this->value = $value;
+
+        return $this;
+    }
+
+    public function importValue($value)
+    {
+        if ($value instanceof UnifiedContainer) {
+            if ($value->isLazy())
+                return $this->import(
+                    [$this->name => $value->getList()]
+                );
+            elseif (
+                $value->getParentObject()->getId()
+                && ($list = $value->getList())
+            ) {
+                return $this->import(
+                    [$this->name => ArrayUtils::getIdsArray($list)]
+                );
+            } else {
+                return parent::importValue(null);
+            }
+        }
+
+        if (is_array($value)) {
+            try {
+                if ($this->scalar)
+                    Assert::isScalar(current($value));
+                else
+                    Assert::isInteger(current($value));
+
+                return $this->import(
+                    [$this->name => $value]
+                );
+            } catch (WrongArgumentException $e) {
+                return $this->import(
+                    [$this->name => ArrayUtils::getIdsArray($value)]
+                );
+            }
+        }
+
+        return parent::importValue($value);
+    }
+
+    public function import($scope)
+    {
+        if (!$this->className)
+            throw new WrongStateException(
+                "no class defined for PrimitiveIdentifierList '{$this->name}'"
+            );
+
+        if (!BasePrimitive::import($scope))
+            return null;
+
+        if (!is_array($scope[$this->name]))
+            return false;
+
+        $list = array_unique($scope[$this->name]);
+
+        $values = [];
+
+        foreach ($list as $id) {
+            if ((string)$id == "" && $this->isIgnoreEmpty())
+                continue;
+
+            if (
+                ($this->scalar && !Assert::checkScalar($id))
+                || (!$this->scalar && !Assert::checkInteger($id))
+            ) {
+                if (!$this->isIgnoreWrong())
+                    return false;
+                else
+                    continue; //just skip it
+            }
+
+            $values[] = $id;
+        }
+
+        $objectList = $this->dao()->getListByIds($values);
+
+        if (
+            (
+                (count($objectList) == count($values))
+                || $this->isIgnoreWrong()
+            )
+            && !($this->min && count($values) < $this->min)
+            && !($this->max && count($values) > $this->max)
+        ) {
+            $this->value = $objectList;
+            return true;
+        }
+
+        return false;
+    }
+
+    public function exportValue()
+    {
+        if (!$this->value)
+            return null;
+
+        return ArrayUtils::getIdsArray($this->value);
+    }
+
+    public function setIgnoreEmpty($orly = true)
+    {
+        $this->ignoreEmpty = ($orly === true);
+
+        return $this;
+    }
+
+    public function isIgnoreEmpty()
+    {
+        return $this->ignoreEmpty;
+    }
+
+    public function setIgnoreWrong($orly = true)
+    {
+        $this->ignoreWrong = ($orly === true);
+
+        return $this;
+    }
+
+    public function isIgnoreWrong()
+    {
+        return $this->ignoreWrong;
+    }
+}
