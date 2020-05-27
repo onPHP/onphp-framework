1aec37f9c5af374ac29157ad3b2e6a27347d0f3d
Deryabin Sergey <DeryabinSergey@gmail.com>
2020-05-27 22:19:48 +0300

    Add another case, two object duplicated then add in revert order

--- tests/DB/DuplicateObjectExceptionTest.php
+++ tests/DB/DuplicateObjectExceptionTest.php
@@ -32,32 +32,40 @@ class DuplicateObjectExceptionTest extends TestCaseDAO
 		
 		DBPool::me()->getLink()->queryRaw("CREATE UNIQUE INDEX uq_name ON custom_table (name)");
 		
-		$moscow	= TestCity::create()->setName('Moscow');
-		$perm	= TestCity::create()->setName('Moscow'); 
-		$kaluga	= TestCity::create()->setName('Kaluga');
+		$moscow	    = TestCity::create()->setName('Moscow');
+		$perm	    = TestCity::create()->setName('Moscow');
+		$domodedovo = TestCity::create()->setName('Moscow');
+		$kaluga	    = TestCity::create()->setName('Kaluga');
+
 		
 		$moscow = $moscow->dao()->add($moscow);
 
 		try {
 		    $perm = $perm->dao()->add($perm);
-		} catch (DatabaseException $e) {
-		    /** **/
-		}
+		} catch (DatabaseException $e) { /** **/ }
+
+		try {
+		    $domodedovo = $domodedovo->dao()->add($domodedovo);
+		} catch (DatabaseException $e) { /** **/ }
 
 		$kaluga = $kaluga->dao()->add($kaluga);
 
+		$domodedovo = $domodedovo->dao()->add($domodedovo->setName('Domodedovo'));
 		$perm = $perm->dao()->add($perm->setName('Perm'));
 
 		$this->assertIsNumeric($moscow->getId());
 		$this->assertIsNumeric($perm->getId());
+		$this->assertIsNumeric($domodedovo->getId());
 		$this->assertIsNumeric($kaluga->getId());
 
-		$moscowFromDb	= TestCity::dao()->getByLogic(Expression::eq('name', 'Moscow'));
-		$permFromDb	= TestCity::dao()->getByLogic(Expression::eq('name', 'Perm'));
-		$kalugaFromDb	= TestCity::dao()->getByLogic(Expression::eq('name', 'Kaluga'));
+		$moscowFromDb	    = TestCity::dao()->getByLogic(Expression::eq('name', 'Moscow'));
+		$permFromDb	    = TestCity::dao()->getByLogic(Expression::eq('name', 'Perm'));
+		$domodedovoFromDb   = TestCity::dao()->getByLogic(Expression::eq('name', 'Domodedovo'));
+		$kalugaFromDb	    = TestCity::dao()->getByLogic(Expression::eq('name', 'Kaluga'));
 
 		$this->assertEquals($moscow->getId(), $moscowFromDb->getId());
 		$this->assertEquals($perm->getId(), $permFromDb->getId());
+		$this->assertEquals($domodedovo->getId(), $domodedovoFromDb->getId());
 		$this->assertEquals($kaluga->getId(), $kalugaFromDb->getId());
 	}
 	
