4f2bb48fed85c636a72feac4eec64d94ca3f13f2
Deryabin Sergey <DeryabinSergey@gmail.com>
2021-04-17 17:55:06 +0300

    Fix copyright year

--- src/Main/Markup/OGP/OpenGraphPlaylist.php
+++ src/Main/Markup/OGP/OpenGraphPlaylist.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *   Copyright (C) 2007 by Sergei V. Deriabin                              *
+ *   Copyright (C) 2021 by Sergei V. Deriabin                              *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Lesser General Public License as        *
--- tests/TestEnvironment/OpenGraph.php
+++ tests/TestEnvironment/OpenGraph.php
@@ -1,4 +1,13 @@
 <?php
+/***************************************************************************
+ *   Copyright (C) 2021 by Sergei V. Deriabin                              *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
 
 namespace OnPHP\Tests\TestEnvironment;
 
